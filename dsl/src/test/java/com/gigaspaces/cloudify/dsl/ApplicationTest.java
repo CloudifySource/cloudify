265299effc9b1b61ccb386cd3ca1436a62c99079
barakme <barakme@eb64e737-3616-4df0-8941-5ee2ae88103d>
2011-11-17 14:29:37 +0000

    CLOUDIFY-281 Application and Service use BaseDSL Script
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@103191 eb64e737-3616-4df0-8941-5ee2ae88103d

---
+++ dsl/src/test/java/com/gigaspaces/cloudify/dsl/ApplicationTest.java
@@ -0,0 +1,26 @@
+package com.gigaspaces.cloudify.dsl;
+
+import java.io.File;
+
+import org.junit.Test;
+
+import com.gigaspaces.cloudify.dsl.internal.ServiceReader;
+import static org.junit.Assert.*;
+
+public class ApplicationTest {
+
+	private final static String SIMPLE_APP_PATH = "testResources/applications/simple/simple-application.groovy";
+	
+	@Test
+	public void testSimpleApplication() throws Exception {
+		Application app = ServiceReader.getApplicationFromFile(new File(SIMPLE_APP_PATH)).getApplication();
+		assertNotNull(app);
+		assertEquals(2, app.getServices().size());
+		assertNotNull(app.getServices().get(0).getLifecycle());
+		assertNotNull(app.getServices().get(1).getLifecycle());
+		assertEquals("icon2.jpg", app.getServices().get(1).getIcon());
+		
+	}
+
+	
+}
