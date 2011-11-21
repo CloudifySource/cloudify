504409c6278e72687abd04bcd0c073903a2a3c7d
barakme <barakme@eb64e737-3616-4df0-8941-5ee2ae88103d>
2011-11-21 14:27:32 +0000

    CLOUDIFY-115
    Cloud DSL
    
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@103340 eb64e737-3616-4df0-8941-5ee2ae88103d

---
+++ dsl/src/test/java/com/gigaspaces/cloudify/dsl/CloudParserTest.java
@@ -0,0 +1,27 @@
+package com.gigaspaces.cloudify.dsl;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.Test;
+
+import com.gigaspaces.cloudify.dsl.internal.ServiceReader;
+
+public class CloudParserTest {
+
+	private final static String SIMPLE_CLOUD_PATH = "testResources/simple/my-cloud.groovy";
+	
+	@Test
+	public void testCloudParser() throws Exception {
+		Cloud2 cloud = ServiceReader.readCloud(new File(SIMPLE_CLOUD_PATH));
+		assertNotNull(cloud);
+		assertNotNull(cloud.getProvider());
+		assertNotNull(cloud.getTemplates());
+		assertNotNull(cloud.getUser());
+		assertNotNull(cloud.getTemplates().size() > 0);
+		assertNotNull(cloud.getTemplates().get("SMALL_LINUX_32"));
+	}
+
+
+}
