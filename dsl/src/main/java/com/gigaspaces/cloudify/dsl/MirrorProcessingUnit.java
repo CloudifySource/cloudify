8472393e260cd824cc673d38bf4cf8a20fce0570
adaml <adaml@eb64e737-3616-4df0-8941-5ee2ae88103d>
2011-11-22 16:25:41 +0000

    changed annotation
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@103406 eb64e737-3616-4df0-8941-5ee2ae88103d

--- dsl/src/main/java/com/gigaspaces/cloudify/dsl/MirrorProcessingUnit.java
+++ dsl/src/main/java/com/gigaspaces/cloudify/dsl/MirrorProcessingUnit.java
@@ -2,7 +2,7 @@ package com.gigaspaces.cloudify.dsl;
 
 import com.gigaspaces.cloudify.dsl.internal.CloudifyDSLEntity;
 
-@CloudifyDSLEntity(name="MirrorProcessingUnit", clazz=DataGrid.class, allowInternalNode = true, allowRootNode = false, parent = "service")
+@CloudifyDSLEntity(name="MirrorProcessingUnit", clazz=MirrorProcessingUnit.class, allowInternalNode = true, allowRootNode = false, parent = "service")
 public class MirrorProcessingUnit extends StatelessProcessingUnit{
 
 	
