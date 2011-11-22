76863c46896954ce5cc7f37ade2d948c13a6381b
barakme <barakme@eb64e737-3616-4df0-8941-5ee2ae88103d>
2011-11-22 12:47:13 +0000

    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@103384 eb64e737-3616-4df0-8941-5ee2ae88103d

---
+++ dsl/testResources/simple/my-cloud.groovy
@@ -0,0 +1,35 @@
+cloud2 {
+	
+	
+	provider {
+		provider "aws-ec2"
+		localDirectory "tools/cli/plugins/esc/ec2/upload"
+		remoteDirectory "/home/ec2-user/gs-files"
+		cloudifyUrl "https://s3.amazonaws.com/test-repository-ec2dev/cloudify/gigaspaces.zip"
+		machineNamePrefix "gs_esm_gsa_"
+		securityGroup "default"
+
+		dedicatedManagementMachines true
+		managementOnlyFiles ([])
+		connectedToPrivateIp false
+
+		sshLoggingLevel java.util.logging.Level.WARNING
+		managementGroup "management_machine"
+		numberOfManagementMachines 2
+		zones (["agent"])
+		reservedMemoryCapacityPerMachineInMB 1024
+	}
+	user {
+		user "0VCFNJS3FXHYC7M6Y782"
+		apiKey "fPdu7rYBF0mtdJs1nmzcdA8yA/3kbV20NgInn4NO"
+		keyFile "cloud-demo.pem"
+		keyPair "cloud-demo"
+	}
+	templates ([
+				SMALL_LINUX_32 : template{
+					imageId "us-east-1/ami-76f0061f"
+					machineMemoryMB "1600"
+					hardwareId "m1.small"
+				}
+			])
+}
