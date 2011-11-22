6a2b81d0ea9246c883a34f693bf500b8a5c586d0
adaml <adaml@eb64e737-3616-4df0-8941-5ee2ae88103d>
2011-11-22 13:20:42 +0000

    added DSL Entity annotation
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@103392 eb64e737-3616-4df0-8941-5ee2ae88103d

--- dsl/src/main/java/com/gigaspaces/cloudify/dsl/MirrorProcessingUnit.java
+++ dsl/src/main/java/com/gigaspaces/cloudify/dsl/MirrorProcessingUnit.java
@@ -1,5 +1,8 @@
 package com.gigaspaces.cloudify.dsl;
 
+import com.gigaspaces.cloudify.dsl.internal.CloudifyDSLEntity;
+
+@CloudifyDSLEntity(name="MirrorProcessingUnit", clazz=DataGrid.class, allowInternalNode = true, allowRootNode = false, parent = "service")
 public class MirrorProcessingUnit extends StatelessProcessingUnit{
 
 	
