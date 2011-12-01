441c49e69130054e0aaebb5178566a418d39674a
itaif <itaif@eb64e737-3616-4df0-8941-5ee2ae88103d>
2011-12-01 19:05:55 +0000

    Fixed ArrayIndexOutOfBoundsException by waiting for at least one rest instance before inspecting it's IP address.
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@104100 eb64e737-3616-4df0-8941-5ee2ae88103d

--- azure/travel-withproxy/tomcat/tomcat_post-start.groovy
+++ azure/travel-withproxy/tomcat/tomcat_post-start.groovy
@@ -4,10 +4,13 @@ import java.io.File
 import java.util.concurrent.TimeUnit
 import java.net.InetAddress
 
+// discover rest server URL
 admin = ServiceContextFactory.getServiceContext().getAdmin()
-restMachine = admin.getProcessingUnits().waitFor("rest", 60, TimeUnit.SECONDS).getInstances()[0].getMachine().getHostAddress()
+admin.getProcessingUnits().waitFor("rest", 60, TimeUnit.SECONDS).waitFor(1, 60, TimeUnit.SECONDS)
+restMachine = admin.getProcessingUnits().getProcessingUnit("rest").getInstances()[0].getMachine().getHostAddress()
 restUrl = "http://${restMachine}:8100/rest/"
 
+//run cloudify invoke command that updates the load balancer
 cliPath = new File(Environment.getHomeDirectory(), "\\tools\\cli\\cloudify.bat").toString()
 
 rewriteLbCommand = [
