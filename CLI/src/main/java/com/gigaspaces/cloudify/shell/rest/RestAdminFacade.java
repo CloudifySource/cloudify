ef9330d0cf5da3d9fbbda9659fd9ffe9dd699b92
adaml <adaml@eb64e737-3616-4df0-8941-5ee2ae88103d>
2011-12-05 11:17:40 +0000

    extended PU lookup timeout period
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@104229 eb64e737-3616-4df0-8941-5ee2ae88103d

--- CLI/src/main/java/com/gigaspaces/cloudify/shell/rest/RestAdminFacade.java
+++ CLI/src/main/java/com/gigaspaces/cloudify/shell/rest/RestAdminFacade.java
@@ -42,7 +42,7 @@ import com.gigaspaces.cloudify.shell.commands.CLIException;
  */
 public class RestAdminFacade extends AbstractAdminFacade {
 
-	private static final int PROCESSINGUNIT_LOOKUP_TIMEOUT = 15;
+	private static final int PROCESSINGUNIT_LOOKUP_TIMEOUT = 30;
 	private static final int POLLING_INTERVAL = 2000;
 	private static final String GS_USM_COMMAND_NAME = "GS_USM_CommandName";
 	private static final String SERVICE_CONTROLLER_URL = "/service/";
