d235536ae13a9a7bbfc32609edda6fb2e9ef0aae
idan <idan@eb64e737-3616-4df0-8941-5ee2ae88103d>
2011-12-14 13:42:50 +0000

    Initial eclipse plugin azure server type commit.
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@104891 eb64e737-3616-4df0-8941-5ee2ae88103d

---
+++ eclipse-plugin/azurePlugin/src/com/gigaspaces/azure/StartServer.java
@@ -0,0 +1,16 @@
+package com.gigaspaces.azure;
+
+import java.util.logging.Logger;
+
+public class StartServer {
+
+	/**
+	 * @param args
+	 * @throws Exception 
+	 */
+	public static void main(String[] args) throws Exception {
+		final Logger logger = Logger.getLogger(StartServer.class.getPackage().getName());		
+		logger.info("Windows Azure Server started");
+	}
+
+}
