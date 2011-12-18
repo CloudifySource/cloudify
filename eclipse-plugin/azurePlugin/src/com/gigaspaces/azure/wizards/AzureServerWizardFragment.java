7af929df2264b37d176b9c9d9b19f714dd9e3fcd
idan <idan@eb64e737-3616-4df0-8941-5ee2ae88103d>
2011-12-18 14:10:07 +0000

    Added wizard implementation (extending generic one..) for adding performFinish functionality.
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@105183 eb64e737-3616-4df0-8941-5ee2ae88103d

---
+++ eclipse-plugin/azurePlugin/src/com/gigaspaces/azure/wizards/AzureServerWizardFragment.java
@@ -0,0 +1,15 @@
+package com.gigaspaces.azure.wizards;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jst.server.generic.ui.internal.GenericServerWizardFragment;
+
+public class AzureServerWizardFragment extends GenericServerWizardFragment {
+
+	@Override
+	public void performFinish(IProgressMonitor monitor) throws CoreException {
+		// TODO Auto-generated method stub
+		super.performFinish(monitor);
+	}
+	
+}
