1b281f6cfbc81f0e88feb1e421e808626a0847a3
sagib <sagib@eb64e737-3616-4df0-8941-5ee2ae88103d>
2011-12-19 14:09:34 +0000

    made travel tomcat to extend the recipe tomcat
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@105257 eb64e737-3616-4df0-8941-5ee2ae88103d

--- recipes/src/main/resources/applications/travel/tomcat/update_war.groovy
+++
@@ -1,6 +0,0 @@
-config = new ConfigSlurper().parse(new File("tomcat.properties").toURL())
-
-new AntBuilder().sequential {
-	get(src:config.applicationWarUrl, dest:config.applicationWar, skipexisting:false)
-	copy(todir: "${config.home}/webapps",  file:config.applicationWar, overwrite:true)
-}
