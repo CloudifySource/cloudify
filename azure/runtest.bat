19b6a01fe5df07b53858588bad8e49c0c2ceb357
itaif <itaif@eb64e737-3616-4df0-8941-5ee2ae88103d>
2011-12-19 17:59:51 +0000

    temporary fix to project.version injection to azure test
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@105274 eb64e737-3616-4df0-8941-5ee2ae88103d

--- azure/runtest.bat
+++ azure/runtest.bat
@@ -89,8 +89,8 @@ REM cd azure
 REM END OF FOR TESTING
 
 cd azure
-call mvn -DskipTests=true install
-call mvn -DskipTests=false -Dsurefire.useFile=false -Dlocal.working.dir=%LOCAL_WORKING_DIR% test
+call mvn -Dproject.version=2.0.0 -DskipTests=true install
+call mvn -Dproject.version=2.0.0 -DskipTests=false -Dsurefire.useFile=false -Dlocal.working.dir=%LOCAL_WORKING_DIR% test
 REM call mvn -Dtest.debug.mode=true -Dmaven.surefire.debug="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8000 -Xnoagent -Djava.compiler=NONE" -DskipTests=false -DuseFile=false -Dlocal.working.dir=%LOCAL_WORKING_DIR% test
 
 if ERRORLEVEL 1 goto err
