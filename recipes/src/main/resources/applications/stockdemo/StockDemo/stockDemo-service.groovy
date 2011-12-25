6c5864506f5f8b93a939ec17d8902c3f7b2fdf28
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2011-12-25 12:53:17 +0000

    copy stockdemp back to recipes
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@105653 eb64e737-3616-4df0-8941-5ee2ae88103d

---
+++ recipes/src/main/resources/applications/stockdemo/StockDemo/stockDemo-service.groovy
@@ -0,0 +1,19 @@
+service {
+	
+	icon "icon.png"
+	name "StockDemo"
+	statelessProcessingUnit {
+		binaries "stockDemo.war" //can be a folder, or a war file   		
+		sla {
+				highlyAvailable false
+				memoryCapacityPerContainer 128 
+			}
+	}
+	
+    // TODO use this value by ServiceController to inject the proper port to jetty
+    network {
+        port = 8080
+        protocolDescription = "HTTP"
+    }
+    
+}
---
+++ recipes/src/main/resources/applications/stockdemo/stockAnalyticsSpace/stockAnalyticsSpace-service.groovy
@@ -0,0 +1,15 @@
+service {
+	
+	icon "icon.png"
+	name "stockAnalyticsSpace"
+	statefulProcessingUnit {
+		binaries "stockAnalyticsSpace" //can be a folder, or a war file   		
+		sla {
+				//memoryCapacity 512
+				//maxMemoryCapacity 512
+				highlyAvailable true
+				memoryCapacityPerContainer 256 
+			}
+	}
+	
+}
