7e4a6b11f49e0fc091a1e70cb92866ab6c4f2b38
itaif <itaif@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-01-01 06:31:41 +0000

    CLOUDIFY-324 Removed extra parenthesis
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@106105 eb64e737-3616-4df0-8941-5ee2ae88103d

--- recipes/src/main/resources/applications/travel-azure/tomcat/tomcat_post-stop.groovy
+++ recipes/src/main/resources/applications/travel-azure/tomcat/tomcat_post-stop.groovy
@@ -6,7 +6,7 @@ import java.net.InetAddress
 
 admin = ServiceContextFactory.getServiceContext().getAdmin()
 admin.getProcessingUnits().waitFor("management.rest", 60, TimeUnit.SECONDS).waitFor(1, 60, TimeUnit.SECONDS)
-restMachine = admin.getProcessingUnits().getProcessingUnit(("management.rest").getInstances()[0].getMachine().getHostAddress()
+restMachine = admin.getProcessingUnits().getProcessingUnit("management.rest").getInstances()[0].getMachine().getHostAddress()
 restUrl = "http://${restMachine}:8100/rest/"
 
 cliPath = new File(Environment.getHomeDirectory(), "\\tools\\cli\\cloudify.bat").toString()
