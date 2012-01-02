0593dee7a510d9185c5bdc7bfb664da23ca41696
barakme <barakme@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-01-02 08:03:23 +0000

    CLOUDIFY-384
    Added private/public IP to cloud node and USM details
    
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@106201 eb64e737-3616-4df0-8941-5ee2ae88103d

--- USM/src/main/java/com/gigaspaces/cloudify/usm/details/ProcessDetails.java
+++ USM/src/main/java/com/gigaspaces/cloudify/usm/details/ProcessDetails.java
@@ -3,6 +3,7 @@ package com.gigaspaces.cloudify.usm.details;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.gigaspaces.cloudify.dsl.internal.CloudifyConstants;
 import com.gigaspaces.cloudify.usm.UniversalServiceManagerBean;
 import com.gigaspaces.cloudify.usm.UniversalServiceManagerConfiguration;
 
@@ -15,6 +16,17 @@ public class ProcessDetails implements Details {
 		final Map<String, Object> map = new HashMap<String, Object>();
 		map.put("GSC PID", usm.getContainerPid());
 		map.put("Working Directory", usm.getPuExtDir().getAbsolutePath());
+		
+		final String privateIp = System.getenv(CloudifyConstants.CLOUDIFY_AGENT_ENV_PRIVATE_IP);
+		final String publicIp = System.getenv(CloudifyConstants.CLOUDIFY_AGENT_ENV_PUBLIC_IP);
+		
+		if(privateIp != null) {
+			map.put(CloudifyConstants.USM_DETAILS_PRIVATE_IP, privateIp);
+		}
+		if(publicIp != null) {
+			map.put(CloudifyConstants.USM_DETAILS_PUBLIC_IP, publicIp);
+		}
+		
 		
 
 		return map;
