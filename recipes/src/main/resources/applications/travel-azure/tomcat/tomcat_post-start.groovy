01ffef298d075979a570a9dd394386120018ba8c
itaif <itaif@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-01-02 20:47:55 +0000

    CLOUDIFY-324 Fixed problem due to CLI invoke changes
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@106367 eb64e737-3616-4df0-8941-5ee2ae88103d

--- recipes/src/main/resources/applications/travel-azure/tomcat/tomcat_post-start.groovy
+++ recipes/src/main/resources/applications/travel-azure/tomcat/tomcat_post-start.groovy
@@ -13,13 +13,7 @@ restUrl = "http://${restMachine}:8100/rest/"
 //run cloudify invoke command that updates the load balancer
 cliPath = new File(Environment.getHomeDirectory(), "\\tools\\cli\\cloudify.bat").toString()
 
-rewriteLbCommand = [
-	"name" : "travel",
-	"port" : 8080
-]
-
-paramsMap = rewriteLbCommand.collect { k, v -> "'${k}=${v}'" }.join(' ')
-def process = "cmd /c call ${cliPath} connect ${restUrl}; use-application management; invoke iisproxy rewrite_add_external_lb ${paramsMap}".execute();
+def process = "cmd /c call ${cliPath} connect ${restUrl}; use-application management; invoke iisproxy rewrite_add_external_lb travel 8080".execute();
 process.waitForProcessOutput(System.out, System.err)
 if (process.exitValue() != 0) {
 	throw new Exception("Failed to set iisproxy rewrite rule");
--- recipes/src/main/resources/applications/travel-azure/tomcat/tomcat_post-stop.groovy
+++ recipes/src/main/resources/applications/travel-azure/tomcat/tomcat_post-stop.groovy
@@ -11,12 +11,7 @@ restUrl = "http://${restMachine}:8100/rest/"
 
 cliPath = new File(Environment.getHomeDirectory(), "\\tools\\cli\\cloudify.bat").toString()
 
-removeRewriteLbCommand = [
-	"name" : "travel"
-]
-
-paramsMap = removeRewriteLbCommand.collect { k, v -> "'${k}=${v}'" }.join(' ')
-def process = "cmd /c call ${cliPath} connect ${restUrl}; use-application management; invoke iisproxy rewrite_remove_external_lb ${paramsMap}".execute();
+def process = "cmd /c call ${cliPath} connect ${restUrl}; use-application management; invoke iisproxy rewrite_remove_external_lb travel".execute();
 process.waitForProcessOutput(System.out, System.err)
 if (process.exitValue() != 0) {
 	throw new Exception("Failed to set iisproxy rewrite rule");
