0f3b96aa5eca321bb180bac4ca51f5c4870349dc
opencm <opencm@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-01-05 18:55:49 +0000

    Update cloudify url in ec2-cloud.groovy
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@106810 eb64e737-3616-4df0-8941-5ee2ae88103d

--- esc/ec2-cloud.groovy
+++ esc/ec2-cloud.groovy
@@ -11,7 +11,7 @@ cloud {
 		provider "aws-ec2"
 		localDirectory "tools/cli/plugins/esc/ec2/upload"
 		remoteDirectory "/home/ec2-user/gs-files"
-		cloudifyUrl "http://d3ce4p99glv9el.cloudfront.net/cloudify/2.0.0/996-266/gigaspaces-cloudify-2.0.0-rc2-b996-266.zip" 
+		cloudifyUrl "http://d3ce4p99glv9el.cloudfront.net/cloudify/2.0.0/996-268/gigaspaces-cloudify-2.0.0-rc2-b996-268.zip" 
 		machineNamePrefix "gs_esm_gsa_"
 		
 		dedicatedManagementMachines true
