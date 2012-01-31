0291f339264a417dbc2a4f647e9d4e18bee63740
itaif <itaif@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-01-31 18:38:28 +0000

    CLOUDIFY-520 Fixed port detection tamir/yaron review by itaif
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@108820 eb64e737-3616-4df0-8941-5ee2ae88103d

--- recipes/src/main/resources/applications/petclinic/mongoConfig/mongoConfig-service.groovy
+++ recipes/src/main/resources/applications/petclinic/mongoConfig/mongoConfig-service.groovy
@@ -1,47 +1,44 @@
 /*******************************************************************************
-* Copyright (c) 2011 GigaSpaces Technologies Ltd. All rights reserved
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*       http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*******************************************************************************/
+ * Copyright (c) 2012 GigaSpaces Technologies Ltd. All rights reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 service {
 	
 	name "mongoConfig"
 	icon "mongodb.png"
 	type "NOSQL_DB"
 	numInstances 1
+	
+	compute {
+		template "SMALL_LINUX_32"
+	}
 
 	lifecycle {
-		init "mongoConfig_install.groovy"
+		install "mongoConfig_install.groovy"
 		start "mongoConfig_start.groovy"		
-		postStart "mongoConfig_poststart.groovy"
+		startDetectionTimeoutSecs 60
+		startDetection {
+			ServiceUtils.isPortOccupied(context.attributes.thisInstance["port"])
+		}
 	}
 	
-	plugins([
-        plugin {
-            name "portLiveness"
-            className "org.cloudifysource.mongodb.MongoLivenessDetector"
-            config ([                
-				"port" : 20000,
-                "timeoutInSeconds" : 60,
-                "host" : "127.0.0.1"
-            ])
-        }, 
+	plugins([	
 		plugin {
 			name "MongoDBMonitorsPlugin"
 			className "org.cloudifysource.mongodb.MongoDBMonitorsPlugin"
 			config([				
 				"host":"127.0.0.1",				
-				"port" : 20000,
 				"dbName":"mydb",
 				"dataSpec":[
 				    "Active Read Clients":"globalLock.activeClients.readers", 
--- recipes/src/main/resources/applications/petclinic/mongoConfig/mongoConfig_start.groovy
+++ recipes/src/main/resources/applications/petclinic/mongoConfig/mongoConfig_start.groovy
@@ -1,31 +1,44 @@
 /*******************************************************************************
-* Copyright (c) 2011 GigaSpaces Technologies Ltd. All rights reserved
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*       http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*******************************************************************************/
+ * Copyright (c) 2012 GigaSpaces Technologies Ltd. All rights reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+import org.cloudifysource.dsl.context.ServiceContextFactory
+
 config = new ConfigSlurper().parse(new File('mongoConfig.properties').toURL())
 
-//start mongod
-dataDir = "${config.home}/data/cfg"
+serviceContext = ServiceContextFactory.getServiceContext()
+instanceID = serviceContext.getInstanceId()
 
+def port = serviceContext.attributes.thisInstance["port"] 
+intPort=port.intValue()
+
+println "mongoConfig_start.groovy: mongoConfig#${instanceID} is using port ${intPort}"
+
+
+dataDir = "${config.home}/data/cfg"
+println "mongoConfig_start.groovy: dataDir is ${dataDir}"
+
+println "mongoConfig_start.groovy: Running script ${config.script} for mongoConfig#${instanceID}..."
 new AntBuilder().sequential {
 	//creating the data directory 	
     delete(file:"${dataDir}/mongod.lock")
 	mkdir(dir:dataDir)
-	exec(executable:config.script) {
+	exec(executable:"${config.script}") {
 		arg value:"--configsvr"
 		arg line:"--dbpath \"${dataDir}\""
-		arg line:"--port ${config.port}"
+		arg line:"--port ${intPort}"
     }
 }
 
+println "mongoConfig_start.groovy: Script ${config.script} ended for mongoConfig#${instanceID}"
