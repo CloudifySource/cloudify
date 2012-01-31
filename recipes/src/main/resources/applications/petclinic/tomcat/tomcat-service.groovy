f38c356d7150005c0723dd2591580452fc12fb56
itaif <itaif@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-01-31 18:38:39 +0000

    CLOUDIFY-520 Replaced port detection, and added more logging. Also changed to dynamic port discovery of mongo tamir/yaron, reviewed by itaif
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@108824 eb64e737-3616-4df0-8941-5ee2ae88103d

--- recipes/src/main/resources/applications/petclinic/tomcat/tomcat-service.groovy
+++ recipes/src/main/resources/applications/petclinic/tomcat/tomcat-service.groovy
@@ -1,25 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2012 GigaSpaces Technologies Ltd. All rights reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 service {
 	name "tomcat"
 	icon "tomcat.gif"
 	type "WEB_SERVER"
 	numInstances 1
 	
+	compute {
+		template "SMALL_LINUX_32"
+	}
+		
 	lifecycle {
-		init "tomcat_install.groovy"
+		install "tomcat_install.groovy"
 		start "tomcat_start.groovy" 		
 		preStop "tomcat_stop.groovy"
+		startDetectionTimeoutSecs 60
+		startDetection {
+			ServiceUtils.isPortOccupied(8080)
+		}
 	}
 	
-	plugins([
-		plugin {			
-            name "portLiveness"
-            className "org.cloudifysource.usm.liveness.PortLivenessDetector"
-            config ([
-                "Port" : [8080],
-                "TimeoutInSeconds" : 240,
-                "Host" : "127.0.0.1"
-            ])
-        }, 
+	plugins([		
 		plugin {
 			name "jmx"
 			className "org.cloudifysource.usm.jmx.JmxMonitor"			
--- recipes/src/main/resources/applications/petclinic/tomcat/tomcat_stop.groovy
+++ recipes/src/main/resources/applications/petclinic/tomcat/tomcat_stop.groovy
@@ -1,6 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2012 GigaSpaces Technologies Ltd. All rights reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+println "tomcat_stop.groovy: About to stop tomcat..."
+
 config = new ConfigSlurper().parse(new File("tomcat.properties").toURL())
 
-println "executing command ${config.script}"
+println "tomcat_stop.groovy: executing command ${config.script}"
 new AntBuilder().sequential {
 	exec(executable:"${config.script}.sh", osfamily:"unix") {
         env(key:"CATALINA_HOME", value: "${config.home}")
@@ -11,3 +28,5 @@ new AntBuilder().sequential {
 		arg(value:"stop")
 	}
 }
+
+println "tomcat_stop.groovy: tomcat is stopped"
