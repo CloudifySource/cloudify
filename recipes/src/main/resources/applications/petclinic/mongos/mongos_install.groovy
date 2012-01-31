9510d554bd125df5703f68bbde85762b292dc8d4
itaif <itaif@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-01-31 18:38:26 +0000

    CLOUDIFY-520 Fixed port detection tamir/yaron, review by itaif
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@108819 eb64e737-3616-4df0-8941-5ee2ae88103d

--- recipes/src/main/resources/applications/petclinic/mongos/mongos_install.groovy
+++ recipes/src/main/resources/applications/petclinic/mongos/mongos_install.groovy
@@ -1,34 +1,45 @@
 /*******************************************************************************
-* Copyright (c) 2011 GigaSpaces Technologies Ltd. All rights reserved
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*       http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*******************************************************************************/
+ * Copyright (c) 2012 GigaSpaces Technologies Ltd. All rights reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+import org.cloudifysource.dsl.context.ServiceContextFactory
 import org.cloudifysource.usm.USMUtils
 
 config = new ConfigSlurper().parse(new File("mongos.properties").toURL())
 osConfig = USMUtils.isWindows() ? config.win32 : config.unix
 
+serviceContext = ServiceContextFactory.getServiceContext()
+instanceID = serviceContext.getInstanceId()
+
+println "mongos_install.groovy: Writing mongos port to this instance(${instanceID}) attributes..."
+serviceContext.attributes.thisInstance["port"] = config.basePort+instanceID
+port = serviceContext.attributes.thisInstance["port"] 
+println "mongos_install.groovy: mongos(${instanceID}) port ${port}"
+
+
 builder = new AntBuilder()
 builder.sequential {
-	mkdir(dir:config.installDir)
-	get(src:osConfig.downloadPath, dest:"${config.installDir}/${osConfig.zipName}", skipexisting:true)
+	mkdir(dir:"${config.installDir}")
+	get(src:"${osConfig.downloadPath}", dest:"${config.installDir}/${osConfig.zipName}", skipexisting:true)
 }
 
 if(USMUtils.isWindows()) {
-	builder.unzip(src:"${config.installDir}/${osConfig.zipName}", dest:config.installDir, overwrite:true)
+	builder.unzip(src:"${config.installDir}/${osConfig.zipName}", dest:"${config.installDir}", overwrite:true)
 } else {
-	builder.untar(src:"${config.installDir}/${osConfig.zipName}", dest:config.installDir, compression:"gzip", overwrite:true)
+	builder.untar(src:"${config.installDir}/${osConfig.zipName}", dest:"${config.installDir}", compression:"gzip", overwrite:true)
 	builder.chmod(dir:"${config.installDir}/${osConfig.name}/bin", perm:'+x', includes:"*")
 }
-builder.move(file:"${config.installDir}/${osConfig.name}", tofile:config.home)
+builder.move(file:"${config.installDir}/${osConfig.name}", tofile:"${config.home}")
 
+println "mongos_install.groovy: Installation of mongos(${instanceID}) ended"
--- recipes/src/main/resources/applications/petclinic/mongos/mongos_start.groovy
+++ recipes/src/main/resources/applications/petclinic/mongos/mongos_start.groovy
@@ -1,30 +1,47 @@
 /*******************************************************************************
-* Copyright (c) 2011 GigaSpaces Technologies Ltd. All rights reserved
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*       http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*******************************************************************************/
+ * Copyright (c) 2012 GigaSpaces Technologies Ltd. All rights reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 import org.cloudifysource.dsl.context.ServiceContextFactory
 import java.util.concurrent.TimeUnit
 
 config = new ConfigSlurper().parse(new File('mongos.properties').toURL())
 
 serviceContext = ServiceContextFactory.getServiceContext()
-mongoService = serviceContext.waitForService("mongoConfig", 20, TimeUnit.SECONDS)
-mongoInstances = mongoService.waitForInstances(mongoService.numberOfPlannedInstances, 60, TimeUnit.SECONDS)
-cfgHost = mongoInstances[0].hostAddress
+mongoCfgService = serviceContext.waitForService("mongoConfig", 20, TimeUnit.SECONDS)
+mongoCfgInstances = mongoCfgService.waitForInstances(mongoCfgService.numberOfPlannedInstances, 60, TimeUnit.SECONDS)
+instanceID = serviceContext.getInstanceId()
+println "mongos_start.groovy: mongos#${instanceID} is using mongoConfig ${instanceID}"
+mongoCfgHost = mongoCfgInstances[instanceID-1].hostAddress
+println "mongos_start.groovy: mongos#${instanceID} is using mongoConfig ${mongoCfgHost}"
 
-new AntBuilder().exec(executable:config.script) {
-	arg line:"--configdb ${cfgHost}:${config.cfgPort}"
-	arg line:"--port ${config.port}"
+
+def port = serviceContext.attributes.thisInstance["port"] 
+intPort=port.intValue()
+println "mongos_start.groovy: mongos(${instanceID}) port ${intPort}"
+
+
+cfgPort=serviceContext.attributes."mongoConfig".instances[instanceID].port
+println "mongos_start.groovy: mongoConfig(${instanceID}) port is ${cfgPort}"
+
+println "mongos_start.groovy: Running script ${config.script} for mongos#${instanceID} ..."
+new AntBuilder().exec(executable:"${config.script}") {
+	arg line:"--configdb ${mongoCfgHost}:${cfgPort}"
+	arg line:"--port ${intPort}"
 	arg line:"--chunkSize 1"
 }
+
+println "mongos_start.groovy: Script ${config.script} ended for mongos#${instanceID}"
+
+
