16107eea813bf8114b5b10df245e519c6543e412
noak <noak@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-02-28 18:11:09 +0000

    CLOUDIFY-388 Cloud Driver for BYON.
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@111102 eb64e737-3616-4df0-8941-5ee2ae88103d

---
+++ esc/byon-cloud.groovy
@@ -0,0 +1,69 @@
+
+cloud {
+	name = "byon"
+	configuration {
+		className "org.cloudifysource.esc.driver.provisioning.byon.ByonProvisioningDriver"
+		managementMachineTemplate "SMALL_LINUX_32"
+		connectToPrivateIp true
+		bootstrapManagementOnPublicIp false
+	}
+
+	provider {
+		provider "byon"
+		localDirectory "tools/cli/plugins/esc/byon/upload"
+		remoteDirectory "/home/test/"
+		cloudifyUrl "http://pc-lab25:8087/publish/gigaspaces.zip"
+		machineNamePrefix "cloudify_agent_"
+		
+		dedicatedManagementMachines true
+		managementOnlyFiles ([])
+		
+
+		sshLoggingLevel "WARNING"
+		managementGroup "cloudify_managemet"
+		numberOfManagementMachines 1
+		zones (["agent"])
+		reservedMemoryCapacityPerMachineInMB 1024
+		
+	}
+	user {
+		user "test"
+		apiKey "test"
+		keyFile ""
+	}
+	templates ([
+				SMALL_LINUX_32 : template{
+					imageId "us-east-1/ami-76f0061f"
+					machineMemoryMB 1600
+					hardwareId "m1.small"
+					locationId "us-east-1"
+					options ([
+						"securityGroups" : ["default"] as String[],
+						"keyPair" : "cloud-demo"
+					])
+				}
+			])
+	custom ([
+		"nodesPool" : ([
+							([
+								"id" : "pc-lab42",
+								"ip" : "192.168.9.62",
+								"username" : "test",
+								"credential" : "test"
+							]),
+							([
+								"id" : "pc-lab43",
+								"ip" : "192.168.9.63",
+								"username" : "test",
+								"credential" : "test"
+							]),
+							([
+								"id" : "pc-lab80",
+								"ip" : "192.168.9.100",
+								"username" : "test",
+								"credential" : "test"
+							])
+						])
+			])
+}
+
