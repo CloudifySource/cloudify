678fd5ca1936de83b62d7e37f19891f7b77533b4
barakme <barakme@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-02-28 14:35:08 +0000

    CLOUDIFY-575 Added initial support for overrides zip file
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@111065 eb64e737-3616-4df0-8941-5ee2ae88103d

--- dsl/src/main/java/org/cloudifysource/dsl/cloud/CloudConfiguration.java
+++ dsl/src/main/java/org/cloudifysource/dsl/cloud/CloudConfiguration.java
@@ -17,8 +17,8 @@ package org.cloudifysource.dsl.cloud;
 
 import org.cloudifysource.dsl.internal.CloudifyDSLEntity;
 
-
-@CloudifyDSLEntity(name = "configuration", clazz = CloudConfiguration.class, allowInternalNode = true, allowRootNode = false, parent = "cloud")
+@CloudifyDSLEntity(name = "configuration", clazz = CloudConfiguration.class, allowInternalNode = true,
+		allowRootNode = false, parent = "cloud")
 public class CloudConfiguration {
 
 	private String className = "org.cloudifysource.esc.driver.provisioning.jclouds.DefaultCloudProvisioning";
@@ -28,70 +28,85 @@ public class CloudConfiguration {
 	private String managementMachineTemplate;
 	private boolean bootstrapManagementOnPublicIp = true;
 	private boolean connectToPrivateIp = true;
-	
+
 	// Remote access credentials for a cloud machine.
 	// These credentials will be used if the cloud does not provide machine specific details.
 	// The password is only used if a key file is not supplied.
 	private String remoteUsername;
 	private String remotePassword;
-	
-	
+
 	public String getNicAddress() {
 		return nicAddress;
 	}
-	public void setNicAddress(String nicAddress) {
+
+	public void setNicAddress(final String nicAddress) {
 		this.nicAddress = nicAddress;
 	}
+
 	public String getLookupGroups() {
 		return lookupGroups;
 	}
-	public void setLookupGroups(String lookupGroups) {
+
+	public void setLookupGroups(final String lookupGroups) {
 		this.lookupGroups = lookupGroups;
 	}
+
 	public String getLookupLocators() {
 		return lookupLocators;
 	}
-	public void setLookupLocators(String lookupLocators) {
+
+	public void setLookupLocators(final String lookupLocators) {
 		this.lookupLocators = lookupLocators;
 	}
+
 	public String getClassName() {
 		return className;
 	}
-	public void setClassName(String className) {
+
+	public void setClassName(final String className) {
 		this.className = className;
 	}
+
 	public String getManagementMachineTemplate() {
 		return managementMachineTemplate;
 	}
-	public void setManagementMachineTemplate(String managementMachineTemplate) {
+
+	public void setManagementMachineTemplate(final String managementMachineTemplate) {
 		this.managementMachineTemplate = managementMachineTemplate;
 	}
 
 	public boolean isBootstrapManagementOnPublicIp() {
 		return bootstrapManagementOnPublicIp;
 	}
-	public void setBootstrapManagementOnPublicIp(
-			boolean bootstrapManagementOnPublicIp) {
+
+	public void setBootstrapManagementOnPublicIp(final boolean bootstrapManagementOnPublicIp) {
 		this.bootstrapManagementOnPublicIp = bootstrapManagementOnPublicIp;
 	}
+
 	public boolean isConnectToPrivateIp() {
 		return connectToPrivateIp;
 	}
-	public void setConnectToPrivateIp(boolean connectToPrivateIp) {
+
+	public void setConnectToPrivateIp(final boolean connectToPrivateIp) {
 		this.connectToPrivateIp = connectToPrivateIp;
 	}
+
 	public String getRemoteUsername() {
 		return remoteUsername;
 	}
-	public void setRemoteUsername(String remoteUsername) {
+
+	public void setRemoteUsername(final String remoteUsername) {
 		this.remoteUsername = remoteUsername;
 	}
+
 	public String getRemotePassword() {
 		return remotePassword;
 	}
-	public void setRemotePassword(String remotePassword) {
+
+	public void setRemotePassword(final String remotePassword) {
 		this.remotePassword = remotePassword;
 	}
+
 	@Override
 	public String toString() {
 		return "CloudConfiguration [className=" + className + ", nicAddress=" + nicAddress + ", lookupGroups="
@@ -101,7 +116,4 @@ public class CloudConfiguration {
 				+ ", remotePassword=***]";
 	}
 
-	
-	
-	
 }
--- dsl/src/main/java/org/cloudifysource/dsl/cloud/CloudProvider.java
+++ dsl/src/main/java/org/cloudifysource/dsl/cloud/CloudProvider.java
@@ -20,19 +20,23 @@ import java.util.logging.Level;
 
 import org.cloudifysource.dsl.internal.CloudifyDSLEntity;
 
-
-@CloudifyDSLEntity(name = "provider", clazz = CloudProvider.class, allowInternalNode = true, allowRootNode = false, parent = "cloud")
+@CloudifyDSLEntity(name = "provider", clazz = CloudProvider.class, allowInternalNode = true, allowRootNode = false,
+		parent = "cloud")
 public class CloudProvider {
 
 	private String provider;
 	private String localDirectory;
 	private String remoteDirectory;
 	private String cloudifyUrl;
+	
+	// location of zip file where additional cloudify files are places. They will be copied on top of the cloudify distribution.
+	private String cloudifyOverridesUrl;
+	
 	private String machineNamePrefix;
 
 	private boolean dedicatedManagementMachines = true;
 
-	private List<String> managementOnlyFiles;	
+	private List<String> managementOnlyFiles;
 
 	private String sshLoggingLevel = Level.INFO.toString();
 
@@ -41,99 +45,121 @@ public class CloudProvider {
 	private String managementGroup;
 	private int numberOfManagementMachines;
 	private int reservedMemoryCapacityPerMachineInMB;
-	
+
 	public String getProvider() {
 		return provider;
 	}
-	public void setProvider(String provider) {
+
+	public void setProvider(final String provider) {
 		this.provider = provider;
 	}
+
 	public String getLocalDirectory() {
 		return localDirectory;
 	}
-	public void setLocalDirectory(String localDirectory) {
+
+	public void setLocalDirectory(final String localDirectory) {
 		this.localDirectory = localDirectory;
 	}
+
 	public String getRemoteDirectory() {
 		return remoteDirectory;
 	}
-	public void setRemoteDirectory(String remoteDirectory) {
+
+	public void setRemoteDirectory(final String remoteDirectory) {
 		this.remoteDirectory = remoteDirectory;
 	}
+
 	public String getCloudifyUrl() {
 		return cloudifyUrl;
 	}
-	public void setCloudifyUrl(String cloudifyUrl) {
+
+	public void setCloudifyUrl(final String cloudifyUrl) {
 		this.cloudifyUrl = cloudifyUrl;
 	}
+
 	public String getMachineNamePrefix() {
 		return machineNamePrefix;
 	}
-	public void setMachineNamePrefix(String machineNamePrefix) {
+
+	public void setMachineNamePrefix(final String machineNamePrefix) {
 		this.machineNamePrefix = machineNamePrefix;
 	}
-	
+
 	public boolean isDedicatedManagementMachines() {
 		return dedicatedManagementMachines;
 	}
-	public void setDedicatedManagementMachines(boolean dedicatedManagementMachines) {
+
+	public void setDedicatedManagementMachines(final boolean dedicatedManagementMachines) {
 		this.dedicatedManagementMachines = dedicatedManagementMachines;
 	}
+
 	public List<String> getManagementOnlyFiles() {
 		return managementOnlyFiles;
 	}
-	public void setManagementOnlyFiles(List<String> managementOnlyFiles) {
+
+	public void setManagementOnlyFiles(final List<String> managementOnlyFiles) {
 		this.managementOnlyFiles = managementOnlyFiles;
 	}
-	
+
 	public String getSshLoggingLevel() {
 		return sshLoggingLevel;
 	}
-	public void setSshLoggingLevel(String sshLoggingLevel) {
+
+	public void setSshLoggingLevel(final String sshLoggingLevel) {
 		this.sshLoggingLevel = sshLoggingLevel;
 	}
+
 	public List<String> getZones() {
 		return zones;
 	}
-	public void setZones(List<String> zones) {
+
+	public void setZones(final List<String> zones) {
 		this.zones = zones;
 	}
+
 	// TODO - move to configuration
 	public String getManagementGroup() {
 		return managementGroup;
 	}
-	public void setManagementGroup(String managementGroup) {
+
+	public void setManagementGroup(final String managementGroup) {
 		this.managementGroup = managementGroup;
 	}
+
 	// TODO - move to configuration
 	public int getNumberOfManagementMachines() {
 		return numberOfManagementMachines;
 	}
-	public void setNumberOfManagementMachines(int numberOfManagementMachines) {
+
+	public void setNumberOfManagementMachines(final int numberOfManagementMachines) {
 		this.numberOfManagementMachines = numberOfManagementMachines;
 	}
+
 	public int getReservedMemoryCapacityPerMachineInMB() {
 		return reservedMemoryCapacityPerMachineInMB;
 	}
-	public void setReservedMemoryCapacityPerMachineInMB(
-			int reservedMemoryCapacityPerMachineInMB) {
+
+	public void setReservedMemoryCapacityPerMachineInMB(final int reservedMemoryCapacityPerMachineInMB) {
 		this.reservedMemoryCapacityPerMachineInMB = reservedMemoryCapacityPerMachineInMB;
 	}
+
 	@Override
 	public String toString() {
-		return "CloudProvider [provider=" + provider + ", localDirectory="
-				+ localDirectory + ", remoteDirectory=" + remoteDirectory
-				+ ", cloudifyUrl=" + cloudifyUrl + ", machineNamePrefix="
-				+ machineNamePrefix  
-				+ ", dedicatedManagementMachines="
-				+ dedicatedManagementMachines + ", managementOnlyFiles="
-				+ managementOnlyFiles + ",  sshLoggingLevel=" + sshLoggingLevel
-				+ ", zones=" + zones + ", managementGroup=" + managementGroup
-				+ ", numberOfManagementMachines=" + numberOfManagementMachines
-				+ ", reservedMemoryCapacityPerMachineInMB="
-				+ reservedMemoryCapacityPerMachineInMB + "]";
+		return "CloudProvider [provider=" + provider + ", localDirectory=" + localDirectory + ", remoteDirectory="
+				+ remoteDirectory + ", cloudifyUrl=" + cloudifyUrl + ", machineNamePrefix=" + machineNamePrefix
+				+ ", dedicatedManagementMachines=" + dedicatedManagementMachines + ", managementOnlyFiles="
+				+ managementOnlyFiles + ",  sshLoggingLevel=" + sshLoggingLevel + ", zones=" + zones
+				+ ", managementGroup=" + managementGroup + ", numberOfManagementMachines=" + numberOfManagementMachines
+				+ ", reservedMemoryCapacityPerMachineInMB=" + reservedMemoryCapacityPerMachineInMB + "]";
 	}
-	
-	
-	
+
+	public String getCloudifyOverridesUrl() {
+		return cloudifyOverridesUrl;
+	}
+
+	public void setCloudifyOverridesUrl(String cloudifyOverridesUrl) {
+		this.cloudifyOverridesUrl = cloudifyOverridesUrl;
+	}
+
 }
