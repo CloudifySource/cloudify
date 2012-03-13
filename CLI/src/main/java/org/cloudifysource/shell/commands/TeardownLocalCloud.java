5198541763cacbbef283a0f516c942fa9f24ea83
adaml <adaml@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-03-13 15:16:47 +0000

    CLOUDIFY-574 added verification question before teardown
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@112409 eb64e737-3616-4df0-8941-5ee2ae88103d

--- CLI/src/main/java/org/cloudifysource/shell/commands/TeardownLocalCloud.java
+++ CLI/src/main/java/org/cloudifysource/shell/commands/TeardownLocalCloud.java
@@ -15,10 +15,13 @@
  *******************************************************************************/
 package org.cloudifysource.shell.commands;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.felix.gogo.commands.Command;
 import org.apache.felix.gogo.commands.Option;
+import org.apache.karaf.util.Properties.PropertiesReader;
 import org.cloudifysource.shell.AdminFacade;
 import org.cloudifysource.shell.Constants;
 import org.cloudifysource.shell.installer.LocalhostGridAgentBootstrapper;
@@ -42,7 +45,9 @@ import org.cloudifysource.shell.installer.LocalhostGridAgentBootstrapper;
 @Command(scope = "cloudify", name = "teardown-localcloud", description = "Tears down the Local Cloud installed"
 		+ " on the local machine.")
 public class TeardownLocalCloud extends AbstractGSCommand {
-
+	
+	private static final int DEFAULT_PROGRESS_INTERVAL = 2;
+	
 	@Option(required = false, name = "-lookup-groups", description = "A unique name that is used to group together"
 			+ " Cloudify components. The default localcloud lookup group is '"
 			+ LocalhostGridAgentBootstrapper.LOCALCLOUD_LOOKUPGROUP
@@ -68,15 +73,37 @@ public class TeardownLocalCloud extends AbstractGSCommand {
 	@Override
 	protected Object doExecute() throws Exception {
 
+		if (!askUninstallConfirmationQuestion()){
+			return getFormattedMessage("teardown_aborted");
+		}
+		
 		final LocalhostGridAgentBootstrapper installer = new LocalhostGridAgentBootstrapper();
 		installer.setVerbose(verbose);
 		installer.setLookupGroups(lookupGroups);
 		installer.setNicAddress(nicAddress);
-		installer.setProgressInSeconds(10);
+		installer.setProgressInSeconds(DEFAULT_PROGRESS_INTERVAL);
 		installer.setForce(force);
+//		installer.addListener(new CLILocalhostBootstrapperListener());
 		installer.setAdminFacade((AdminFacade) session.get(Constants.ADMIN_FACADE));
 
 		installer.teardownLocalCloudOnLocalhostAndWait(timeoutInMinutes, TimeUnit.MINUTES);
 		return getFormattedMessage("agent_terminated_successfully");
+	}
+	
+	private boolean askUninstallConfirmationQuestion() throws IOException {
+
+		// we skip question if the shell is running a script.
+		if ((Boolean) session.get(Constants.INTERACTIVE_MODE)) {
+			final String confirmationQuestion = getFormattedMessage("teardown_confirmation_question");
+			System.out.print(confirmationQuestion);
+			System.out.flush();
+			final PropertiesReader pr = new PropertiesReader(new InputStreamReader(System.in));
+			final String readLine = pr.readProperty();
+			System.out.println();
+			System.out.flush();
+			return "y".equalsIgnoreCase(readLine);
+		}
+		// Shell is running in nonInteractive mode. we skip the question.
+		return true;
 	}
 }
