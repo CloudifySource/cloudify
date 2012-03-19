321363671543648162860cedb29ba55cbd4b778f
opencm <opencm@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-03-19 12:17:28 +0000

    Update cloudify url
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@112994 eb64e737-3616-4df0-8941-5ee2ae88103d

--- esc/src/main/resources/clouds/byon/byon-cloud.groovy
+++ esc/src/main/resources/clouds/byon/byon-cloud.groovy
@@ -14,7 +14,7 @@ cloud {
 		provider "byon"
 		localDirectory "tools/cli/plugins/esc/byon/upload"
 		remoteDirectory "/tmp/gs-files"
-		cloudifyUrl "http://repository.cloudifysource.org/org/cloudifysource/2.1.0/gigaspaces-cloudify-2.1.0-m4-b1194-6.zip"
+		cloudifyUrl "http://repository.cloudifysource.org/org/cloudifysource/2.1.0/gigaspaces-cloudify-2.1.0-m4-b1194-8.zip"
 		machineNamePrefix "cloudify_agent_"
 		
 		dedicatedManagementMachines true
