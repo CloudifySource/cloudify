f13b71a189475608a8cc0ee77b06795a972dd478
barakme <barakme@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-03-20 12:37:35 +0000

    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@113128 eb64e737-3616-4df0-8941-5ee2ae88103d

--- esc/src/main/resources/clouds/byon/byon-cloud.groovy
+++ esc/src/main/resources/clouds/byon/byon-cloud.groovy
@@ -3,7 +3,7 @@ cloud {
 	name = "byon"
 	configuration {
 		className "org.cloudifysource.esc.driver.provisioning.byon.ByonProvisioningDriver"
-		managementMachineTemplate "SMALL_LINUX_32"
+		managementMachineTemplate "SMALL_LINUX"
 		connectToPrivateIp true
 		bootstrapManagementOnPublicIp false
 		remoteUsername "ENTER_CLOUD_USER"
@@ -15,35 +15,24 @@ cloud {
 		localDirectory "tools/cli/plugins/esc/byon/upload"
 		remoteDirectory "/tmp/gs-files"
 		cloudifyUrl "http://repository.cloudifysource.org/org/cloudifysource/2.1.0/gigaspaces-cloudify-2.1.0-m4-b1194-24.zip"
-		machineNamePrefix "cloudify_agent_"
 		
 		dedicatedManagementMachines true
 		managementOnlyFiles ([])
 		
 
 		sshLoggingLevel "WARNING"
-		managementGroup "cloudify_managemet"
 		numberOfManagementMachines 1
 		zones (["agent"])
 		reservedMemoryCapacityPerMachineInMB 1024
 	}
 	
 	user {
-		user "test"
-		apiKey "test"
 		keyFile ""
 	}
 	
 	templates ([
-				SMALL_LINUX_32 : template{
-					imageId "us-east-1/ami-76f0061f"
+				SMALL_LINUX : template{
 					machineMemoryMB 1600
-					hardwareId "m1.small"
-					locationId "us-east-1"
-					options ([
-						"securityGroups" : ["default"] as String[],
-						"keyPair" : "cloud-demo"
-					])
 					custom ([
 						"nodesList" : ([
 										([
