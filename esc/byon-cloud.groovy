2b31ff0880d9202102dac5453c842332f5462bf6
noak <noak@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-04-03 12:29:09 +0000

    CLOUDIFY-626 documented and added a setting for deleting the files on teardown.
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@114728 eb64e737-3616-4df0-8941-5ee2ae88103d

--- esc/byon-cloud.groovy
+++ esc/byon-cloud.groovy
@@ -1,45 +1,83 @@
-
+/***************
+ * Cloud configuration file for the Bring-Your-Own-Node (BYON) cloud.
+ * See org.cloudifysource.dsl.cloud.Cloud for more details.
+ *
+ * @author noak
+ *
+ */
+ 
 cloud {
+	// Mandatory. The name of the cloud, as it will appear in the Cloudify UI.
 	name = "byon"
+	
+	/********
+	 * General configuration information about the cloud driver implementation.
+	 */
 	configuration {
+		// The cloud-driver implementation class.
 		className "org.cloudifysource.esc.driver.provisioning.byon.ByonProvisioningDriver"
+		// Optional. The template name for the management machines. Defaults to the first template in the templates section below.
 		managementMachineTemplate "SMALL_LINUX"
+		// Optional. Indicates whether internal cluster communications should use the machine private IP. Defaults to true.
 		connectToPrivateIp true
+		//Indicates whether communications with the management servers should use the machine private IP.
 		bootstrapManagementOnPublicIp false
+		// Optional. Cloud-generic credentials. Can be overridden by specific credentials on each node, in the templates section.
 		remoteUsername "ENTER_CLOUD_USER"
 		remotePassword "ENTER_CLOUD_PASSWORD"
 	}
 
+	/*************
+	 * Provider specific information.
+	 */
 	provider {
+		// Mandatory. The name of the provider.
 		provider "byon"
-		localDirectory "tools/cli/plugins/esc/byon/upload"
-		remoteDirectory "/tmp/gs-files"
-		cloudifyUrl "http://repository.cloudifysource.org/org/cloudifysource/2.1.0/gigaspaces-cloudify-2.1.0-m4-b1194-24.zip"
-		machineNamePrefix "cloudify_agent_"
 		
+		// Mandatory. All files from this LOCAL directory will be copied to the remote machine directory.  
+		localDirectory "tools/cli/plugins/esc/byon/upload"
+		// Mandatory. Files from the local directory will be copied to this directory on the remote machine. 
+		remoteDirectory "/tmp/gs-files"
+		// Mandatory. The HTTP/S URL where cloudify can be downloaded from by newly started machines.
+		cloudifyUrl "http://repository.cloudifysource.org/org/cloudifysource/2.1.0/gigaspaces-cloudify-2.1.0-m4-b1194-181.zip"
+		// Mandatory. The prefix for new machines started for servies.
+		machineNamePrefix "cloudify_agent_"
+		// Optional. Defaults to true. Specifies whether cloudify should try to deploy services on the management machine.
+		// Do not change this unless you know EXACTLY what you are doing.
 		dedicatedManagementMachines true
 		managementOnlyFiles ([])
 		
-
-		sshLoggingLevel "WARNING"
-		managementGroup "cloudify_managemet"
+		// Optional. Logging level for the intenal cloud provider logger. Defaults to INFO.
+		sshLoggingLevel "INFO"
+		// Mandatory. Name of the new machine/s started as cloudify management machines. 
+		managementGroup "cloudify_manager"
+		// Mandatory. Number of management machines to start on bootstrap-cloud. In production, should be 2. Can be 1 for dev.
 		numberOfManagementMachines 1
 		zones (["agent"])
 		reservedMemoryCapacityPerMachineInMB 1024
 	}
 	
+	/*************
+	 * Cloud authentication information
+	 */
 	user {
+		// Optional. Key file used to access the cloud.
 		keyFile ""
 	}
 	
+	/***********
+	 * Cloud machine templates available with this cloud. 
+	 */
 	templates ([
+				// Mandatory. Template Name.
 				SMALL_LINUX : template{
-					machineMemoryMB 1600
-					options ([
-						"securityGroups" : ["default"] as String[],
-						"keyPair" : "cloud-demo"
-					])
+				// Mandatory for BYON.
 					custom ([
+						// Mandatory for BYON. The nodesList custom property lists the nodes that compose this cloud-like environment.
+						// For each node required:
+						// An alias for this node. can be static or use a template with an dynamic-index.
+						// The server's private (local) IP. can be a single IP, a list, a range or CIDR.
+						//Optional - user and password for the node. can be taken from general cloud configuration.
 						"nodesList" : ([
 										([
 											"id" : "byon-pc-lab{0}",
@@ -49,6 +87,13 @@ cloud {
 					])
 				}
 	])
+	
+	/*****************
+	 * Optional. Custom properties used to extend existing drivers or create new ones. 
+	 */
+	 // Optional. Sets whether to delete the remoteDirectory created by the cloud driver, when shutting down.
+	custom ([
+		"cleanGsFilesOnShutdown": "false"
+	])
 
 }
-
