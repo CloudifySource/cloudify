8c97107ade284f0df147ec726e38924ce2b16e65
guy <guy@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-04-08 10:39:42 +0000

    CLOUDIFY-695  Add lightweight version of Pet-Clinc
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@115278 eb64e737-3616-4df0-8941-5ee2ae88103d

---
+++ recipes/src/main/resources/applications/petclinic-simple/mongod/mongod_install.groovy
@@ -0,0 +1,43 @@
+import org.cloudifysource.dsl.context.ServiceContextFactory
+import org.cloudifysource.usm.USMUtils
+
+serviceContext = ServiceContextFactory.getServiceContext()
+
+config = new ConfigSlurper().parse(new File("mongod-service.properties").toURL())
+osConfig = USMUtils.isWindows() ? config.win32 : config.unix
+
+
+
+instanceID = serviceContext.getInstanceId()
+
+installDir = System.properties["user.home"]+ "/.cloudify/mongod" + instanceID
+
+
+home = "${serviceContext.serviceDirectory}/mongodb-${config.version}"
+
+
+serviceContext.attributes.thisInstance["home"] = "${home}"
+println "mongod_install.groovy: mongod(${instanceID}) home is ${home}"
+
+serviceContext.attributes.thisInstance["script"] = "${home}/bin/mongod"
+println "mongod_install.groovy: mongod(${instanceID}) script is ${home}/bin/mongod"
+
+serviceContext.attributes.thisInstance["port"] = config.port
+
+builder = new AntBuilder()
+builder.sequential {
+	mkdir(dir:"${installDir}")
+	get(src:"${osConfig.downloadPath}", dest:"${installDir}/${osConfig.zipName}", skipexisting:true)
+}
+
+if(USMUtils.isWindows()) {
+	builder.unzip(src:"${installDir}/${osConfig.zipName}", dest:"${installDir}", overwrite:true)
+} else {
+	builder.untar(src:"${installDir}/${osConfig.zipName}", dest:"${installDir}", compression:"gzip", overwrite:true)
+	builder.chmod(dir:"${installDir}/${osConfig.name}/bin", perm:'+x', includes:"*")
+}
+
+println "mongod_install.groovy: mongod(${instanceID}) moving ${installDir}/${osConfig.name} to ${home}..."
+builder.move(file:"${installDir}/${osConfig.name}", tofile:"${home}")
+
+println "mongod_install.groovy: mongod(${instanceID}) ended"
---
+++ recipes/src/main/resources/applications/petclinic-simple/tomcat/updateWarFile.groovy
@@ -0,0 +1,28 @@
+import org.cloudifysource.dsl.context.ServiceContextFactory
+
+println "updateWarFile.groovy: Starting..."
+
+context = ServiceContextFactory.getServiceContext()
+config  = new ConfigSlurper().parse(new File("${context.serviceDirectory}/tomcat.properties").toURL())
+
+newWarFile=context.attributes.thisInstance["warUrl"] 
+println "updateWarFile.groovy: newWarFile is ${newWarFile}"
+
+home = context.attributes.thisInstance["home"]
+webApps="${home}/webapps"
+destWarFile="${webApps}/${config.warName}"
+println "updateWarFile.groovy: destWarFile is ${destWarFile}"
+
+new AntBuilder().sequential {
+	
+	echo(message:"updateWarFile.groovy: Getting ${newWarFile} ...")
+	get(src:"${newWarFile}", dest:"${config.applicationWar}", skipexisting:false)
+	
+	echo(message:"updateWarFile.groovy: Copying ${config.applicationWar} to ${webApps}...")
+	copy(todir: "${webApps}", file:"${config.applicationWar}", overwrite:true)	
+}
+
+println "updateWarFile.groovy: End"
+return true
+
+
