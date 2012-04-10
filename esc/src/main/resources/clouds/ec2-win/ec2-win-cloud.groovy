ab0226a360425cdd0b18761e5bb7e23911a8afc8
opencm <opencm@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-04-10 03:47:13 +0000

    Update cloudify url
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@115514 eb64e737-3616-4df0-8941-5ee2ae88103d

--- esc/src/main/resources/clouds/ec2-win/ec2-win-cloud.groovy
+++ esc/src/main/resources/clouds/ec2-win/ec2-win-cloud.groovy
@@ -34,7 +34,7 @@ cloud {
 		// Mandatory. Files from the local directory will be copied to this directory on the remote machine. 
 		remoteDirectory "/C\$/Users/Administrator/gs-files"
 		// Mandatory. The HTTP/S URL where cloudify can be downloaded from by newly started machines.
-		cloudifyUrl "http://repository.cloudifysource.org/org/cloudifysource/2.1.0/gigaspaces-cloudify-2.1.0-m4-b1195-115.zip"
+		cloudifyUrl "http://repository.cloudifysource.org/org/cloudifysource/2.1.0/gigaspaces-cloudify-2.1.0-m4-b1195-116.zip"
 		// Mandatory. The prefix for new machines started for servies.
 		machineNamePrefix "cloudify_agent_"
 		// Optional. Defaults to true. Specifies whether cloudify should try to deploy services on the management machine.
