4a4db26e78449cc209e30e570f36cc2133db15d8
itaif <itaif@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-04-15 11:40:06 +0000

    CLOUDIFY-660 Added documentation to scalingRules recipe
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@115955 eb64e737-3616-4df0-8941-5ee2ae88103d

--- recipes/src/main/resources/applications/petclinic-simple-scalingRules/tomcat/tomcat-service.groovy
+++ recipes/src/main/resources/applications/petclinic-simple-scalingRules/tomcat/tomcat-service.groovy
@@ -184,6 +184,7 @@ service {
 		]
 		)
 	}
+	
 	// global flag that enables changing number of instances for this service
 	elastic true
 
@@ -217,22 +218,49 @@ service {
 
 			serviceStatistics {
 
+        // The name of the metric that is the basis for the scale rule decision
 				metric "Total Requests Count"
 
+        // (Optional)
+        // The sliding time range (in seconds) for aggregating per-instance metric samples
+        // The number of samples in the time windows equals the time window divided by the sampling period
+        // Default: 300
 				movingTimeRangeInSeconds 20
 
+        // (Optional)
+        // The algorithms for aggregating metric samples by instances and by time.
+        // Metric samples are aggregated separately per instance in the specified time range,
+        // and then aggregated again for all instances.
+        // Default: Statistics.averageOfAverages
+        // Possible values: Statistics.maximumOfAverages, Statistics.minimumOfAverages, Statistics.averageOfAverages, Statistics.percentileOfAverages(90)
+        //                  Statistics.maximumOfMaximums, Statistics.minimumOfMinimums, Statistics.maximumThroughput
+        //
+        // This has the same effect as setting instancesStatistics and timeStatistics separately. 
+        // For example: 
+        // statistics Statistics.maximumOfAverages
+        // is the same as:
+        // timeStatistics Statistics.average
+        // instancesStatistics Statistics.maximum
+        // 
 				statistics Statistics.maximumThroughput
 			}
 
 
-			// The instancesStatistics over which the number of instances is increased or decreased
 			highThreshold {
+			
+        // The value above which the number of instances is increased			
 				value 1
+				
+				// The number of instances to increase when above threshold
 				instancesIncrease 1
 			}
 
 			lowThreshold {
+			
+        // The value below which the number of instances is decreased
 				value 0.2
+				
+				// The number of instances to decrease when below threshold
 				instancesDecrease 1
 			}
 		}
