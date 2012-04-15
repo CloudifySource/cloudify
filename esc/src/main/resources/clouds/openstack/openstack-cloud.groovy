e1e9292e594af00e8fafe99bcbf0061995ecf7c4
opencm <opencm@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-04-15 14:01:34 +0000

    Update cloudify url
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@115968 eb64e737-3616-4df0-8941-5ee2ae88103d

--- esc/src/main/resources/clouds/openstack/openstack-cloud.groovy
+++ esc/src/main/resources/clouds/openstack/openstack-cloud.groovy
@@ -16,7 +16,7 @@ cloud {
 		provider "openstack"
 		localDirectory "tools/cli/plugins/esc/openstack/upload"
 		remoteDirectory "/root/gs-files"
-		cloudifyUrl "http://repository.cloudifysource.org/org/cloudifysource/2.1.0/gigaspaces-cloudify-2.1.0-rc-b1195-218.zip" 
+		cloudifyUrl "http://repository.cloudifysource.org/org/cloudifysource/2.1.0/gigaspaces-cloudify-2.1.0-rc-b1195-222.zip" 
 		machineNamePrefix "cloudify_agent_"
 		
 		dedicatedManagementMachines true
