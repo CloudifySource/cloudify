0760588458f86557869e6b8c5d11e23317b95d43
tamir <tamir@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-04-19 22:51:55 +0000

    Removed old code
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@116569 eb64e737-3616-4df0-8941-5ee2ae88103d

--- recipes/src/main/resources/recipes/services/mongodb/mongos/mongos_poststart.groovy
+++ recipes/src/main/resources/recipes/services/mongodb/mongos/mongos_poststart.groovy
@@ -1,60 +1,54 @@
-/*******************************************************************************
-* Copyright (c) 2011 GigaSpaces Technologies Ltd. All rights reserved
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*       http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*******************************************************************************/
 @Grab(group='com.gmongo', module='gmongo', version='0.8')
 import com.gmongo.GMongo
 
 import org.cloudifysource.dsl.context.ServiceContextFactory
 import java.util.concurrent.TimeUnit
 
-config = new ConfigSlurper().parse(new File('mongos.properties').toURL())
+config = new ConfigSlurper().parse(new File('mongos-service.properties').toURL())
 
-println "sleeping for 10 secs"
+println "mongos_poststart.groovy: sleeping for 60 secs..."
+sleep(60)
 
 serviceContext = ServiceContextFactory.getServiceContext()
 
+println "mongos_poststart.groovy: serviceContext object is " + serviceContext
+
 //waiting for mongod service to become available, will not be needed in one of the upcoming builds 
-println "waiting for mongod"
+println "mongos_poststart.groovy: Waiting for mongod..."
 mongodService = serviceContext.waitForService("mongod", 20, TimeUnit.SECONDS) 
 mongodHostInstances = mongodService.waitForInstances(mongodService.numberOfPlannedInstances, 60, TimeUnit.SECONDS) 
 
-println "Connecting to mongos on port ${config.port}"
-try {
-    //check connection 
-	mongo = new GMongo("127.0.0.1", config.port)
-	db = mongo.getDB("admin")
-	assert db != null 	
-    println "Connection succeeded"	
+println "mongos_poststart.groovy: mongodHostInstances length is "+mongodHostInstances.length
 
-	mongodHostInstances.each {
-		mongodPort = config.mongodBasePort + it.instanceID
-		mongodHost = it.hostAddress		
-		println "mongod #"+it.instanceID + " host and port = ${mongodHost}:${mongodPort}"		
-		result = db.command(["addshard":"${mongodHost}:${mongodPort}"])		
-		println result
-	}	
-    
-	result = db.command(["enablesharding":"petclinic"])	
-	println result 	
+intPort = serviceContext.attributes.thisInstance["port"] as int
+println "mongos_poststart.groovy: Connecting to mongos on port ${intPort} ..."
+
+mongo = new GMongo("127.0.0.1", intPort)
 	
-	result = db.command(["shardcollection":"petclinic.Person", "key":["_id":1]])
-	println result 	
-} catch (Exception e) {
-    println "Connection Failed!"
-	throw e; 
+println "mongos_poststart.groovy: After new GMongo port ${intPort} ..."
+	
+db = mongo.getDB("admin")
+assert db != null 	
+println "Connection succeeded"	
+	
+	
+mongodInstances=serviceContext.attributes.mongod.instances		
+	
+mongodHostInstances.each {
+		mongodPort = mongodInstances[it.instanceID].port		
+		mongodHost = it.hostAddress		
+		println "mongos_poststart.groovy: mongod #"+it.instanceID + " host and port = ${mongodHost}:${mongodPort}"		
+		result = db.command(["addshard":"${mongodHost}:${mongodPort}"])		
+		println "mongos_poststart.groovy: db result: ${result}"
 }
+    
+	
+result = db.command(["enablesharding":"petclinic"])	
+println "mongos_poststart.groovy: db result: ${result}"
+	
+result = db.command(["shardcollection":"petclinic.Person", "key":["_id":1]])
+println "mongos_poststart.groovy: db result: ${result}"
+	
 
  
 
