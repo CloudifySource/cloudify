66e2073cb73749191ce6659678c877285d03ba21
tamir <tamir@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-04-19 22:04:41 +0000

    added port as int
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@116550 eb64e737-3616-4df0-8941-5ee2ae88103d

--- recipes/src/main/resources/recipes/apps/petclinic/mongod/mongod-service.groovy
+++ recipes/src/main/resources/recipes/apps/petclinic/mongod/mongod-service.groovy
@@ -22,6 +22,7 @@ service {
 
 		monitors{
 			try { 
+				port  = context.attributes.thisInstance["port"] as int
 				mongo = new Mongo("127.0.0.1", port)			
 				db = mongo.getDB("mydb")
 														
@@ -36,15 +37,15 @@ service {
 					"Current Active Connections":result.connections.current,
 					"Open Cursors":result.cursors.totalOpen
 				]
-			}
+			}			
 			finally {
-				if (mongo != null) 
-					mongo.close()
+				if (null!=mongo) mongo.close()
 			}
+			
 			
 		}
 	}
-		
+
 	userInterface {
 		metricGroups = ([
 			metricGroup {
