ec1b49a4a3ef9583df0d2ca74ae76db87f203598
tamir <tamir@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-04-19 22:50:03 +0000

    Using mongod-service.properties and moved chmod
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@116567 eb64e737-3616-4df0-8941-5ee2ae88103d

--- recipes/src/main/resources/recipes/services/mongodb/mongod/mongod_install.groovy
+++ recipes/src/main/resources/recipes/services/mongodb/mongod/mongod_install.groovy
@@ -1,55 +1,56 @@
-/*******************************************************************************
-* Copyright (c) 2011 GigaSpaces Technologies Ltd. All rights reserved
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*       http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*******************************************************************************/
 import org.cloudifysource.dsl.utils.ServiceUtils;
-
 import org.cloudifysource.dsl.context.ServiceContextFactory
 
 
-println("Checking operating system's compatibility with mongod")
-if (!ServiceUtils.isWindows()){
-	String command = "uname -a"
-	Process pr = command.execute()
-	pr.waitFor();
-	if (pr.in.text.contains("2007")){
-		throw new Exception("The Linux legacy-static builds are needed for older systems." 
-			+ "Try and run mongod from the commandline and if you get a floating point exception, use a legacy-static build.");  
-	}
-}
-println("op system is compatible with mongod")
-
 serviceContext = ServiceContextFactory.getServiceContext()
-instanceIdFile = new File("./instanceId.txt")
-instanceIdFile.text = serviceContext.instanceId
 
-config = new ConfigSlurper().parse(new File("mongod.properties").toURL())
-osConfig = ServiceUtils.isWindows() ? config.win32 : config.linux
+config = new ConfigSlurper().parse(new File("mongod-service.properties").toURL())
+osConfig = ServiceUtils.isWindows() ? config.win32 : config.unix
 
-portFile = new File("./port.txt")
-portFile.text = config.port
+
+
+instanceID = serviceContext.getInstanceId()
+
+installDir = System.properties["user.home"]+ "/.cloudify/${config.serviceName}" + instanceID
+
+
+home = "${serviceContext.serviceDirectory}/mongodb-${config.version}"
+
+
+serviceContext.attributes.thisInstance["home"] = "${home}"
+println "mongod_install.groovy: mongod(${instanceID}) home is ${home}"
+
+serviceContext.attributes.thisInstance["script"] = "${home}/bin/mongod"
+println "mongod_install.groovy: mongod(${instanceID}) script is ${home}/bin/mongod"
+
+currPort=config.basePort
+if (serviceContext.isLocalCloud()) {
+	currPort+=instanceID
+}
+
+serviceContext.attributes.thisInstance["port"] = currPort
+
+println "mongod_install.groovy: mongod(${instanceID}) port ${currPort}"
+
 
 builder = new AntBuilder()
 builder.sequential {
-	mkdir(dir:config.installDir)
-	get(src:osConfig.downloadPath, dest:"${config.installDir}/${osConfig.zipName}", skipexisting:true)
+	mkdir(dir:"${installDir}")
+	get(src:"${osConfig.downloadPath}", dest:"${installDir}/${osConfig.zipName}", skipexisting:true)
 }
 
 if(ServiceUtils.isWindows()) {
-	builder.unzip(src:"${config.installDir}/${osConfig.zipName}", dest:config.installDir, overwrite:true)
+	builder.unzip(src:"${installDir}/${osConfig.zipName}", dest:"${installDir}", overwrite:true)
 } else {
-	builder.untar(src:"${config.installDir}/${osConfig.zipName}", dest:config.installDir, compression:"gzip", overwrite:true)
-	builder.chmod(dir:"${config.installDir}/${osConfig.name}/bin", perm:'+x', includes:"*")
+	builder.untar(src:"${installDir}/${osConfig.zipName}", dest:"${installDir}", compression:"gzip", overwrite:true)
 }
-builder.move(file:"${config.installDir}/${osConfig.name}", tofile:config.home)
+
+println "mongod_install.groovy: mongod(${instanceID}) moving ${installDir}/${osConfig.name} to ${home}..."
+builder.move(file:"${installDir}/${osConfig.name}", tofile:"${home}")
+
+if(!ServiceUtils.isWindows()) {
+	println "calling chmod on ${home}/bin"
+	builder.chmod(dir:"${home}/bin", perm:'+x', includes:"*")
+}
+
+println "mongod_install.groovy: mongod(${instanceID}) ended"
