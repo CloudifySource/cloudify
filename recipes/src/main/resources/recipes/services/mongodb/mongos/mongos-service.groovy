7e1965f193e051fd608e05243c24dc5c82f3759e
tamir <tamir@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-04-19 22:48:52 +0000

    Using monitors instead of plugins
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@116566 eb64e737-3616-4df0-8941-5ee2ae88103d

--- recipes/src/main/resources/recipes/services/mongodb/mongos/mongos-service.groovy
+++ recipes/src/main/resources/recipes/services/mongodb/mongos/mongos-service.groovy
@@ -1,56 +1,45 @@
-/*******************************************************************************
-* Copyright (c) 2011 GigaSpaces Technologies Ltd. All rights reserved
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*       http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*******************************************************************************/
+import com.mongodb.CommandResult;
+import com.mongodb.Mongo;
+import com.mongodb.DB;
 service {
 	
 	name "mongos"
 	icon "mongodb.png"
 	type "NOSQL_DB"
 	numInstances 1
+	
+	compute {
+		template "SMALL_LINUX"
+	}
 
 	lifecycle {
-		init "mongos_install.groovy"
+		install "mongos_install.groovy"
 		start "mongos_start.groovy"		
 		postStart "mongos_poststart.groovy"
+		startDetectionTimeoutSecs 240
+		startDetection {
+			ServiceUtils.isPortOccupied(context.attributes.thisInstance["port"])
+		}
+		
+		monitors{
+			try { 
+				port  = context.attributes.thisInstance["port"] as int
+				mongo = new Mongo("127.0.0.1", port)			
+				db = mongo.getDB("mydb")
+														
+				result = db.command("serverStatus")
+				println "mongod-service.groovy: result is ${result}"	
+														
+				return [
+					"Current Active Connections":result.connections.current					
+				]
+			}			
+			finally {
+				if (null!=mongo) mongo.close()
+			}					
+		}		
 	}
 	
-	plugins([		
-        plugin {
-            name "portLiveness"
-            className "org.cloudifysource.mongodb.MongoLivenessDetector"
-            config ([                
-				"port" : 30000,
-                "timeoutInSeconds" : 240,
-                "host" : "127.0.0.1"
-            ])
-        }, 
-		plugin {
-			name "MongoDBMonitorsPlugin"
-			className "org.cloudifysource.mongodb.MongoDBMonitorsPlugin"
-			config([				
-				"host":"127.0.0.1",
-				"port" : 30000,
-				"dbName":"mydb", 
-				"dataSpec":([				    
-					"Current Active Connections":"connections.current"					
-				])
-			])
-		}		
-		
-	])
-
 	userInterface {
 		metricGroups = ([
 			metricGroup {
