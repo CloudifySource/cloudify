5139c58a01a6c20a19be16fd0d70ce39336441f9
barakme <barakme@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-04-22 11:27:12 +0000

    CLOUDIFY-23
    MInor fix to client script.
    
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@116718 eb64e737-3616-4df0-8941-5ee2ae88103d

--- esc/src/main/resources/clouds/ec2-win/upload/bootstrap-client.ps1
+++ esc/src/main/resources/clouds/ec2-win/upload/bootstrap-client.ps1
@@ -16,23 +16,9 @@
 
 
 #############################################################################
-# This script starts a Gigaspaces agent for use with the Gigaspaces
-# Cloudify. The agent will function as management depending on the value of $GSA_MODE
-#
-# Example:
-# Invoke-Command -ComputerName ec2-107-21-132-11.compute-1.amazonaws.com -Credential $cred -ScriptBlock {#$ENV:LUS_IP_ADDRESS=10.46.178;$ENV:GSA_MODE=lus;$ENV:NO_WEB_SERVICES=false;$ENV:MACHINE_IP_ADDRESS=10.46.178.72;$ENV:MACHINE_ZONES="ZONE";$ENV:WORKING_HOME_DIRECTORY="C:\Users\Administrator\Documents";$ENV:CLOUDIFY_LINK="http://repository.cloudifysource.org/org/cloudifysource/2.1.0/gigaspaces-cloudify-2.1.0-m2-b1193-82.zip";$ENV:CLOUD_FILE="ec2-cloud.groovy";.\bootstrap-management.ps1 }
-#  
-#
-# The following environment variables should be set before calling this script:
-# 	$LUS_IP_ADDRESS - Ip of the head node that runs a LUS and ESM. May be my IP. (Required)
-#   $GSA_MODE - 'agent' if this node should join an already running node. Otherwise, any value.
-#	$NO_WEB_SERVICES - 'true' if web-services (rest, webui) should not be deployed (only if GSA_MODE != 'agent')
-#   $MACHINE_IP_ADDRESS - The IP of this server (Useful if multiple NICs exist)
-#	$MACHINE_ZONES - This is required if this is not a management machine
-# 	$WORKING_HOME_DIRECTORY - This is where the files were copied to (cloudify installation, etc..)
-#	$CLOUDIFY_LINK - If this url is found, it will be downloaded to $WORKING_HOME_DIRECTORY/gigaspaces.zip
-#	$CLOUDIFY_OVERRIDES_LINK - If this url is found, it will be downloaded to $WORKING_HOME_DIRECTORY/gigaspaces.zip
-#   $CLOUD_FILE - File name of the cloud file, which should be placed in the WORKING_HOME_DIRECTORY
+# Remote command invocation script for use with Cloudify.
+# This script will connect to a remote machine, set up the required configuration
+# and then execute the given command.
 #
 # Author: barakm
 # Since: 2.1
@@ -41,7 +27,9 @@
 param ([string]$target, [string]$username, [string]$password, [string]$command)
 
 $ErrorActionPreference="Stop"
-$securePassword = ConvertTo-SecureString -AsPlainText -Force "$password"
+
+# Set up the password
+$securePassword = ConvertTo-SecureString -AsPlainText -Force $password
 $cred = New-Object System.Management.Automation.PSCredential $username, $securePassword
 
 Write-Host "Connecting to management service of $target"
