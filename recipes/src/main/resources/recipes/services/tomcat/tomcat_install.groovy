6ba515a73c487f23a8326ca630d91eee18428097
itaif <itaif@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-04-24 13:27:16 +0000

    Fixed missing import
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@117114 eb64e737-3616-4df0-8941-5ee2ae88103d

--- recipes/src/main/resources/recipes/services/tomcat/tomcat_install.groovy
+++ recipes/src/main/resources/recipes/services/tomcat/tomcat_install.groovy
@@ -13,6 +13,8 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 *******************************************************************************/
+import org.cloudifysource.dsl.context.ServiceContextFactory
+
 config = new ConfigSlurper().parse(new File("tomcat.properties").toURL())
 
 def serviceContext = ServiceContextFactory.getServiceContext()
