435637c5abe1164fac83a840825abce8d20fa7d6
noak <noak@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-05-16 12:52:35 +0000

    added "itemsToClean": ""
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@119080 eb64e737-3616-4df0-8941-5ee2ae88103d

--- esc/src/main/resources/clouds/byon/byon-cloud.groovy
+++ esc/src/main/resources/clouds/byon/byon-cloud.groovy
@@ -97,6 +97,7 @@ cloud {
 	 // Optional. Sets whether to delete the remoteDirectory created by the cloud driver, when shutting down.
 	custom ([
 		"cleanGsFilesOnShutdown": "false"
+		"itemsToClean": ""
 	])
 
 }
