cd1f6124a99ff8abafbea4573a0a5304d9b4a4ee
noak <noak@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-05-17 13:44:02 +0000

    Test for CLOUDIFY-528 (bootstrap-cloud ec2 does not close management machine in case of partial failure).
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@119179 eb64e737-3616-4df0-8941-5ee2ae88103d

---
+++ dsl/src/test/java/org/cloudifysource/dsl/CloudPemValidationTest.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2011 GigaSpaces Technologies Ltd. All rights reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package org.cloudifysource.dsl;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import org.cloudifysource.dsl.internal.ServiceReader;
+import org.junit.Test;
+
+
+public class CloudPemValidationTest {
+
+	private final static String EC2_MISSING_PEM_PATH = "testResources/testparsing/ec2-missing-pem-cloud.groovy";
+	
+	@Test
+	public void testCloudParser() throws Exception {
+		try {
+			org.cloudifysource.dsl.cloud.Cloud cloud = ServiceReader.readCloud(new File(EC2_MISSING_PEM_PATH));
+			//if we got to the next line - the validation exception wwasn't thrown.
+			assertTrue("The key file is not found yet no error was thrown", false);
+		} catch (Throwable e) {
+			assertTrue("The key file is not found yet no error was thrown", 
+					e.getMessage().contains("The specified key file is missing"));
+		}
+	}
+
+
+}
