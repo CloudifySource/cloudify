03196a97002fa5105eab5619b73314974bd8725d
noak <noak@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-05-21 08:45:28 +0000

    Unit test for CLOUDIFY-682 (Wrong configuration of the cloud groovy template name causes null pointer exception).
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@119414 eb64e737-3616-4df0-8941-5ee2ae88103d

---
+++ dsl/src/test/java/org/cloudifysource/dsl/CloudMissingTemplateTest.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * Copyright (c) 2011 GigaSpaces Technologies Ltd. All rights reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package org.cloudifysource.dsl;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import org.cloudifysource.dsl.internal.ServiceReader;
+import org.junit.Test;
+
+
+public class CloudMissingTemplateTest {
+
+	private final static String EC2_MISSING_TEMPLATE_PATH = 
+			"testResources/testparsing/ec2-missing-template-cloud.groovy";
+	
+	@Test
+	public void testCloudParser() throws Exception {
+		try {
+			org.cloudifysource.dsl.cloud.Cloud cloud = ServiceReader.readCloud(new File(EC2_MISSING_TEMPLATE_PATH));
+			//if we got to the next line - the validation exception wasn't thrown.
+			assertTrue("The management template does not exist yet no error was thrown", false);
+		} catch (Throwable e) {
+			assertTrue("The management template does not exist yet no error was thrown", 
+					e.getMessage().contains("is not listed in the cloud's templates section")
+					|| e.getMessage().contains("managementMachineTemplate"));
+		}
+	}
+
+
+}
---
+++ dsl/testResources/testparsing/ec2-missing-template-cloud.groovy
@@ -0,0 +1,46 @@
+
+cloud {
+	name = "ec2"
+	configuration {
+		className "org.cloudifysource.esc.driver.provisioning.jclouds.DefaultProvisioningDriver"
+		managementMachineTemplate "MISSING_TEMPLATE"
+		connectToPrivateIp true
+	}
+
+	provider {
+		provider "aws-ec2"
+		localDirectory "tools/cli/plugins/esc/ec2/upload"
+		cloudifyUrl "http://repository.cloudifysource.org/org/cloudifysource/2.0.1/gigaspaces-cloudify-2.0.1-m1-b1190-13.zip" 
+		machineNamePrefix "NOA_test.cli.cloudify.cloud.MissingPemEc2Testsgtest_cloudify_agent1"
+		
+		dedicatedManagementMachines true
+		managementOnlyFiles ([])
+		
+
+		sshLoggingLevel "WARNING"
+		managementGroup "NOA_test.cli.cloudify.cloud.MissingPemEc2Testsgtest_test.cli.cloudify.cloud.MissingPemEc2Testsgtest_test.cli.cloudify.cloud.MissingPemEc2Testsgtest_test.cli.cloudify.cloud.MissingPemEc2Testsgtest_test.cli.cloudify.cloud.MissingPemEc2Testsgtest_test.cli.cloudify.cloud.CleanGSFilesByonTestsgtest_cloudify_manager_1"
+		numberOfManagementMachines 2
+		zones (["agent"])
+		reservedMemoryCapacityPerMachineInMB 1024
+		
+	}
+	user {
+		user "0VCFNJS3FXHYC7M6Y782"
+		apiKey "fPdu7rYBF0mtdJs1nmzcdA8yA/3kbV20NgInn4NO"
+		keyFile "cloud-demo.pem"
+	}
+	templates ([
+				SMALL_LINUX : template{
+					imageId "us-east-1/ami-76f0061f"
+					machineMemoryMB 1600
+					remoteDirectory "/home/ec2-user/gs-files"
+					hardwareId "m1.small"
+					locationId "us-east-1"
+					options ([
+						"securityGroups" : ["default"] as String[],
+						"keyPair" : "cloud-demo"
+					])
+				}
+			])
+}
+
