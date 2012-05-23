bd6e69ef824c24d5ceda7e6b049df3ec532a2eea
noak <noak@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-05-23 07:10:32 +0000

    added a missing ","
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@119668 eb64e737-3616-4df0-8941-5ee2ae88103d

--- esc/src/main/resources/clouds/byon/byon-cloud.groovy
+++ esc/src/main/resources/clouds/byon/byon-cloud.groovy
@@ -96,7 +96,7 @@ cloud {
 	 */
 	 // Optional. Sets whether to delete the remoteDirectory created by the cloud driver, when shutting down.
 	custom ([
-		"cleanGsFilesOnShutdown": "false"
+		"cleanGsFilesOnShutdown": "false",
 		"itemsToClean": ""
 	])
 
