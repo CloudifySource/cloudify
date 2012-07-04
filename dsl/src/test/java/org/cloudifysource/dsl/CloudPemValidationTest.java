6144f076812812518285332f3cb3c8867ef195eb
noak <noak@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-07-04 11:05:15 +0000

    updated test
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@123083 eb64e737-3616-4df0-8941-5ee2ae88103d

--- dsl/src/test/java/org/cloudifysource/dsl/CloudPemValidationTest.java
+++ dsl/src/test/java/org/cloudifysource/dsl/CloudPemValidationTest.java
@@ -35,7 +35,7 @@ public class CloudPemValidationTest {
 			assertTrue("The key file is not found yet no error was thrown", false);
 		} catch (Throwable e) {
 			assertTrue("The key file is not found yet no error was thrown. Error was: " + e.getMessage(), 
-					e.getMessage().contains("The specified key file is missing"));
+					e.getMessage().contains("The specified key file is missing") || e.getMessage().contains("The specified key file is not found"));
 		}
 	}
 
