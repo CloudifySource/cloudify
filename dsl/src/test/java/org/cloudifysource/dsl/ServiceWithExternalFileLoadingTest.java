329199a82c1bbe0b37fa27db0324954fbdf73f95
barakme <barakme@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-07-08 09:17:15 +0000

    CLOUDIFY-948
    Adding load() method to load DSL elements from external file.
    
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@123297 eb64e737-3616-4df0-8941-5ee2ae88103d

---
+++ dsl/src/test/java/org/cloudifysource/dsl/ServiceWithExternalFileLoadingTest.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2011 GigaSpaces Technologies Ltd. All rights reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package org.cloudifysource.dsl;
+
+import java.io.File;
+
+import junit.framework.Assert;
+
+import org.cloudifysource.dsl.internal.ServiceReader;
+import org.junit.Test;
+
+/******
+ * Tests for parsing of DSL files using the 'load' method to read DSL elements from external files.
+ * 
+ * @author barakme
+ * 
+ */
+public class ServiceWithExternalFileLoadingTest {
+
+	private static final String RECIPE_PATH = "testResources/testparsing/externalLoad/test_loading.groovy";
+
+	@Test
+	public void testCloudParser()
+			throws Exception {
+		Service service = ServiceReader.readService(new File(RECIPE_PATH));
+		Assert.assertNotNull("lifecycle loaded from external file should not be null", service.getLifecycle());
+		Assert.assertNotNull("unexpected field value in lifecycle", service.getLifecycle().getInit());
+		Assert.assertNotNull("unexpected field value in lifecycle", service.getLifecycle().getStart());
+		Assert.assertNotNull("userinterface loaded from external file should not be null", service.getUserInterface());
+		Assert.assertEquals("unexpected value in UI field", 1, service.getUserInterface().getMetricGroups().size());
+		Assert.assertEquals("unexpected value in UI field", 2, service.getUserInterface().getWidgetGroups().size());
+
+	}
+
+}
