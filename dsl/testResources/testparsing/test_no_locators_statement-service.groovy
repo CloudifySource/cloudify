c1df1abae72bb80453a02e65c314e4909c7832a3
barakme <barakme@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-07-10 12:48:53 +0000

    CLOUDIFY-898
    Added NO_PROCESS_LOCATORS constant for use with recipes that want a simple way to specify that no process locators should be used.
    
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@123547 eb64e737-3616-4df0-8941-5ee2ae88103d

---
+++ dsl/testResources/testparsing/test_no_locators_statement-service.groovy
@@ -0,0 +1,29 @@
+
+service {
+
+    name "iisproxy" 
+    numInstances 2
+    maxAllowedInstances 2
+	lifecycle{
+		
+				init "test_parsing_base_install.groovy"
+		
+				start ([ "Linux":"tomcat_start.groovy" ,
+							"Windows.*": "tomcat_start.groovy"
+						])
+		
+				postStart {
+					println "post start"
+				}
+				
+				preStop (["Win.*":"catalina-stop.bat",
+							"Linux":"./catalina-stop.sh"])
+				
+				locator {
+					NO_PROCESS_LOCATORS
+				}
+			}
+    
+    
+	
+}
