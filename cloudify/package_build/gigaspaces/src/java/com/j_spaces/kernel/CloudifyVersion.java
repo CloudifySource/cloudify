0e1efc2330743f1b1c4c7c6b08e9dd4fca68edf5
opencm <opencm@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-07-12 09:09:13 +0000

    803 Build version advanced to 2489-58
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@123699 eb64e737-3616-4df0-8941-5ee2ae88103d

--- cloudify/package_build/gigaspaces/src/java/com/j_spaces/kernel/CloudifyVersion.java
+++ cloudify/package_build/gigaspaces/src/java/com/j_spaces/kernel/CloudifyVersion.java
@@ -34,7 +34,7 @@ public class CloudifyVersion implements ProductVersion {
 	// !!!IMPORTANT, read below
 	// Must be of either "int-int-string", "int-int" or "int" format otherwise
 	// PlatformLogicalVersion will fail parsing!!!
-	private final String BUILD_NUM = "2489-57";
+	private final String BUILD_NUM = "2489-58";
 	private final String V_NAME = "GigaSpaces";
 	private final String PRODUCT_HELP_URL = "http://www.cloudifysource.org/guide";
 
