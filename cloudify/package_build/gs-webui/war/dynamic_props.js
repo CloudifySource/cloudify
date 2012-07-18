a2ff9c3b6094a7fdf753c1c20bad29baa3413f9b
evgeny <evgeny@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-07-18 11:34:58 +0000

    GS-10487 All appearances of grid services in Cloudify have now different name: GSA = Agent LUS = Discovery Service GSC = USM ESM = Orchestrator GSM = Deployer
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@124072 eb64e737-3616-4df0-8941-5ee2ae88103d

--- cloudify/package_build/gs-webui/war/dynamic_props.js
+++ cloudify/package_build/gs-webui/war/dynamic_props.js
@@ -2,5 +2,15 @@ var ServicesDynamicProperties={
 	serviceName:"Service",
 	serviceInstanceName:"Service Instance",
 	servicesName:"Services",
-	serviceInstancesName:"Service Instances"
+	serviceInstancesName:"Service Instances",
+	gsaName:"agent",
+	gsmName:"deployer",
+	gscName:"usm",	
+	esmName:"orchestrator",
+	lusName:"discovery service",
+	gsaInstancesName:"Agents",
+	gsmInstancesName:"Deployers",
+	gscInstancesName:"USMs",
+	esmInstancesName:"Orchestrators",
+	lusInstancesName:"Discovery Services"	
 };
