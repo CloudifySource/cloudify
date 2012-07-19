874f70c80d08d1c2f1639339252ac32ba73edb85
adaml <adaml@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-07-19 09:06:27 +0000

    CLOUDIFY-972 reverted char change
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@124180 eb64e737-3616-4df0-8941-5ee2ae88103d

--- CLI/src/main/java/org/cloudifysource/shell/ShellUtils.java
+++ CLI/src/main/java/org/cloudifysource/shell/ShellUtils.java
@@ -43,6 +43,9 @@ import java.util.logging.Logger;
 public final class ShellUtils {
 
 	protected static final Logger logger = Logger.getLogger(ShellUtils.class.getName());
+	
+	 private static final char WIN_RETURN_CHAR = '\r';
+	 private static final char LINUX_RETURN_CHAR = '\n';
 
     private static final long TWO_WEEKS_IN_MILLIS = 86400000L * 14L;
     private static final File VERSION_CHECK_FILE = new File(System.getProperty("user.home") + "/.karaf/lastVersionCheckTimestamp");
@@ -89,7 +92,7 @@ public final class ShellUtils {
             StringBuilder responseBuffer = new StringBuilder();
             while (true) {
                 responseChar = (char) session.getKeyboard().read();
-                if (responseChar == Character.LINE_SEPARATOR) {
+                if (responseChar == WIN_RETURN_CHAR || responseChar == LINUX_RETURN_CHAR) {
                     session.getConsole().println();
                     break;
                 }
