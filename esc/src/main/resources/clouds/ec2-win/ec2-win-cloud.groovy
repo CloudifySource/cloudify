16cd5daf568cf534a5e08b70ad0d879c22370beb
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-07-30 15:18:26 +0000

    fix windows default ami
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@124913 eb64e737-3616-4df0-8941-5ee2ae88103d

--- esc/src/main/resources/clouds/ec2-win/ec2-win-cloud.groovy
+++ esc/src/main/resources/clouds/ec2-win/ec2-win-cloud.groovy
@@ -80,7 +80,7 @@ cloud {
 	templates ([
 				MEDIUM_WIN : template{
 					// Mandatory. Image ID.
-					imageId "us-east-1/ami-2ccd6e45"
+					imageId "us-east-1/ami-2b41ef42"
 					// Mandatory. Amount of RAM available to machine.
 					machineMemoryMB 1600
 					// Mandatory. Hardware ID.
