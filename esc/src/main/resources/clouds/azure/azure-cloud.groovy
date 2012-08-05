4c55885aaf9fe82f46561492e7b84f1036819e10
elip <elip@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-08-05 15:12:10 +0000

    CLOUDIFY-962 add cloud driver configuration file for azure cloud driver add bootstrap-management.sh script for azure cloud driver
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@125200 eb64e737-3616-4df0-8941-5ee2ae88103d

---
+++ esc/src/main/resources/clouds/azure/azure-cloud.groovy
@@ -0,0 +1,179 @@
+
+cloud {
+
+	// Mandatory. The name of the cloud, as it will appear in the Cloudify UI.
+	name = "Azure"
+	configuration {
+	
+		// Mandatory - Azure IaaS cloud driver.
+		className "org.cloudifysource.esc.driver.provisioning.azure.MicrosoftAzureCloudDriver"
+		
+		// Optional. The template name for the management machines. Defaults to the first template in the templates section below.
+		managementMachineTemplate "SMALL_LINUX"
+		
+		// Optional. Indicates whether internal cluster communications should use the machine private IP. Defaults to true.
+		connectToPrivateIp true
+	}
+
+	provider {
+	
+		// Optional 
+		provider "azure"
+			
+		/*************************************************************************************************************************
+		 * Optional. The HTTP/S URL where cloudify can be downloaded from by newly started machines. Defaults to downloading the *
+		 * cloudify version matching that of the client from the cloudify CDN.													 *
+		 * Change this if your compute nodes do not have access to an internet connection, or if you prefer to use a			 *
+		 * different HTTP server instead.																						 *
+		************************************************ *************************************************************************/
+		
+		// cloudifyUrl "http://repository.cloudifysource.org/org/cloudifysource/2.2.0/gigaspaces-cloudify-2.2.0-m2-b2492-9.zip"
+		
+		machineNamePrefix "eli_azure_cloudify_agent_"
+		
+		dedicatedManagementMachines true
+		managementOnlyFiles ([])
+		
+		managementGroup "eli_azure_cloudify_manager"
+		numberOfManagementMachines 1
+		zones (["agent"])
+		reservedMemoryCapacityPerMachineInMB 1024
+		
+		sshLoggingLevel "WARNING"
+		
+		
+	}
+	
+	user {
+		
+		// Azure subscription id
+		user "9f24fac0-f989-4873-b3d5-6886fbc6cd29"
+			
+	}
+
+	templates ([
+				SMALL_LINUX : template{
+				
+					imageId "OpenLogic__OpenLogic-CentOS-62-20120531-en-us-30GB.vhd"
+					machineMemoryMB 1600
+					hardwareId "Small"
+					remoteDirectory "/home/elip/gs-files"
+					localDirectory "tools/cli/plugins/esc/azure/upload"
+					
+					username "elip"
+					password "1408Rokk"
+					
+					custom ([
+					
+						// Optional. each availability set represents a different fault domain.
+						
+						"azure.availability.set" : "eliset1",
+						
+						// Choose whether do deploy this instance in Staging or Production environment. defaults to Staging
+						
+						"azure.deployment.slot": "Staging",
+						
+						/**************************************************************
+						 * Mandatory only for templates used for management machines. *
+						 * Put this file under the path specified in 'localDirectory' *
+						***************************************************************/
+						
+						"azure.pfx.file": "eli-azure-cert.pfx",
+						
+						// Password that was used to create the certificate
+						
+						"azure.pfx.password" : "1408Rokk"
+					])
+				},
+				
+				HTTP_OUT : template{
+				
+					imageId "OpenLogic__OpenLogic-CentOS-62-20120531-en-us-30GB.vhd"
+					machineMemoryMB 1600
+					hardwareId "Small"
+					remoteDirectory "/home/elip/gs-files"
+					localDirectory "tools/cli/plugins/esc/azure/upload"
+					
+					username "elip"
+					password "1408Rokk"
+					
+					custom ([
+					
+						// Optional. each availability set represents a different fault domain.
+						
+						"azure.availability.set" : "eliset1",
+						
+						// Choose whether do deploy this instance in Staging or Production environment. defaults to Staging
+						
+						"azure.deployment.slot": "Staging",
+						
+						// Tcp EndPoints to open
+						
+						"azure.endpoints" : ([
+												([
+													"name" : "WEB",
+													"port" : "8080" 
+												])
+											]),
+						
+						"azure.pfx.file": "eli-azure-cert.pfx",
+						
+						// Password that was used to create the certificate
+						
+						"azure.pfx.password" : "1408Rokk"
+					])
+				}
+			])
+			
+	custom ([
+			
+		/*****************************************************************************************
+		 * A Virtaul Network name.																 *
+		 * All VM's will belong to this network. 												 *
+		 * If the specified network does not exist, it will be created automatically for you.	 *
+		 * in this case, you must specify the 'azure.address.space' property					 *	 
+		******************************************************************************************/
+		
+		"azure.network.name" : "elitestnetwork",
+		
+		/***************************************************************************************
+		 * CIDR notation specifying the Address Space for your Virtaul Network. 			   *
+		 * All VM's will be assigned a private ip from this address space.					   *
+		****************************************************************************************/
+		
+		"azure.address.space" : "10.4.0.0/16",
+		
+		/****************************************************************************************	
+		 * An Affinity Group name.																*
+		 * if the specified group does not exist, one will be created automatically for you.	*
+		 * in this case, you must specify the 'azure.affinity.location' property				*
+		*****************************************************************************************/
+		
+		"azure.affinity.group" : "cloudifyaffinitygroup",
+
+		/********************************************************************************************************************************
+		 * The MS Data Center location. 																								*
+		 * All VM's will be launched onto this Data Center. see http://matthew.sorvaag.net/2011/06/windows-azure-data-centre-locations/	*	
+		 * Mandatory only if the affinity group specifed above is not a pre-existing one.												*
+		*********************************************************************************************************************************/
+		
+		"azure.affinity.location" : "East US",
+		
+		/*****************************************************************************************
+		 * A Storage Account name.																 *
+		 * All OS Disk Images will be stored in this account. 									 *
+		 * If the specified account does not exist, it will be created automatically for you.	 *
+		******************************************************************************************/
+
+		"azure.storage.account" : "cloudifystorage",
+		
+		// Specify whether or not to delete the network (if found) when you execute a teardown command. 
+		
+		"azure.delete.network.on.teardown" : "true",
+
+		// Enable/Disable Cloud Requests Logging. 
+		
+		"azure.wireLog": "false"
+	])
+}
+
---
+++ esc/src/main/resources/clouds/azure/upload/bootstrap-management.sh
@@ -0,0 +1,213 @@
+#! /bin/bash
+
+#############################################################################
+# This script starts a Gigaspaces agent for use with the Gigaspaces
+# Cloudify. The agent will function as management depending on the value of $GSA_MODE
+#
+# Parameters the should be exported beforehand:
+# 	$LUS_IP_ADDRESS - Ip of the head node that runs a LUS and ESM. May be my IP. (Required)
+#   $GSA_MODE - 'agent' if this node should join an already running node. Otherwise, any value.
+#	$NO_WEB_SERVICES - 'true' if web-services (rest, webui) should not be deployed (only if GSA_MODE != 'agent')
+#   $MACHINE_IP_ADDRESS - The IP of this server (Useful if multiple NICs exist)
+#	$MACHINE_ZONES - This is required if this is not a management machine
+# 	$WORKING_HOME_DIRECTORY - This is where the files were copied to (cloudify installation, etc..)
+#	$CLOUDIFY_LINK - If this url is found, it will be downloaded to $WORKING_HOME_DIRECTORY/gigaspaces.zip
+#	$CLOUDIFY_OVERRIDES_LINK - If this url is found, it will be downloaded and unzipped into the same location as cloudify
+#	$CLOUD_FILE - Location of the cloud configuration file. Only available in bootstrap of management machines.
+#	$NO_WEB_SERVICES - If set to 'true', indicates that the rest and web-ui services should not be deployed in this machine.
+#	$CLOUDIFY_CLOUD_IMAGE_ID - If set, indicates the image ID for this machine.
+#	$CLOUDIFY_CLOUD_HARDWARE_ID - If set, indicates the hardware ID for this machine.
+#	$PASSWORD - the machine password
+#############################################################################
+
+# args:
+# $1 the error code of the last command (should be explicitly passed)
+# $2 the message to print in case of an error
+# 
+# an error message is printed and the script exists with the provided error code
+function error_exit {
+	echo "in error_exit"
+	echo "$2 : error code: $1"
+	exit ${1}
+}
+
+# args:
+# $1 the error code of the last command (should be explicitly passed)
+# $2 the message to print in case of an error 
+# $3 the threshold to exit on
+#
+# if (last_error_code [$1]) >= (threshold [$3]) the provided message[$2] is printed and the script
+# exists with the provided error code ($1)
+function error_exit_on_level {
+	if [ ${1} -ge ${3} ]; then
+		error_exit ${1} ${2}
+	fi
+}
+
+
+JAVA_32_URL="http://repository.cloudifysource.org/com/oracle/java/1.6.0_32/jdk-6u32-linux-i586.bin"
+JAVA_64_URL="http://repository.cloudifysource.org/com/oracle/java/1.6.0_32/jdk-6u32-linux-x64.bin"
+
+if [ -z "$CLOUDIFY_AGENT_ENV_JAVA_URL" ]; then
+	ARCH=`uname -m`
+	echo Machine Architecture -- $ARCH
+	if [ "$ARCH" = "i686" ]; then
+		export CLOUDIFY_AGENT_ENV_JAVA_URL=$JAVA_32_URL
+	elif [ "$ARCH" = "x86_64" ]; then
+		export CLOUDIFY_AGENT_ENV_JAVA_URL=$JAVA_64_URL
+	else 
+		echo Unknown architecture -- $ARCH -- defaulting to 32 bit JDK
+		export CLOUDIFY_AGENT_ENV_JAVA_URL=$JAVA_32_URL
+	fi
+	
+fi  
+
+if [ "$CLOUDIFY_AGENT_ENV_JAVA_URL" = "NO_INSTALL" ]; then
+	echo "JDK will not be installed"
+else
+	echo Previous JAVA_HOME value -- $JAVA_HOME 
+	if [ -d ~/java ] 
+	then 
+		echo "Deleteing previous java installtion"
+		rm -rf ~/java
+	fi
+	export CLOUDIFY_ORIGINAL_JAVA_HOME=$JAVA_HOME
+
+	echo Downloading JDK from $CLOUDIFY_AGENT_ENV_JAVA_URL    
+	wget -q -O $WORKING_HOME_DIRECTORY/java.bin $CLOUDIFY_AGENT_ENV_JAVA_URL
+	chmod +x $WORKING_HOME_DIRECTORY/java.bin
+	echo -e "\n" > $WORKING_HOME_DIRECTORY/input.txt
+	mkdir ~/java
+	cd ~/java
+	
+	echo Installing JDK
+	$WORKING_HOME_DIRECTORY/java.bin < $WORKING_HOME_DIRECTORY/input.txt > /dev/null
+	mv ~/java/*/* ~/java || error_exit $? "Failed moving JDK installation"
+	rm -f $WORKING_HOME_DIRECTORY/input.txt
+    export JAVA_HOME=~/java
+fi  
+
+export EXT_JAVA_OPTIONS="-Dcom.gs.multicast.enabled=false"
+
+# Some distros do not come with unzip built-in
+if [ ! -f "/usr/bin/unzip" ]; then
+	chmod +x $WORKING_HOME_DIRECTORY/unzip || error_exit $? "Failed changing execution permission to unzip"
+	chmod +x $WORKING_HOME_DIRECTORY/unzipsfx || error_exit $? "Failed changing execution permission to unzip"
+	
+	cp $WORKING_HOME_DIRECTORY/unzip /usr/bin || error_exit $? "Failed copying unzip"
+	cp $WORKING_HOME_DIRECTORY/unzipsfx /usr/bin || error_exit $? "Failed copying unzip"
+fi
+
+
+if [ ! -z "$CLOUDIFY_LINK" ]; then
+	echo Downloading cloudify installation
+	wget -q $CLOUDIFY_LINK -O $WORKING_HOME_DIRECTORY/gigaspaces.zip || error_exit $? "Failed downloading cloudify installation"
+fi
+
+if [ ! -z "$CLOUDIFY_OVERRIDES_LINK" ]; then
+	echo Downloading cloudify overrides
+	wget -q $CLOUDIFY_OVERRIDES_LINK -O $WORKING_HOME_DIRECTORY/gigaspaces_overrides.zip || error_exit $? "Failed downloading cloudify overrides"
+fi
+
+# Todo: Check this condition
+if [ ! -d "~/gigaspaces" -o $WORKING_HOME_DIRECTORY/gigaspaces.zip -nt ~/gigaspaces ]; then
+	rm -rf ~/gigaspaces || error_exit $? "Failed removing old gigaspaces directory"
+	mkdir ~/gigaspaces || error_exit $? "Failed creating gigaspaces directory"
+	
+	# 2 is the error level threshold. 1 means only warnings
+	# this is needed for testing purposes on zip files created on the windows platform 
+	unzip -q $WORKING_HOME_DIRECTORY/gigaspaces.zip -d ~/gigaspaces || error_exit_on_level $? "Failed extracting cloudify installation" 2 
+
+	# Todo: consider removing this line
+	chmod -R 777 ~/gigaspaces || error_exit $? "Failed changing permissions in cloudify installion"
+	mv ~/gigaspaces/*/* ~/gigaspaces || error_exit $? "Failed moving cloudify installation"
+	
+	if [ ! -z "$CLOUDIFY_OVERRIDES_LINK" ]; then
+		echo Copying overrides into cloudify distribution
+		unzip -qo $WORKING_HOME_DIRECTORY/gigaspaces_overrides.zip -d ~/gigaspaces || error_exit_on_level $? "Failed extracting cloudify overrides" 2 		
+	fi
+fi
+
+# if an overrides directory exists, copy it into the cloudify distribution
+if [ -d $WORKING_HOME_DIRECTORY/cloudify-overrides ]; then
+	cp -rf $WORKING_HOME_DIRECTORY/cloudify-overrides/* ~/gigaspaces
+fi
+
+# UPDATE SETENV SCRIPT...
+echo Updating environment script
+cd ~/gigaspaces/bin || error_exit $? "Failed changing directory to bin directory"
+
+sed -i "1i export NIC_ADDR=$MACHINE_IP_ADDRESS" setenv.sh || error_exit $? "Failed updating setenv.sh"
+sed -i "1i export LOOKUPLOCATORS=$LUS_IP_ADDRESS" setenv.sh || error_exit $? "Failed updating setenv.sh"
+sed -i "1i export CLOUDIFY_CLOUD_IMAGE_ID=$CLOUDIFY_CLOUD_IMAGE_ID" setenv.sh || error_exit $? "Failed updating setenv.sh"
+sed -i "1i export CLOUDIFY_CLOUD_HARDWARE_ID=$CLOUDIFY_CLOUD_HARDWARE_ID" setenv.sh || error_exit $? "Failed updating setenv.sh"
+sed -i "1i export PATH=$JAVA_HOME/bin:$PATH" setenv.sh || error_exit $? "Failed updating setenv.sh"
+
+cd ~/gigaspaces/tools/cli || error_exit $? "Failed changing directory to cli directory"
+
+# START AGENT ALONE OR WITH MANAGEMENT
+if [ -f nohup.out ]; then
+  rm nohup.out
+fi
+
+if [ -f nohup.out ]; then
+   error_exit 1 "Failed to remove nohup.out Probably used by another process"
+fi
+
+# Privileged mode handling
+
+if [ "$CLOUDIFY_AGENT_ENV_PRIVILEGED" = "true" ]; then
+	echo Setting privileged mode
+	export CLOUDIFY_USER=`whoami`
+	if [ "$CLOUDIFY_USER" = "root" ]; then
+		# root is privileged by definition
+		echo Already running as root
+	else
+		sudo -n ls > /dev/null || error_exit_on_level $? "Current user is not a sudoer, or requires a password for sudo" 1
+		
+		if [ ! -f "/etc/sudoers" ]; then
+			error_exit 101 "Could not find sudoers file at expected location (/etc/sudoers)"
+		fi
+		
+		echo Disabling requiretty directive
+		sudo sed -i 's/^Defaults    requiretty/# Defaults    requiretty/g' /etc/sudoers || error_exit_on_level $? "Failed to edit sudoers file to disable requiretty directive" 1
+		
+		
+	fi
+
+fi
+
+# add localhost mapping to /etc/hosts (god dammit azure)
+
+HOST_NAME=`hostname`
+echo -e "#! /bin/bash\n echo $PASSWORD" > $WORKING_HOME_DIRECTORY/password.sh
+chmod +x $WORKING_HOME_DIRECTORY/password.sh
+SUDO_ASKPASS=$WORKING_HOME_DIRECTORY/password.sh
+export SUDO_ASKPASS
+echo "$MACHINE_IP_ADDRESS $HOST_NAME" | sudo -A tee -a /etc/hosts
+rm $WORKING_HOME_DIRECTORY/password.sh
+
+if [ ! -z "$CLOUDIFY_AGENT_ENV_INIT_COMMAND" ]; then
+	echo Executing initialization command
+	$CLOUDIFY_AGENT_ENV_INIT_COMMAND
+fi
+
+if [ "$GSA_MODE" = "agent" ]; then
+	ERRMSG="Failed starting agent"
+	nohup ./cloudify.sh start-agent -timeout 30 --verbose -zone $MACHINE_ZONES -auto-shutdown
+else
+	ERRMSG="Failed starting management services"
+	if [ "$NO_WEB_SERVICES" = "true" ]; then
+		nohup ./cloudify.sh start-management -no-web-services -no-management-space -timeout 30 --verbose -auto-shutdown -cloud-file $CLOUD_FILE
+	else
+		nohup ./cloudify.sh start-management -timeout 30 --verbose -auto-shutdown -cloud-file $CLOUD_FILE
+	fi
+fi	
+
+RETVAL=$?
+echo cat nohup.out
+cat nohup.out
+if [ $RETVAL -ne 0 ]; then
+  error_exit $RETVAL $ERRMSG
+fi
+exit 0
