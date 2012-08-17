9f84f53e4f69e5aa19f0cb62958812295b59c64a
opencm <opencm@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-08-17 05:20:13 +0000

    Update cloudify url
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@126240 eb64e737-3616-4df0-8941-5ee2ae88103d

--- esc/src/main/resources/clouds/openstack/openstack-cloud.groovy
+++ esc/src/main/resources/clouds/openstack/openstack-cloud.groovy
@@ -20,7 +20,7 @@ cloud {
 		// Change this if your compute nodes do not have access to an internet connection, or if you prefer to use a
 		// different HTTP server instead.
 		// 
-		// cloudifyUrl "http://repository.cloudifysource.org/org/cloudifysource/2.2.0/gigaspaces-cloudify-2.2.0-m3-b2491-86.zip"
+		// cloudifyUrl "http://repository.cloudifysource.org/org/cloudifysource/2.2.0/gigaspaces-cloudify-2.2.0-m3-b2491-87.zip"
 		
 		machineNamePrefix "cloudify_agent_"
 		
