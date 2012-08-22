f9edd0122ec2a1fa8d2f81c83e914e5d4b9aeb2a
opencm <opencm@eb64e737-3616-4df0-8941-5ee2ae88103d>
2012-08-22 18:39:02 +0000

    Update cloudify url
    git-svn-id: svn://pc-lab14/SVN/cloudify/trunk/cloudify@127139 eb64e737-3616-4df0-8941-5ee2ae88103d

--- esc/src/main/resources/clouds/azure/azure-cloud.groovy
+++ esc/src/main/resources/clouds/azure/azure-cloud.groovy
@@ -27,7 +27,7 @@ cloud {
 		 * different HTTP server instead.																						 *
 		************************************************ *************************************************************************/
 		
-		// cloudifyUrl "http://repository.cloudifysource.org/org/cloudifysource/2.2.0/gigaspaces-cloudify-2.2.0-m4-b2493-37.zip"
+		// cloudifyUrl "http://repository.cloudifysource.org/org/cloudifysource/2.2.0/gigaspaces-cloudify-2.2.0-m4-b2493-38.zip"
 		
 		machineNamePrefix "cloudify_agent_"
 		
