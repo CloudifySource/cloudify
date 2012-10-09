77ee9373c2a9287793e63c1270bda4802ca57238
Alexander Beresnev <alexb@gigaspaces.com>
2012-10-09 14:45:10 +0200

    Add the ability to upload folders with subfolders to S3

--- cloudify/tools/maven-plugins/s3client-maven-plugin/src/main/java/com/gigaspaces/cloudify/s3client/S3PutMojo.java
+++ cloudify/tools/maven-plugins/s3client-maven-plugin/src/main/java/com/gigaspaces/cloudify/s3client/S3PutMojo.java
@@ -16,7 +16,9 @@
 package org.cloudifysource.s3client;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Properties;
 import java.util.Set;
 
@@ -41,95 +43,144 @@ import com.google.inject.Module;
  */
 public class S3PutMojo extends AbstractMojo {
 
-    /**
-     * The S3 user
-     * 
-     * @parameter
-     *  expression="${put.user}"
-     *  default-value=""
-     */
-    private String user;
-    
-    /**
-     * The S3 key
-     * 
-     * @parameter
-     *  expression="${put.key}"
-     *  default-value=""
-     */
-    private String key;
-    
-    /**
-     * The file to put 
-     * 
-     * @parameter 
-     *  expression="${put.source}" 
-     *  type="java.io.File"
-     *  default-value=""
-     */
-    private File source;
-    
-    /**
-     * The containter to put into
-     * 
-     * @parameter 
-     *  expression="${put.container}" 
-     *  default-value=""
-     */
-    private String container;
-    
-    /**
-     * The target path of the blob
-     * 
-     * @parameter 
-     *  expression="${put.target}" 
-     *  default-value=""
-     */
-    private String target;
-    
-    /**
-     * Should the blob have a public url
-     * 
-     * @parameter 
-     *  expression="${put.publicUrl}" 
-     *  default-value=""
-     */
-    private String publicUrl;
-    
-    public void execute() throws MojoExecutionException, MojoFailureException {
+	/**
+	 * The S3 user
+	 * 
+	 * @parameter
+	 *  expression="${put.user}"
+	 *  default-value=""
+	 */
+	private String user;
 
-        BlobStoreContext context = null;
-        
-        try {
-            Set<Module> wiring = new HashSet<Module>();
-            context = new BlobStoreContextFactory().createContext("aws-s3", user, key, wiring, new Properties());
-                
-            BlobStore store = context.getBlobStore();
-            Blob newBlob = store.blobBuilder(target)
-                .payload(source)
-                .build();
+	/**
+	 * The S3 key
+	 * 
+	 * @parameter
+	 *  expression="${put.key}"
+	 *  default-value=""
+	 */
+	private String key;
 
-            getLog().info("upload size is: " + source.length());
-            getLog().info("waiting for upload to end");
-            
-            store.putBlob(container, newBlob);
-            
-            if (publicUrl != null && Boolean.parseBoolean(publicUrl)) {
-                S3Client client = S3Client.class.cast(context.getProviderSpecificContext().getApi());
-                String ownerId = client.getObjectACL(container, target).getOwner().getId();
-                
-                client.putObjectACL(container, target, 
-                        AccessControlList.fromCannedAccessPolicy(CannedAccessPolicy.PUBLIC_READ, ownerId));
-            }
-                
-        } catch (Exception e) {
-            throw new MojoFailureException("Failed put operation", e);
-        } finally {
-            if (context != null) {
-                context.close();
-            }
-        }
-        
-    }
-    
+	/**
+	 * The file to put 
+	 * 
+	 * @parameter 
+	 *  expression="${put.source}" 
+	 *  type="java.io.File"
+	 *  default-value=""
+	 */
+	private File source;
+
+	/**
+	 * The containter to put into
+	 * 
+	 * @parameter 
+	 *  expression="${put.container}" 
+	 *  default-value=""
+	 */
+	private String container;
+
+	/**
+	 * The target path of the blob
+	 * 
+	 * @parameter 
+	 *  expression="${put.target}" 
+	 *  default-value=""
+	 */
+	private String target;
+
+	/**
+	 * Should the blob have a public url
+	 * 
+	 * @parameter 
+	 *  expression="${put.publicUrl}" 
+	 *  default-value=""
+	 */
+	private String publicUrl;
+
+	public void execute() throws MojoExecutionException, MojoFailureException {
+
+		BlobStoreContext context = null;
+
+		try {
+			Set<Module> wiring = new HashSet<Module>();
+			context = new BlobStoreContextFactory().createContext("aws-s3", user, key, wiring, new Properties());
+
+			BlobStore store = context.getBlobStore();
+
+			uploadFile( source, target, store);
+			
+			/*if (source.isDirectory())
+			{
+				getLog().info("Source " + source + " is a directory. Adding all files");
+
+				for (File f : source. listFiles())
+				{
+					getLog().info("Processing " + source + "/" + f.getName() + ", upload size is: " + (source+"/"+f.getName()).length() + ". Target: " + target + "/" + f.getName());
+					store.putBlob(container, store.blobBuilder(target + "/" + f.getName())
+							.payload(new File(source.getPath() + "/" + f.getName()))
+							.build());
+					getLog().info("Upload of " + source + "/" + f.getName() + " was ended successfully");
+
+			 if (publicUrl != null && Boolean.parseBoolean(publicUrl)) {
+						S3Client client = S3Client.class.cast(context.getProviderSpecificContext().getApi());
+						String ownerId = client.getObjectACL(container, target+"/"+f.getName()).getOwner().getId();
+
+						client. putObjectACL(container, target+"/"+f.getName(), 
+								AccessControlList.fromCannedAccessPolicy(CannedAccessPolicy.PUBLIC_READ, ownerId));
+					}
+				}
+
+			}
+			else
+			{
+				Blob newBlob = store.blobBuilder(target)
+				.payload(source)
+				.build();
+				getLog().info("upload size is: " + source.length());
+				getLog().info("waiting for upload to end");
+				store.putBlob(container, newBlob);
+				
+				if (publicUrl != null && Boolean.parseBoolean(publicUrl)) {
+					S3Client client = S3Client.class.cast(context.getProviderSpecificContext().getApi());
+					String ownerId = client.getObjectACL(container, target).getOwner().getId();
+
+					client. putObjectACL(container, target, 
+							AccessControlList.fromCannedAccessPolicy(CannedAccessPolicy.PUBLIC_READ, ownerId));
+				}
+
+			}*/
+
+
+		} catch (Exception e) {
+			throw new MojoFailureException("Failed put operation", e);
+		} finally {
+			if (context != null) {
+				context.close();
+			}
+		}
+
+	}
+	
+	private void uploadFile(File source, String target, BlobStore store) throws MojoFailureException
+	{
+		if (source.isDirectory())
+		{
+			for (File f : source.listFiles())
+			{
+				uploadFile(new File(source.getPath() + "/" + f.getName()), target + "/" + f.getName(), store);
+			}
+		}
+		else
+		{
+			getLog().info("Processing " + source  + ", upload size is: " + (source).length() + ". Target: " + target);
+	
+			store.putBlob(container, store.blobBuilder(target)
+					.payload(source)
+					.build());
+			getLog().info("Upload of " + source + " was ended successfully");
+		}
+			
+	}
+
 }
