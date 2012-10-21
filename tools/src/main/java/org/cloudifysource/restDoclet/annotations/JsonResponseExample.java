76c587ea03b09674cb119ced3157f6496bdb5d93
yaelpeled <yael@gigaspaces.com>
2012-10-21 17:16:16 +0200

    CLOUDIFY-1035 Added the Gigaspaces copyright notice at the top of each file.

--- tools/src/main/java/org/cloudifysource/restDoclet/annotations/JsonResponseExample.java
+++ tools/src/main/java/org/cloudifysource/restDoclet/annotations/JsonResponseExample.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2012 GigaSpaces Technologies Ltd. All rights reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 package org.cloudifysource.restDoclet.annotations;
 
 import java.lang.annotation.Documented;
@@ -11,7 +26,7 @@ import java.lang.annotation.Target;
  * 
  * Annotation to specify an example of a JSON response.
  * Should be use within the REST's controllers above requestMapping methods 
- * to specify an example that will be shown in the REST API documentation. 
+ * to specify a response example that will be shown in the REST API documentation. 
  * @author yael
  *
  */
