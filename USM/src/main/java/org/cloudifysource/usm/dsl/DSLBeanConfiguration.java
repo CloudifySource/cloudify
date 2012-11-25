7c2832cd55efc788d5b1b6f755fc38d34953360e
Barak <barakme@gigaspaces.com>
2012-11-25 15:15:04 +0200

    CLOUDIFY-1253 Start detection closure return type must be boolean.

--- USM/src/main/java/org/cloudifysource/usm/dsl/DSLBeanConfiguration.java
+++ USM/src/main/java/org/cloudifysource/usm/dsl/DSLBeanConfiguration.java
@@ -30,6 +30,7 @@ import org.cloudifysource.dsl.Plugin;
 import org.cloudifysource.dsl.PluginDescriptor;
 import org.cloudifysource.dsl.Service;
 import org.cloudifysource.dsl.context.ServiceContext;
+import org.cloudifysource.dsl.entry.ClosureExecutableEntry;
 import org.cloudifysource.dsl.entry.ExecutableDSLEntry;
 import org.cloudifysource.dsl.entry.ExecutableDSLEntryFactory;
 import org.cloudifysource.dsl.internal.CloudifyConstants;
@@ -524,9 +525,14 @@ public class DSLBeanConfiguration implements ApplicationContextAware {
 					final Object retcode = result.getResult();
 					if (retcode instanceof Boolean) {
 						return (Boolean) retcode;
-					} else {
-						throw new USMException("The start detector return code was not a boolean! Result was: " + retcode);
 					}
+					
+					// If closure, make sure return value is boolean.
+					if(detector instanceof ClosureExecutableEntry) {
+						throw new USMException("A start detector closure return a result that is not boolean! Result was: " + retcode);	
+					}
+					
+					return true;
 				}
 				// process exited with abnormal status code
 				logger.log(Level.WARNING, "Liveness Detector failed to execut. Exception was: "
