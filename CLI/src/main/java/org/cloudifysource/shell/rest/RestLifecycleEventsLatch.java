9f776c409d5826cc215c839f3f37c9668a970c5c
adamlavie <adaml@gigaspaces.com>
2012-11-27 19:45:13 +0200

    Cloudify-1236 removed unnecessary print

--- CLI/src/main/java/org/cloudifysource/shell/rest/RestLifecycleEventsLatch.java
+++ CLI/src/main/java/org/cloudifysource/shell/rest/RestLifecycleEventsLatch.java
@@ -88,7 +88,7 @@ public class RestLifecycleEventsLatch {
 					lifecycleEventLogs = (Map<String, Object>) client.get(url);
 				} catch (final ErrorStatusException e) {
 					if (e.getCause() instanceof IOException) {
-						displayer.printEvent("Communication Error accessing "+ url + ". Reason: " + e.getMessage()); 
+						displayer.printEvent("Communication Error accessing "+ url); 
 						return false;
 					} 
 					throw new CLIException("Deployment failed. Reason: " + e.getMessage(), e);
