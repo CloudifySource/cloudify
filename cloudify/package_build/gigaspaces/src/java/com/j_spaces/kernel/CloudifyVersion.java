7125f1869430d51ace892df9668d41b2c911030b
Tgrid <tgrid@build64A.gspaces.com>
2012-12-09 10:39:10 +0200

    Build version advanced to 3485-21

--- cloudify/package_build/gigaspaces/src/java/com/j_spaces/kernel/CloudifyVersion.java
+++ cloudify/package_build/gigaspaces/src/java/com/j_spaces/kernel/CloudifyVersion.java
@@ -27,14 +27,14 @@ public class CloudifyVersion implements ProductVersion {
 	// Must be of this format otherwise PlatformLogicalVersion will fail
 	// parsing!!!
 	private String VERSION = "2.3.0";
-	private String MILESTONE = "rc";
+	private String MILESTONE = "ga";
 	private String BUILD_TYPE = "regular";
 	private String V_NUM = VERSION + '-' + EDITION + '-' + MILESTONE;
 	private String V_LICENSE_NUM = "2.2" + EDITION;
 	// !!!IMPORTANT, read below
 	// Must be of either "int-int-string", "int-int" or "int" format otherwise
 	// PlatformLogicalVersion will fail parsing!!!
-	private final String BUILD_NUM = "3483-19";
+	private final String BUILD_NUM = "3485-21";
 	private final String V_NAME = "GigaSpaces";
 	private final String PRODUCT_HELP_URL = "http://www.cloudifysource.org/guide";
 
