f3538ef967ee9955800e6b6ee6a2ba86da99494b
Guy Korland <gkorland@gmail.com>
2012-12-13 09:48:55 +0200

    Clean imports

--- USM/src/main/java/org/cloudifysource/usm/jmx/JmxGenericClient.java
+++ USM/src/main/java/org/cloudifysource/usm/jmx/JmxGenericClient.java
@@ -20,7 +20,6 @@ import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
--- esc/src/main/java/org/cloudifysource/esc/byon/ByonDeployer.java
+++ esc/src/main/java/org/cloudifysource/esc/byon/ByonDeployer.java
@@ -20,7 +20,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Hashtable;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
