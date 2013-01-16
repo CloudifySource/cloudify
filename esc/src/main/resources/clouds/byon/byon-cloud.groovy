0d318687825eb8e99b7013709eca2aa7a3716881
iliapolo <Eli.polonsky@gmail.com>
2013-01-16 18:21:07 +0200

    CLOUDIFY-1458 expose this property in the cloud driver with a default setting to false

--- esc/src/main/resources/clouds/byon/byon-cloud.groovy
+++ esc/src/main/resources/clouds/byon/byon-cloud.groovy
@@ -110,6 +110,7 @@ cloud {
 	 // Optional. Sets whether to delete the remoteDirectory created by the cloud driver, when shutting down.
 	custom ([
 		"cleanGsFilesOnShutdown": "true",
+		"clearRemoteDirectoryOnStart":"false",
 		"itemsToClean": ([
 			"/tmp/gs-files/gigaspaces.tar.gz"
 		])
