6e7e30e93b2f7285f3a0d1baf9617214cb39a1fa
Barak <barakme@gigaspaces.com>
2013-01-28 14:47:58 +0200

    CLOUDIFY-1250 Added Scp file transfer

---
+++ esc/src/main/java/org/cloudifysource/esc/installer/filetransfer/ScpFileTransfer.java
@@ -0,0 +1,240 @@
+/*******************************************************************************
+ * Copyright (c) 2013 GigaSpaces Technologies Ltd. All rights reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *******************************************************************************/
+
+package org.cloudifysource.esc.installer.filetransfer;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.logging.Level;
+
+import net.schmizz.sshj.SSHClient;
+import net.schmizz.sshj.common.IOUtils;
+import net.schmizz.sshj.connection.ConnectionException;
+import net.schmizz.sshj.connection.channel.direct.Session;
+import net.schmizz.sshj.connection.channel.direct.Session.Command;
+import net.schmizz.sshj.transport.TransportException;
+import net.schmizz.sshj.transport.verification.PromiscuousVerifier;
+import net.schmizz.sshj.userauth.UserAuthException;
+import net.schmizz.sshj.userauth.method.AuthNone;
+import net.schmizz.sshj.xfer.FileSystemFile;
+import net.schmizz.sshj.xfer.LocalFileFilter;
+import net.schmizz.sshj.xfer.LocalSourceFile;
+import net.schmizz.sshj.xfer.scp.SCPFileTransfer;
+import net.schmizz.sshj.xfer.scp.SCPUploadClient;
+
+import org.apache.commons.lang.StringUtils;
+import org.cloudifysource.esc.installer.InstallationDetails;
+import org.cloudifysource.esc.installer.InstallerException;
+
+/********
+ * A file transfer implementation using Secure Copy (SCP), based on the sshj library.
+ *
+ * @author barakme
+ * @since 2.5.0
+ *
+ */
+public class ScpFileTransfer implements FileTransfer {
+
+	private static final String CREATE_REMOTE_DIRECTORY_WITH_DELETE = "if [ -d {0} ]; then rm -rf {0}; fi; mkdir {0}";
+	private static final String CREATE_REMOTE_DIRECTORY = "if [ ! -d {0} ]; then mkdir {0}; fi";
+
+	private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(ScpFileTransfer.class
+			.getName());
+
+	private String host;
+
+	private String localDirPath;
+
+	private boolean deleteRemoteDirectoryContents;
+
+	@Override
+	public void copyFiles(final InstallationDetails details, final Set<String> excludedFiles,
+			final List<File> additionalFiles,
+			final long endTimeMillis) throws TimeoutException, InstallerException {
+		final SSHClient ssh = new SSHClient();
+		ssh.addHostKeyVerifier(new PromiscuousVerifier());
+		try {
+			ssh.connect(host);
+		} catch (final IOException e) {
+			throw new InstallerException("Failed to connect to host: " + host + ": " + e.getMessage(), e);
+		}
+
+		try {
+
+			try {
+				if (!StringUtils.isEmpty(details.getKeyFile())) {
+					final File keyFile = new File(details.getKeyFile());
+					if (!keyFile.exists() || !keyFile.isFile()) {
+						throw new InstallerException("Expected to find key file at: " + keyFile.getAbsolutePath());
+					}
+					ssh.authPublickey(details.getUsername(), keyFile.getAbsolutePath());
+				} else if (!StringUtils.isEmpty(details.getPassword())) {
+					ssh.authPassword(details.getUsername(), details.getPassword());
+				} else {
+					ssh.auth(details.getUsername(), new AuthNone());
+				}
+			} catch (final TransportException e) {
+				throw new InstallerException("Failed to authenticate to remote server: " + e.getMessage(), e);
+			} catch (final UserAuthException e) {
+				throw new InstallerException("Failed to authenticate to remote server: " + e.getMessage(), e);
+			}
+
+			// First, we need to create the remote directory
+
+			createRemoteDirectory(details, endTimeMillis, ssh);
+
+			// Note: With SCP, we do not delete the contents of the remote directory! Use SFTP if y ou want this
+			// feature.
+			final SCPFileTransfer transfer = ssh.newSCPFileTransfer();
+			try {
+				final SCPUploadClient uploadClient = transfer.newSCPUploadClient();
+				final File localDirectory = new File(this.localDirPath);
+				final String localDirectoryPath = localDirectory.getCanonicalPath();
+
+				// Filter out excluded files
+				uploadClient.setUploadFilter(new LocalFileFilter() {
+
+					@Override
+					public boolean accept(final LocalSourceFile originalFile) {
+
+						final FileSystemFile file = (FileSystemFile) originalFile;
+						final File localFile = file.getFile();
+						String localFilePath;
+						try {
+							localFilePath = localFile.getCanonicalPath();
+						} catch (final IOException e) {
+							throw new IllegalStateException("Failed to get canonical path of: " + localFile, e);
+						}
+
+						if (!localFilePath.startsWith(localDirectoryPath)) {
+							throw new IllegalStateException("Upload candidate file: " + localFilePath
+									+ " is not a descendant of local directory: " + localDirectoryPath);
+						}
+
+						String relativePath = localFilePath.substring(localDirectoryPath.length());
+
+						// remote trailing separator
+						if (relativePath.startsWith(File.separator)) {
+							relativePath = relativePath.substring(1);
+						}
+
+						// convert windows separator to linux
+						relativePath = relativePath.replace("\\", "/");
+						if (excludedFiles.contains(relativePath)) {
+							return false;
+						}
+						return true;
+					}
+				});
+
+				// uploading local directory to remote directory
+				File[] files = localDirectory.listFiles();
+				for (File file : files) {
+					uploadClient.copy(new FileSystemFile(file), details.getRemoteDir());
+				}
+
+				// uploading additional files to remote directory
+				for (File file : additionalFiles) {
+					transfer.upload(new FileSystemFile(file), details.getRemoteDir());
+				}
+			} catch (final IOException e) {
+				throw new InstallerException("Failed to upload files to remote server: " + e.getMessage(), e);
+			}
+		} finally {
+			try {
+				ssh.disconnect();
+			} catch (final IOException e) {
+				logger.log(Level.WARNING, "Failed to disconnect ssh session", e);
+			}
+		}
+	}
+
+	private void createRemoteDirectory(final InstallationDetails details, final long endTimeMillis, final SSHClient ssh)
+			throws InstallerException {
+		Session session = null;
+		try {
+			session = ssh.startSession();
+			session.allocateDefaultPTY();
+			final long timeout = endTimeMillis - System.currentTimeMillis();
+			String commandString = createCommandString(details);
+			Command command = session.exec(commandString);
+			final String commandOutput = IOUtils.readFully(command.getInputStream()).toString();
+			command.join((int) timeout, TimeUnit.MILLISECONDS);
+			Integer exitStatus = command.getExitStatus();
+			if (exitStatus == null) {
+				throw new InstallerException("Remote command to create directory did not return");
+			} else {
+				if (exitStatus != 0) {
+					throw new InstallerException("Failed to create remote directory: " + details.getRemoteDir()
+							+ ". Command output was: " + commandOutput);
+				}
+			}
+		} catch (IOException e) {
+			throw new InstallerException("Failed to create remote directory: " + details.getRemoteDir()
+					+ ". Error was: " + e.getMessage(), e);
+		} finally {
+			if (session != null) {
+				try {
+					session.close();
+				} catch (TransportException e) {
+					logger.log(Level.WARNING, "Failed to close ssh session while creating remote directory", e);
+				} catch (ConnectionException e) {
+					logger.log(Level.WARNING, "Failed to close ssh session while creating remote directory", e);
+				}
+			}
+		}
+	}
+
+	private String createCommandString(final InstallationDetails details) {
+		if (this.deleteRemoteDirectoryContents) {
+			return MessageFormat.format(CREATE_REMOTE_DIRECTORY_WITH_DELETE,
+					details.getRemoteDir());
+		} else {
+			return MessageFormat.format(CREATE_REMOTE_DIRECTORY,
+					details.getRemoteDir());
+		}
+	}
+
+	@Override
+	public void initialize(final InstallationDetails details, final long endTimeMillis) throws TimeoutException,
+			InstallerException {
+		// TODO - this code should be in the installer, and not copied in each of the file transfer implementations!
+		this.deleteRemoteDirectoryContents = details.isDeleteRemoteDirectoryContents();
+		if (details.isConnectedToPrivateIp()) {
+			host = details.getPrivateIp();
+		} else {
+			host = details.getPublicIp();
+		}
+
+		// when bootstrapping a management machine, pass all of the cloud
+		// configuration, including all template
+		// for an agent machine, just pass the upload dir fot the specific
+		// template.
+
+		localDirPath = details.getLocalDir();
+		if (details.isManagement()) {
+			if (details.getCloudFile() == null) {
+				throw new IllegalArgumentException("While bootstrapping a management machine, cloud file is null");
+			}
+
+			localDirPath = details.getCloudFile().getParentFile().getAbsolutePath();
+
+		}
+	}
+
+}
---
+++ esc/src/test/java/org/cloudifysource/esc/installer/FileTransferTest.java
@@ -0,0 +1,247 @@
+package org.cloudifysource.esc.installer;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.security.InvalidKeyException;
+import java.security.PublicKey;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.TimeoutException;
+
+import org.apache.sshd.SshServer;
+import org.apache.sshd.common.Channel;
+import org.apache.sshd.common.Cipher;
+import org.apache.sshd.common.Compression;
+import org.apache.sshd.common.ForwardingAcceptorFactory;
+import org.apache.sshd.common.KeyExchange;
+import org.apache.sshd.common.Mac;
+import org.apache.sshd.common.NamedFactory;
+import org.apache.sshd.common.Signature;
+import org.apache.sshd.common.cipher.AES128CBC;
+import org.apache.sshd.common.cipher.AES128CTR;
+import org.apache.sshd.common.cipher.AES192CBC;
+import org.apache.sshd.common.cipher.AES256CBC;
+import org.apache.sshd.common.cipher.AES256CTR;
+import org.apache.sshd.common.cipher.ARCFOUR128;
+import org.apache.sshd.common.cipher.ARCFOUR256;
+import org.apache.sshd.common.cipher.BlowfishCBC;
+import org.apache.sshd.common.cipher.TripleDESCBC;
+import org.apache.sshd.common.compression.CompressionNone;
+import org.apache.sshd.common.forward.DefaultForwardingAcceptorFactory;
+import org.apache.sshd.common.mac.HMACMD5;
+import org.apache.sshd.common.mac.HMACMD596;
+import org.apache.sshd.common.mac.HMACSHA1;
+import org.apache.sshd.common.mac.HMACSHA196;
+import org.apache.sshd.common.random.BouncyCastleRandom;
+import org.apache.sshd.common.random.JceRandom;
+import org.apache.sshd.common.random.SingletonRandomFactory;
+import org.apache.sshd.common.signature.SignatureDSA;
+import org.apache.sshd.common.signature.SignatureRSA;
+import org.apache.sshd.common.util.OsUtils;
+import org.apache.sshd.common.util.SecurityUtils;
+import org.apache.sshd.server.ForwardingFilter;
+import org.apache.sshd.server.PasswordAuthenticator;
+import org.apache.sshd.server.PublickeyAuthenticator;
+import org.apache.sshd.server.channel.ChannelDirectTcpip;
+import org.apache.sshd.server.channel.ChannelSession;
+import org.apache.sshd.server.filesystem.NativeFileSystemFactory;
+import org.apache.sshd.server.kex.DHG1;
+import org.apache.sshd.server.kex.DHG14;
+import org.apache.sshd.server.keyprovider.PEMGeneratorHostKeyProvider;
+import org.apache.sshd.server.keyprovider.SimpleGeneratorHostKeyProvider;
+import org.apache.sshd.server.session.ServerSession;
+import org.apache.sshd.server.shell.ProcessShellFactory;
+import org.cloudifysource.dsl.cloud.FileTransferModes;
+import org.cloudifysource.esc.installer.filetransfer.ScpFileTransfer;
+import org.junit.Test;
+
+public class FileTransferTest {
+
+	public static SshServer setUpDefaultServer() {
+		SshServer sshd = new SshServer();
+		// DHG14 uses 2048 bits key which are not supported by the default JCE provider
+		if (SecurityUtils.isBouncyCastleRegistered()) {
+			sshd.setKeyExchangeFactories(Arrays.<NamedFactory<KeyExchange>> asList(
+					new DHG14.Factory(),
+					new DHG1.Factory()));
+			sshd.setRandomFactory(new SingletonRandomFactory(new BouncyCastleRandom.Factory()));
+		} else {
+			sshd.setKeyExchangeFactories(Arrays.<NamedFactory<KeyExchange>> asList(
+					new DHG1.Factory()));
+			sshd.setRandomFactory(new SingletonRandomFactory(new JceRandom.Factory()));
+		}
+		setUpDefaultCiphers(sshd);
+		// Compression is not enabled by default
+		// sshd.setCompressionFactories(Arrays.<NamedFactory<Compression>>asList(
+		// new CompressionNone.Factory(),
+		// new CompressionZlib.Factory(),
+		// new CompressionDelayedZlib.Factory()));
+		sshd.setCompressionFactories(Arrays.<NamedFactory<Compression>> asList(
+				new CompressionNone.Factory()));
+		sshd.setMacFactories(Arrays.<NamedFactory<Mac>> asList(
+				new HMACMD5.Factory(),
+				new HMACSHA1.Factory(),
+				new HMACMD596.Factory(),
+				new HMACSHA196.Factory()));
+		sshd.setChannelFactories(Arrays.<NamedFactory<Channel>> asList(
+				new ChannelSession.Factory(),
+				new ChannelDirectTcpip.Factory()));
+		sshd.setSignatureFactories(Arrays.<NamedFactory<Signature>> asList(
+				new SignatureDSA.Factory(),
+				new SignatureRSA.Factory()));
+		sshd.setFileSystemFactory(new NativeFileSystemFactory());
+
+		ForwardingAcceptorFactory faf = new DefaultForwardingAcceptorFactory();
+		sshd.setTcpipForwardNioSocketAcceptorFactory(faf);
+		sshd.setX11ForwardNioSocketAcceptorFactory(faf);
+
+		return sshd;
+	}
+
+	private static void setUpDefaultCiphers(SshServer sshd) {
+		List<NamedFactory<Cipher>> avail = new LinkedList<NamedFactory<Cipher>>();
+		avail.add(new AES128CTR.Factory());
+		avail.add(new AES256CTR.Factory());
+		avail.add(new ARCFOUR128.Factory());
+		avail.add(new ARCFOUR256.Factory());
+		avail.add(new AES128CBC.Factory());
+		avail.add(new TripleDESCBC.Factory());
+		avail.add(new BlowfishCBC.Factory());
+		avail.add(new AES192CBC.Factory());
+		avail.add(new AES256CBC.Factory());
+
+		for (Iterator<NamedFactory<Cipher>> i = avail.iterator(); i.hasNext();) {
+			final NamedFactory<Cipher> f = i.next();
+			try {
+				final Cipher c = f.create();
+				final byte[] key = new byte[c.getBlockSize()];
+				final byte[] iv = new byte[c.getIVSize()];
+				c.init(Cipher.Mode.Encrypt, key, iv);
+			} catch (InvalidKeyException e) {
+				i.remove();
+			} catch (Exception e) {
+				i.remove();
+			}
+		}
+		sshd.setCipherFactories(avail);
+	}
+
+	@Test
+	public void testScpFileTransfer() throws IOException, TimeoutException, InstallerException {
+		ScpFileTransfer transfer = new ScpFileTransfer();
+		InstallationDetails details = new InstallationDetails();
+
+		details.setAdmin(null);
+		details.setBindToPrivateIp(true);
+		details.setConnectedToPrivateIp(true);
+		details.setDeleteRemoteDirectoryContents(true);
+		details.setFileTransferMode(FileTransferModes.SCP);
+		details.setLocalDir("c:/temp/temp");
+		details.setManagement(false);
+
+		// TODO
+		details.setPassword("reverse");
+		details.setPrivateIp("192.168.0.6");
+		details.setPublicIp("BLABLA");
+		details.setRemoteDir("/home/ubuntu/transfer");
+		details.setUsername("ubuntu");
+
+		final long end = System.currentTimeMillis() + 1000 * 60;
+
+		transfer.initialize(details, end);
+		transfer.copyFiles(details, new HashSet<String>(Arrays.asList("2.txt", "subfolder1", "subfolder2/1.txt")),
+				new ArrayList<File>(), end);
+		// SshServer sshd = startSshServer(2222);
+		// sshd.setPasswordAuthenticator(new PasswordAuthenticator() {
+		//
+		// @Override
+		// public boolean authenticate(String username, String password, ServerSession session) {
+		// System.out.println(username);
+		// System.out.println(password);
+		// return true;
+		// }
+		// });
+
+		// sshd.setPort(2222);
+
+		// SshServer sshd = new SshServer();
+		// sshd.setPasswordAuthenticator(new PasswordAuthenticator() {
+		//
+		// @Override
+		// public boolean authenticate(String username, String password, ServerSession session) {
+		// System.out.println(username);
+		// System.out.println(password);
+		// return true;
+		// }
+		// });
+		// sshd.setKeyExchangeFactories(new ArrayList<NamedFactory<KeyExchange>>());
+		// sshd.start();
+		// System.in.read();
+
+	}
+
+	private SshServer startSshServer(final int port) throws IOException {
+		boolean error = false;
+
+		System.err.println("Starting SSHD on port " + port);
+
+		SshServer sshd = SshServer.setUpDefaultServer();
+		sshd.setPort(port);
+		if (SecurityUtils.isBouncyCastleRegistered()) {
+			sshd.setKeyPairProvider(new PEMGeneratorHostKeyProvider("key.pem"));
+		} else {
+			sshd.setKeyPairProvider(new SimpleGeneratorHostKeyProvider("key.ser"));
+		}
+		if (OsUtils.isUNIX()) {
+			sshd.setShellFactory(new ProcessShellFactory(new String[] { "/bin/sh", "-i", "-l" },
+					EnumSet.of(ProcessShellFactory.TtyOptions.ONlCr)));
+		} else {
+			sshd.setShellFactory(new ProcessShellFactory(new String[] { "cmd.exe " },
+					EnumSet.of(ProcessShellFactory.TtyOptions.Echo, ProcessShellFactory.TtyOptions.ICrNl,
+							ProcessShellFactory.TtyOptions.ONlCr)));
+		}
+		sshd.setPasswordAuthenticator(new PasswordAuthenticator() {
+			@Override
+			public boolean authenticate(String username, String password, ServerSession session) {
+				return username != null && username.equals(password);
+			}
+		});
+		sshd.setPublickeyAuthenticator(new PublickeyAuthenticator() {
+			@Override
+			public boolean authenticate(String username, PublicKey key, ServerSession session) {
+				// File f = new File("/Users/" + username + "/.ssh/authorized_keys");
+				return true;
+			}
+		});
+		sshd.setForwardingFilter(new ForwardingFilter() {
+			@Override
+			public boolean canForwardAgent(ServerSession session) {
+				return true;
+			}
+
+			@Override
+			public boolean canForwardX11(ServerSession session) {
+				return true;
+			}
+
+			@Override
+			public boolean canListen(InetSocketAddress address, ServerSession session) {
+				return true;
+			}
+
+			@Override
+			public boolean canConnect(InetSocketAddress address, ServerSession session) {
+				return true;
+			}
+		});
+		sshd.start();
+		return sshd;
+	}
+
+}
