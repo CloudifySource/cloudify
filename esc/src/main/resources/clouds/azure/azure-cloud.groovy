8f2779ea70ddc9423b4e103ccdc520947d0cb167
iliapolo <Eli.polonsky@gmail.com>
2013-02-01 15:15:26 +0200

    CLOUDIFY-1490 fixed

--- esc/src/main/resources/clouds/azure/azure-cloud.groovy
+++ esc/src/main/resources/clouds/azure/azure-cloud.groovy
@@ -56,7 +56,7 @@ cloud {
 	templates ([
 				SMALL_LINUX : template{
 				
-					imageId "5112500ae3b842c8b9c604889f8753c3__OpenLogic-CentOS63DEC20121220"
+					imageId "5112500ae3b842c8b9c604889f8753c3__OpenLogic-CentOS63JAN20130122"
 					machineMemoryMB 1600
 					hardwareId "Small"
 					localDirectory "upload"
