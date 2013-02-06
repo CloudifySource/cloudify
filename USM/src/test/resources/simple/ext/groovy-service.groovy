33a07161a74e736963b34430521463b6dfe33e46
Barak <barakme@gigaspaces.com>
2013-02-06 16:26:33 +0200

    Adding simple groovy recipe to USM for tests

---
+++ USM/src/test/resources/simple/ext/groovy-service.groovy
@@ -0,0 +1,35 @@
+import java.util.concurrent.TimeUnit
+
+
+
+
+service {
+	name "groovy"
+	type "WEB_SERVER"
+	elastic true
+	numInstances 1	
+	lifecycle { 
+	
+//		install { throw new Exception("This is a failure test") }
+		
+		start "run.groovy" 
+		
+		
+		startDetection {
+			new File(context.serviceDirectory + "/marker.txt").exists()
+		}
+		
+		locator {
+			println "Sleeping for 5 secs"
+			sleep(5000)
+			def query = "Exe.Cwd.eq=" + context.serviceDirectory+",Args.*.eq=org.codehaus.groovy.tools.GroovyStarter"
+			println "qeury is: " + query
+			def pids = ServiceUtils.ProcessUtils.getPidsWithQuery(query)
+			
+			println "LOCATORS GOT: " + pids
+			return pids;
+		}
+	}
+
+	
+}
