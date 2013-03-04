894821f8bef692f011c43c9d50eae8bf9f2946aa
Barak <barakme@gigaspaces.com>
2013-03-04 12:44:58 +0200

    CLOUDIFY-1514 Explicit check for trying to use ServiceContextFactory outside of a USM-launched script.

--- dsl/src/main/java/org/cloudifysource/dsl/context/ServiceContextFactory.java
+++ dsl/src/main/java/org/cloudifysource/dsl/context/ServiceContextFactory.java
@@ -1,17 +1,14 @@
 /*******************************************************************************
  * Copyright (c) 2011 GigaSpaces Technologies Ltd. All rights reserved
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  *******************************************************************************/
 package org.cloudifysource.dsl.context;
 
@@ -64,8 +61,17 @@ public final class ServiceContextFactory {
 			// name
 			final ClusterInfo info = createClusterInfo();
 			final File dir = new File(".");
+			String serviceFileName = System.getenv(CloudifyConstants.USM_ENV_SERVICE_FILE_NAME);
+			if (serviceFileName == null) {
+				throw new IllegalStateException(
+						"Expected service file name in environment variable: "
+								+ CloudifyConstants.USM_ENV_SERVICE_FILE_NAME
+								+ " but got null. "
+								+ "The ServiceContextFactory can only be used from a script launched by a Cloudify "
+								+ " Service and will not run on its own");
+			}
 			final File dslFile = new File(dir,
-					System.getenv(CloudifyConstants.USM_ENV_SERVICE_FILE_NAME));
+					serviceFileName);
 			Service service;
 			try {
 				service = ServiceReader.readService(dslFile);
