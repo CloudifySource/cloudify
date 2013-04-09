8ac67db0bf4d80aa36172128c1ad7aaab0d568ca
Tgrid <tgrid@build64A.gspaces.com>
2013-04-09 16:31:01 +0300

    Update cloudify url

--- esc/src/main/resources/clouds/byon/byon-cloud.groovy
+++ esc/src/main/resources/clouds/byon/byon-cloud.groovy
@@ -41,7 +41,7 @@ cloud {
 		// different HTTP server instead.
 		// IMPORTANT: the default linux bootstrap script appends '.tar.gz' to the url whereas the default windows script appends '.zip'.
 		// Therefore, if setting a custom URL, make sure to leave out the suffix.
-		// cloudifyUrl "http://repository.cloudifysource.org/org/cloudifysource/2.5.0-RELEASE/gigaspaces-cloudify-2.5.0-ga-b4000.zip"
+		// cloudifyUrl "http://repository.cloudifysource.org/org/cloudifysource/2.5.0-RELEASE/gigaspaces-cloudify-2.5.0-ga-b4510.zip"
 
 		
 		// Mandatory. The prefix for new machines started for servies.
