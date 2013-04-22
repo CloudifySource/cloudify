7db93c39862d4c90eb469871a8395a1ed3d84104
Tgrid <tgrid@build64A.gspaces.com>
2013-04-22 20:54:02 +0300

    Build version advanced to 4981-3

--- cloudify/package_build/gigaspaces/src/java/com/j_spaces/kernel/CloudifyVersion.java
+++ cloudify/package_build/gigaspaces/src/java/com/j_spaces/kernel/CloudifyVersion.java
@@ -27,14 +27,14 @@ public class CloudifyVersion implements ProductVersion {
 	// Must be of this format otherwise PlatformLogicalVersion will fail
 	// parsing!!!
 	private String VERSION = "2.6.0";
-	private String MILESTONE = "m1";
+	private String MILESTONE = "m2";
 	private String BUILD_TYPE = "regular";
 	private String V_NUM = VERSION + '-' + EDITION + '-' + MILESTONE;
 	private String V_LICENSE_NUM = "2.2" + EDITION;
 	// !!!IMPORTANT, read below
 	// Must be of either "int-int-string", "int-int" or "int" format otherwise
 	// PlatformLogicalVersion will fail parsing!!!
-	private final String BUILD_NUM = "4981";
+	private final String BUILD_NUM = "4981-3";
 	private final String V_NAME = "GigaSpaces";
 	private final String PRODUCT_HELP_URL = "http://www.cloudifysource.org/guide";
 
