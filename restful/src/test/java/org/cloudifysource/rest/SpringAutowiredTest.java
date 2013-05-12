8bd41108ab34dd870042520b33ceb6cee70ba3ec
alexberesnev <alexb@gigaspaces.com>
2013-05-12 10:22:18 +0300

    add spring-aspects available in test scope

--- restful/src/test/java/org/cloudifysource/rest/SpringAutowiredTest.java
+++ restful/src/test/java/org/cloudifysource/rest/SpringAutowiredTest.java
@@ -9,7 +9,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@Ignore
+
 //Swap the default JUnit4 with the spring specific SpringJUnit4ClassRunner.
 //This will allow spring to inject the application context
 @RunWith(SpringJUnit4ClassRunner.class)
