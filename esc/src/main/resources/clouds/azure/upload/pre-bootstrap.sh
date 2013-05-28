702c4164c6be596285c2f3725c6e04493cd729d0
iliapolo <elip@gigaspaces.com>
2013-05-28 12:58:57 +0300

    CLOUDIFY-1766 fixed. edit sudoers file to enable running sudo command without a password

--- esc/src/main/resources/clouds/azure/upload/pre-bootstrap.sh
+++ esc/src/main/resources/clouds/azure/upload/pre-bootstrap.sh
@@ -1,11 +1,17 @@
 #! /bin/bash
 
-# add localhost mapping to /etc/hosts
-
 HOST_NAME=`hostname`
 echo -e "#! /bin/bash\n echo $PASSWORD" > $WORKING_HOME_DIRECTORY/password.sh
 chmod +x $WORKING_HOME_DIRECTORY/password.sh
 SUDO_ASKPASS=$WORKING_HOME_DIRECTORY/password.sh
 export SUDO_ASKPASS
+
+# add localhost mapping to /etc/hosts
 echo "$MACHINE_IP_ADDRESS $HOST_NAME" | sudo -A tee -a /etc/hosts
+
+# enable current user to run sudo without a password
+USER=`whoami`
+echo "$USER ALL=(ALL)   NOPASSWD :ALL" | sudo -A tee -a /etc/sudoers
+
+# delete the password script
 rm $WORKING_HOME_DIRECTORY/password.sh
