89a6dfa84e5fc2ded3799eae033d65535e6aca3a
Barak <barakme@gigaspaces.com>
2013-05-29 17:14:58 +0300

    CLOUDIFY-1778 resources are downloaded as unique filename and copied to destination if it does not already exist, to prevent issues with parallel downloaders

--- dsl/src/main/java/org/cloudifysource/dsl/internal/tools/download/ResourceDownloader.java
+++ dsl/src/main/java/org/cloudifysource/dsl/internal/tools/download/ResourceDownloader.java
@@ -23,7 +23,9 @@ import java.text.MessageFormat;
 import java.util.concurrent.TimeoutException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
@@ -32,36 +34,38 @@ import org.apache.http.client.methods.HttpHead;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 /**
- * This class enables resource download and resource verification using the 
- * VerifyChecksum class {@link org.cloudifysource.dsl.internal.tools.download.ChecksumVerifier}
- * Supported checksum algorithms include md5, sha1, sha256, sha384 and sha512,
- * See enum {@link org.cloudifysource.dsl.internal.tools.download.ChecksumVerifier.ChecksumAlgorithm}
- * The default hash message format used to extract the hash message from the hash file is of the form {0} *{1}
- * i.e 'hash string *some string'. The file hash output will be compared against the {0} index.   
- * 
+ * This class enables resource download and resource verification using the VerifyChecksum class
+ * {@link org.cloudifysource.dsl.internal.tools.download.ChecksumVerifier} Supported checksum algorithms include md5,
+ * sha1, sha256, sha384 and sha512, See enum
+ * {@link org.cloudifysource.dsl.internal.tools.download.ChecksumVerifier.ChecksumAlgorithm} The default hash message
+ * format used to extract the hash message from the hash file is of the form {0} *{1} i.e 'hash string *some string'.
+ * The file hash output will be compared against the {0} index.
+ *
  * @author adaml
  * @since 2.6.0
  *
  */
 public class ResourceDownloader {
-	
-	//big buffer
-	private final int BUFFER_SIZE = 100 * 1024;
 
-	private final long DEFAULT_DOWNLOAD_TIMEOUT_MILLIS = 600000;
-	
-	private final int DEFAULT_NUMBER_OF_RETRIES = 3;
-	
+	private static final int TEMPORARY_FILE_CREATION_RETY_LIMIT = 100;
+
+	// big buffer
+	private static final int BUFFER_SIZE = 100 * 1024;
+
+	private static final long DEFAULT_DOWNLOAD_TIMEOUT_MILLIS = 600000;
+
+	private static final int DEFAULT_NUMBER_OF_RETRIES = 3;
+
 	private static final Logger logger = Logger
 			.getLogger(ResourceDownloadFacadeImpl.class.getName());
 
 	private URL resourceUrl;
 
 	private URL hashUrl;
-	
-	//destination where the resource file will be saved
+
+	// destination where the resource file will be saved
 	private File resourceDest;
-	
+
 	private long timeoutInMillis = DEFAULT_DOWNLOAD_TIMEOUT_MILLIS;
 
 	private String userName;
@@ -69,10 +73,10 @@ public class ResourceDownloader {
 	private String password;
 
 	private int numberOfRetries = DEFAULT_NUMBER_OF_RETRIES;
-	
+
 	private boolean skipExisting;
 
-	//the hash message format.
+	// the hash message format.
 	private MessageFormat format = new MessageFormat("{0} *{1}");
 
 	public void setUrl(final URL urlString) {
@@ -85,16 +89,16 @@ public class ResourceDownloader {
 
 	public void setResourceDest(final File resourceDest) {
 		this.resourceDest = resourceDest;
-		
+
 	}
 
 	public File getResourceDest() {
 		return this.resourceDest;
 	}
-	
+
 	public void setHashUrl(final URL hashUrl) {
 		this.hashUrl = hashUrl;
-		
+
 	}
 
 	public URL getHashUrl() {
@@ -111,16 +115,16 @@ public class ResourceDownloader {
 
 	public void setUserName(final String userName) {
 		this.userName = userName;
-		
+
 	}
 
 	public void setPassword(final String password) {
 		this.password = password;
 	}
-	
+
 	public void setNumberOfRetries(final int numberOfRetries) {
 		this.numberOfRetries = numberOfRetries;
-		
+
 	}
 
 	public int getNumberOfRetries() {
@@ -134,26 +138,25 @@ public class ResourceDownloader {
 	public boolean getSkipExisting() {
 		return this.skipExisting;
 	}
-	
+
 	public void setFormat(final MessageFormat format) {
 		this.format = format;
 	}
-	
+
 	public MessageFormat getFormat() {
 		return this.format;
 	}
 
 	/**
-	 * Use this method to verify resource-file's integrity using 
-	 * a checksum file containing the file hash. The checksum file extension
-	 * determines the hashing algorithm used.  
-	 * 
+	 * Use this method to verify resource-file's integrity using a checksum file containing the file hash. The checksum
+	 * file extension determines the hashing algorithm used.
+	 *
 	 * @param checksumFile
-	 * 			A file containing the hash code.
+	 *            A file containing the hash code.
 	 * @throws ResourceDownloadException
-	 * 			if hashing algorithm does not exist, or other exception occurs. 
+	 *             if hashing algorithm does not exist, or other exception occurs.
 	 */
-	public void verifyResourceChecksum(final File checksumFile) 
+	public void verifyResourceChecksum(final File checksumFile)
 			throws ResourceDownloadException {
 		final ChecksumVerifier cv = new ChecksumVerifier();
 		cv.setFile(this.resourceDest);
@@ -178,101 +181,178 @@ public class ResourceDownloader {
 	}
 
 	/**
-	 * 
+	 *
 	 * @throws ResourceDownloadException
-	 * 			if download fails.
+	 *             if download fails.
 	 * @throws TimeoutException
-	 * 			if timeout exceeded.
+	 *             if timeout exceeded.
 	 */
-    public void download()
-    			throws ResourceDownloadException, TimeoutException {
-    	
-    	if (this.resourceDest.exists() && this.skipExisting) {
-    		logger.log(Level.INFO, "File already exists. "
-    				+ this.resourceDest.getAbsolutePath() + " Skipping download.");
-    		return;
-    	}
-    	for (int attempt = 1; attempt <= this.numberOfRetries; attempt++) {
-    		try {
-    			getResource(this.resourceUrl, this.resourceDest);
-    			if (this.hashUrl != null) {
-    				//create checksum file destination.
-    				//The checksum file extension determines the hashing algorithm used.
-    				String resourceName = getResourceName(this.hashUrl);
-					File checksumFile = new File(this.resourceDest.getParent(), resourceName);
-					
-    				getResource(this.hashUrl, checksumFile);
-    				logger.log(Level.FINE, "Verifying resource checksum using checksum file " 
-    						+ checksumFile.getAbsolutePath());
-    				verifyResourceChecksum(checksumFile);
-    				return;
-    			}
-    		} catch (ResourceDownloadException e) {
-    			logger.log(Level.WARNING, "Failed downloading resource on attempt " + attempt
-    					+ ". Reason was " + e.getMessage());
-    			if (attempt == numberOfRetries) {
-    				throw e;
-    			}
-    		}
-		}
-    }
+	public void download()
+			throws ResourceDownloadException, TimeoutException {
 
-	private void getResource(final URL downloadURL, final File destination) 
+		if (this.resourceDest.exists() && this.skipExisting) {
+			logger.log(Level.INFO, "File already exists. "
+					+ this.resourceDest.getAbsolutePath() + " Skipping download.");
+			return;
+		}
+
+		createDestinationDirectories();
+
+		for (int attempt = 1; attempt <= this.numberOfRetries; attempt++) {
+			try {
+				getResource(this.resourceUrl, this.resourceDest);
+				if (this.hashUrl != null) {
+					// create checksum file destination.
+					// The checksum file extension determines the hashing algorithm used.
+					String resourceName = getResourceName(this.hashUrl);
+					File checksumFile = new File(this.resourceDest.getParent(), resourceName);
+
+					getResource(this.hashUrl, checksumFile);
+					logger.log(Level.FINE, "Verifying resource checksum using checksum file "
+							+ checksumFile.getAbsolutePath());
+					verifyResourceChecksum(checksumFile);
+				}
+				return;
+			} catch (ResourceDownloadException e) {
+				logger.log(Level.WARNING, "Failed downloading resource on attempt " + attempt
+						+ ". Reason was " + e.getMessage());
+				if (attempt == numberOfRetries) {
+					throw e;
+				}
+			}
+		}
+	}
+
+	private void createDestinationDirectories() throws ResourceDownloadException {
+		File destinationParent = this.resourceDest.getParentFile();
+		if (!destinationParent.exists()) {
+			if (!destinationParent.mkdirs()) {
+				throw new ResourceDownloadException("Failed to create the required directories for destination: "
+						+ this.resourceDest);
+			}
+		}
+	}
+
+	private void getResource(final URL downloadURL, final File destination)
 			throws ResourceDownloadException, TimeoutException {
 
 		final long end = System.currentTimeMillis() + this.timeoutInMillis;
 		final InputStream is = openConnectionInputStream(downloadURL);
-        if (is == null) {
-            logger.log(Level.WARNING, "connection input stream failed to initialize");
-            throw new ResourceDownloadException(
-            					"Failed getting " + this.resourceUrl + " to " + destination.getAbsolutePath());
-        }
-        final OutputStream os = getFileOutputString(destination);
-        boolean finished = false;
-        try {
-            final byte[] buffer = new byte[BUFFER_SIZE];
-            int length;
-            logger.info("Downloading " + downloadURL.toString() + " to " + this.resourceDest);
-            while ((length = is.read(buffer)) >= 0) {
-                os.write(buffer, 0, length);
-                if (end < System.currentTimeMillis()) {
-                	throw new TimeoutException();
-                }
-            }
-            finished = true;
-        } catch (IOException e) {
-        	logger.warning("Failed downloading resource from " + downloadURL.toString() 
-        			+ ". Reason was: " + e.getMessage());
-        	throw new ResourceDownloadException("Failed downloading resource. Reason was: " 
-        			+ e.getMessage(), e);
-        } finally {
-        	IOUtils.closeQuietly(os);
-        	IOUtils.closeQuietly(is);
-        	
-            if (!finished) {
-            	logger.log(Level.WARNING, "Download did not complete successfully. deleting file.");
-            	destination.delete();
-            }
-        }
+		if (is == null) {
+			logger.log(Level.WARNING, "connection input stream failed to initialize");
+			throw new ResourceDownloadException(
+					"Failed getting " + this.resourceUrl + " to " + destination.getAbsolutePath());
+		}
+
+		final File temporaryDestination = createTemporaryDestinationFile(destination);
+
+		final OutputStream os = getFileOutputString(temporaryDestination);
+		boolean finished = false;
+		try {
+			final byte[] buffer = new byte[BUFFER_SIZE];
+			int length;
+			if (logger.isLoggable(Level.FINE)) {
+				logger.fine("Downloading " + downloadURL.toString() + " to " + this.resourceDest);
+			}
+			while ((length = is.read(buffer)) >= 0) {
+				os.write(buffer, 0, length);
+				if (end < System.currentTimeMillis()) {
+					throw new TimeoutException();
+				}
+			}
+			finished = true;
+
+		} catch (IOException e) {
+			logger.warning("Failed downloading resource from " + downloadURL.toString()
+					+ ". Reason was: " + e.getMessage());
+			throw new ResourceDownloadException("Failed downloading resource. Reason was: "
+					+ e.getMessage(), e);
+		} finally {
+			IOUtils.closeQuietly(os);
+			IOUtils.closeQuietly(is);
+
+			if (!finished) {
+				logger.log(Level.WARNING, "Download did not complete successfully. deleting file.");
+				FileUtils.deleteQuietly(temporaryDestination);
+				FileUtils.deleteQuietly(destination);
+
+			}
+		}
+		if (finished) {
+			try {
+
+				FileUtils.copyFile(temporaryDestination, destination);
+
+			} catch (IOException e) {
+				if (destination.exists()) {
+					logger.warning("Failed to write downloaded file to destination: "
+							+ destination
+							+ ". Destination file already exists. "
+							+ "This probably indicates a concurrent download of the same file.");
+				} else {
+					throw new ResourceDownloadException("Failed to copy downloaded file to target location: "
+							+ e.getMessage(), e);
+				}
+
+			} finally {
+				FileUtils.deleteQuietly(temporaryDestination);
+			}
+		}
+
+	}
+
+	private File createTemporaryDestinationFile(final File destination) throws ResourceDownloadException {
+		Exception lastException = null;
+		String temporaryFileName = null;
+		for (int i = 0; i < TEMPORARY_FILE_CREATION_RETY_LIMIT; ++i) {
+			temporaryFileName = destination.getName() + ".part." + System.nanoTime();
+			if (i > 0) {
+				temporaryFileName += "_" + i;
+			}
+
+			final File file = new File(destination.getParentFile(), temporaryFileName);
+			try {
+				if (file.createNewFile()) {
+					return file;
+				}
+			} catch (IOException e) {
+				// probably concurrent access
+				lastException = e;
+				if (logger.isLoggable(Level.FINE)) {
+					logger.log(Level.FINE, "Failed to create new file: " + file + ". Error was: " + e.getMessage(), e);
+				}
+			}
+		}
+
+		if (lastException == null) {
+			throw new ResourceDownloadException("Failed to create temporary file : " + temporaryFileName);
+		} else {
+			throw new ResourceDownloadException("Failed to create temporary file : " + temporaryFileName
+					+ ", last error was: " + lastException.getMessage(), lastException);
+		}
+
 	}
 
 	private OutputStream getFileOutputString(final File destination)
 			throws ResourceDownloadException {
-		destination.getParentFile().mkdirs(); 
-        try {
-        	destination.createNewFile();
-        	return new FileOutputStream(destination);
-        } catch (Exception e) {
-        	throw new ResourceDownloadException("Failed opening stream to dest file " 
-        							+ destination.getAbsolutePath(), e);
-        }
+		destination.getParentFile().mkdirs();
+		try {
+			// if (!destination.createNewFile()) {
+			// throw new IllegalStateException("Failed to create a new file called " + destination.getAbsolutePath()
+			// + ": file already exists");
+			// }
+			return new FileOutputStream(destination);
+		} catch (final IOException e) {
+			throw new ResourceDownloadException("Failed opening stream to dest file "
+					+ destination.getAbsolutePath(), e);
+		}
 	}
 
 	private InputStream openConnectionInputStream(final URL url) throws ResourceDownloadException {
-		
+
 		final DefaultHttpClient httpClient = new DefaultHttpClient();
 		final HttpHead httpMethod = new HttpHead(url.toString());
-		
+
 		HttpResponse response;
 		try {
 			logger.fine("validating url");
@@ -282,14 +362,14 @@ public class ResourceDownloader {
 				throw new ResourceDownloadException("Invalid resource URL: " + url.toString());
 			}
 			final URLConnection connection = url.openConnection();
-			 if (this.userName != null || this.password != null) {
-				 logger.fine("Setting connection credentials");
-		            String up = this.userName + ":" + this.password;
-		            String encoding = new String(
-		            		Base64.encodeBase64(up.getBytes()));
-		            connection.setRequestProperty("Authorization", "Basic " + encoding);
-		        }
-			 return connection.getInputStream();
+			if (this.userName != null || this.password != null) {
+				logger.fine("Setting connection credentials");
+				String up = this.userName + ":" + this.password;
+				String encoding = new String(
+						Base64.encodeBase64(up.getBytes()));
+				connection.setRequestProperty("Authorization", "Basic " + encoding);
+			}
+			return connection.getInputStream();
 		} catch (ClientProtocolException e) {
 			throw new ResourceDownloadException("Invalid connection protocol " + url.toString(), e);
 		} catch (IOException e) {
--- dsl/src/test/java/org/cloudifysource/dsl/download/ResourceDownloaderTest.java
+++ dsl/src/test/java/org/cloudifysource/dsl/download/ResourceDownloaderTest.java
@@ -13,9 +13,14 @@
 package org.cloudifysource.dsl.download;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.net.URL;
 import java.text.MessageFormat;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
@@ -35,66 +40,66 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
- * This test starts an embedded Jetty server on the localhost
- *  and tests download requests made by the download facade utility.
- *  
+ * This test starts an embedded Jetty server on the localhost and tests download requests made by the download facade
+ * utility.
+ *
  * @author adaml
  * @since 2.6.0
  */
 public class ResourceDownloaderTest {
 
-	//default resource params.
+	// default resource params.
 	private static final String RESOURCE_NAME = "testResource.txt";
 	private static final String DESTINATION_FOLDER = "src/test/resources/resourceDownloader/downloadFolder/";
 	private static final String RESOURCE_DESTINATION = DESTINATION_FOLDER + RESOURCE_NAME;
 	private static final String RESOURCE_URL = "http://localhost:8080/" + RESOURCE_NAME;
 	private static final String RESOURCE_FOLDER = "src/test/resources/resourceDownloader/";
-	
-	//used for testing checksum failure.
+
+	// used for testing checksum failure.
 	private static final String DUMMY_RESOURCE_NAME = "dummyTestResource.txt";
 	private static final String DUMMY_RESOURCE_URL = "http://localhost:8080/" + DUMMY_RESOURCE_NAME;
-	private static final ResourceDownloadFacade rdf = 
+	private static final ResourceDownloadFacade rdf =
 			new ResourceDownloadFacadeImpl(new ResourceDownloader());
-	
+
 	private static final Server server = new Server(8080);
-	
+
 	@BeforeClass
-	public static void beforeClass() 
+	public static void beforeClass()
 			throws Exception {
-		
-		//Start embedded jetty server.
+
+		// Start embedded jetty server.
 		final URL resourceUrl = new File(RESOURCE_FOLDER).
-							getAbsoluteFile().toURI().toURL();
+				getAbsoluteFile().toURI().toURL();
 		final Resource resource = new FileResource(resourceUrl);
 		final ResourceHandler handler = new ResourceHandler();
 		handler.setBaseResource(resource);
 		server.setHandler(handler);
 		server.start();
 	}
-	
+
 	@AfterClass
-	public static void afterClass() 
+	public static void afterClass()
 			throws Exception {
 		server.stop();
 	}
 
 	@Test
 	public void testTimeout() {
-		//Try doGet with very short timeout.
+		// Try doGet with very short timeout.
 		try {
 			rdf.get(RESOURCE_URL, RESOURCE_DESTINATION, false, 1, TimeUnit.MICROSECONDS);
 			Assert.fail("Expected timeout exception.");
 		} catch (TimeoutException e) {
-			//test passed.
+			// test passed.
 		} catch (ResourceDownloadException e) {
 			Assert.fail("Expecting timeout exception. got " + e.getMessage());
 		}
 	}
-	
+
 	@Test
 	public void testBadChecksumVerification() throws Exception {
-		//run doGet on a dummy file and try to verify it against
-		//non-matching checksum files.
+		// run doGet on a dummy file and try to verify it against
+		// non-matching checksum files.
 		assertChecksumFailure(RESOURCE_URL + ".md5");
 		cleanDownloadFolder();
 		assertChecksumFailure(RESOURCE_URL + ".sha1");
@@ -109,7 +114,7 @@ public class ResourceDownloaderTest {
 
 	void assertChecksumFailure(final String hashUrl) {
 		try {
-			rdf.get(DUMMY_RESOURCE_URL, RESOURCE_DESTINATION, 
+			rdf.get(DUMMY_RESOURCE_URL, RESOURCE_DESTINATION,
 					false, hashUrl);
 			Assert.fail("File checksum verified. This is not suppose to happen.");
 		} catch (Exception e) {
@@ -118,12 +123,13 @@ public class ResourceDownloaderTest {
 			}
 		}
 	}
-	
+
 	@Test
 	public void testResourceDownload() throws Exception {
-		//test get with checksum verification. 
-		//Hashing algorithm determined by the checksum file extension.
-		rdf.get(RESOURCE_URL, 
+		// test get with checksum verification.
+		// Hashing algorithm determined by the checksum file extension.
+		cleanDownloadFolder();
+		rdf.get(RESOURCE_URL,
 				RESOURCE_DESTINATION);
 		cleanDownloadFolder();
 		assertDownloadSuccess(RESOURCE_URL + ".md5");
@@ -137,11 +143,64 @@ public class ResourceDownloaderTest {
 		assertDownloadSuccess(RESOURCE_URL + ".sha512");
 		cleanDownloadFolder();
 	}
-	
+
+	private class DownloadTask implements Callable<Exception> {
+
+		@Override
+		public Exception call() throws Exception {
+			try {
+				rdf.get(RESOURCE_URL,
+						RESOURCE_DESTINATION);
+			} catch (ResourceDownloadException e) {
+				return e;
+			} catch (TimeoutException e) {
+				return e;
+			}
+			return null;
+		}
+
+	}
+
+	@Test
+	public void testConcurrentDownload() throws Exception {
+		for (int i = 0; i < 10; ++i) {
+			cleanDownloadFolder();
+			ExecutorService pool = Executors.newFixedThreadPool(3);
+
+			try {
+				DownloadTask task1 = new DownloadTask();
+				DownloadTask task2 = new DownloadTask();
+				DownloadTask task3 = new DownloadTask();
+
+				Future<Exception> future1 = pool.submit(task1);
+				Future<Exception> future2 = pool.submit(task2);
+				Future<Exception> future3 = pool.submit(task3);
+
+				Exception e1 = future1.get();
+				Exception e2 = future2.get();
+				Exception e3 = future3.get();
+
+				Assert.assertNull("concurrent download failed: " + e1, e1);
+				Assert.assertNull("concurrent download failed: " + e2, e2);
+				Assert.assertNull("concurrent download failed: " + e3, e3);
+
+				final File destinationFolder = new File(DESTINATION_FOLDER);
+				File[] files = destinationFolder.listFiles();
+				Assert.assertEquals("Expecting only one file", 1, files.length);
+
+			} finally {
+				pool.shutdown();
+				pool.awaitTermination(10, TimeUnit.SECONDS);
+				cleanDownloadFolder();
+			}
+		}
+
+	}
+
 	@Test
 	public void testChecksumFormatting() throws Exception {
-		//Tests checksum verification on checksum files that 
-		//are formatted using format type '{0}'.
+		// Tests checksum verification on checksum files that
+		// are formatted using format type '{0}'.
 		assertFormattedChecksumVerification("testResourceFormatted.txt.md5");
 		cleanDownloadFolder();
 		assertFormattedChecksumVerification("testResourceFormatted.txt.sha1");
@@ -153,50 +212,50 @@ public class ResourceDownloaderTest {
 		assertFormattedChecksumVerification("testResourceFormatted.txt.sha512");
 		cleanDownloadFolder();
 	}
-	
+
 	@Test
 	public void testSkipExistingFlag() throws Exception {
-		//asserts file was not overridden.
-		rdf.get(RESOURCE_URL, 
-				RESOURCE_DESTINATION, 
-				false, 
+		// asserts file was not overridden.
+		rdf.get(RESOURCE_URL,
+				RESOURCE_DESTINATION,
+				false,
 				RESOURCE_URL + ".md5");
-		
+
 		File downloadedResource = new File(DESTINATION_FOLDER, RESOURCE_NAME);
 		long lastModified = downloadedResource.lastModified();
-		
-		rdf.get(RESOURCE_URL, 
-				RESOURCE_DESTINATION, 
-				true, 
+
+		rdf.get(RESOURCE_URL,
+				RESOURCE_DESTINATION,
+				true,
 				RESOURCE_URL + ".md5");
-		Assert.assertTrue("File was modified but skip existing flag is true", 
-						downloadedResource.lastModified() == lastModified);
+		Assert.assertTrue("File was modified but skip existing flag is true",
+				downloadedResource.lastModified() == lastModified);
 		cleanDownloadFolder();
 	}
 
 	void assertFormattedChecksumVerification(final String hashFileName)
 			throws ResourceDownloadException {
-		
-		//test verification for special checksum format types
+
+		// test verification for special checksum format types
 		final ResourceDownloader downloader = new ResourceDownloader();
 		final File resourceFile = new File(RESOURCE_FOLDER, RESOURCE_NAME);
 		final File hashFile = new File(RESOURCE_FOLDER, hashFileName);
-		
+
 		downloader.setFormat(new MessageFormat("{0}"));
 		downloader.setResourceDest(resourceFile);
-		//Expecting not to throw exception.
+		// Expecting not to throw exception.
 		downloader.verifyResourceChecksum(hashFile);
 	}
 
 	void assertDownloadSuccess(final String hashUrl) throws ResourceDownloadException,
 			TimeoutException, IOException {
-		rdf.get(RESOURCE_URL, 
-				RESOURCE_DESTINATION, 
-				false, 
+		rdf.get(RESOURCE_URL,
+				RESOURCE_DESTINATION,
+				false,
 				hashUrl);
 		assertFilesDownloaded("testResource.txt", getResourceName(hashUrl));
 	}
-	
+
 	private String getResourceName(final String url) {
 		final int slashIndex = url.lastIndexOf('/');
 		final String filename = url.substring(slashIndex + 1);
@@ -204,22 +263,34 @@ public class ResourceDownloaderTest {
 	}
 
 	private void assertFilesDownloaded(final String resourceFileName, final String hashFileName) throws IOException {
-		
+
 		final File downloadedResourceFile = new File(DESTINATION_FOLDER, resourceFileName);
 		final File downloadedHashFile = new File(DESTINATION_FOLDER, hashFileName);
 		Assert.assertTrue("Resource file download failed", downloadedResourceFile.exists());
 		Assert.assertTrue("Hash file download failed", downloadedHashFile.exists());
-		
+
+		final File destinationFolder = new File(DESTINATION_FOLDER);
+		File[] partFiles = destinationFolder.listFiles(new FilenameFilter() {
+
+			@Override
+			public boolean accept(final File dir, final String name) {
+				return name.contains(".part.");
+
+			}
+		});
+
+		Assert.assertEquals("Expected no part files to be found", 0, partFiles.length);
+
 		final File originalResourceFile = new File(RESOURCE_FOLDER, resourceFileName);
 		final File originalHashFile = new File(RESOURCE_FOLDER, hashFileName);
-		//asserting file content is the same.
+		// asserting file content is the same.
 		Assert.assertTrue("Resource file was not downloaded properly",
 				FileUtils.readFileToString(downloadedResourceFile).
-				equals(FileUtils.readFileToString(originalResourceFile)));
+						equals(FileUtils.readFileToString(originalResourceFile)));
 		Assert.assertTrue("Hash file was not downloaded properly",
 				FileUtils.readFileToString(downloadedHashFile).
-				equals(FileUtils.readFileToString(originalHashFile)));
-		
+						equals(FileUtils.readFileToString(originalHashFile)));
+
 	}
 
 	private void cleanDownloadFolder() throws IOException {
