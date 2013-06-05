a468f278d39f00874845219ea23b3a21447f16c1
iliapolo <elip@gigaspaces.com>
2013-06-05 12:30:55 +0300

    CLOUDIFY-1743 added POJO's to represent service/application life cycle events

---
+++ dsl/src/main/java/org/cloudifysource/dsl/rest/response/DeploymentEvent.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (c) 2013 GigaSpaces Technologies Ltd. All rights reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *******************************************************************************/
+
+package org.cloudifysource.dsl.rest.response;
+
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: elip
+ * Date: 5/20/13
+ * Time: 2:04 PM
+ * <br></br>
+ *
+ * Represents a life cycle deployment event.
+ */
+public class DeploymentEvent {
+
+    private String description;
+    private int index;
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(final int index) {
+        this.index = index;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(final String description) {
+        this.description = description;
+    }
+}
