d7e0532559724c9cb503327715b7b85d96b76529
Tgrid <tgrid@build64A.gspaces.com>
2013-06-06 22:05:57 +0300

    Build version advanced to 4983-313

--- cloudify/package_build/gigaspaces/src/java/com/j_spaces/kernel/CloudifyVersion.java
+++ cloudify/package_build/gigaspaces/src/java/com/j_spaces/kernel/CloudifyVersion.java
@@ -34,10 +34,10 @@ public class CloudifyVersion implements ProductVersion {
 	// !!!IMPORTANT, read below
 	// Must be of either "int-int-string", "int-int" or "int" format otherwise
 	// PlatformLogicalVersion will fail parsing!!!
-	private final String BUILD_NUM = "4983-312";
+	private final String BUILD_NUM = "4983-313";
 	private final String V_NAME = "GigaSpaces";
 	private final String PRODUCT_HELP_URL = "http://www.cloudifysource.org/guide";
-    private final String BUILD_TIMESTAMP = "20130606.b4983-312";
+    private final String BUILD_TIMESTAMP = "20130606.b4983-313";
 
 	/** default constructor for Class.forName() - see com.j_spaces.kernel.PlatformVersion */
 	public CloudifyVersion() {
