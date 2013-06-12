25c5b0dda1bf18d08fdd883cc776e8c9c46ccecd
Tgrid <tgrid@build64A.gspaces.com>
2013-06-12 12:00:28 +0300

    Update cloudify url

--- esc/src/main/resources/clouds/openstack/openstack-cloud.groovy
+++ esc/src/main/resources/clouds/openstack/openstack-cloud.groovy
@@ -39,7 +39,7 @@ cloud {
 		// different HTTP server instead.
 		// IMPORTANT: the default linux bootstrap script appends '.tar.gz' to the url whereas the default windows script appends '.zip'.
 		// Therefore, if setting a custom URL, make sure to leave out the suffix.
-		// cloudifyUrl "http://repository.cloudifysource.org/org/cloudifysource/2.6.0-RC-4985/gigaspaces-cloudify-2.6.0-rc-b4985.zip"
+		// cloudifyUrl "http://repository.cloudifysource.org/org/cloudifysource/2.6.0-M4-4985/gigaspaces-cloudify-2.6.0-m4-b4985.zip"
 
 		// Mandatory. The prefix for new machines started for servies.
 		machineNamePrefix "cloudify-agent-"
