0ff806a79f3d649cace17f23342be5f05498b68a
iliapolo <elip@gigaspaces.com>
2013-06-16 11:20:36 +0300

    CLOUDIFY-1744 added equals and hashcode to event

--- dsl/src/main/java/org/cloudifysource/dsl/rest/response/DeploymentEvent.java
+++ dsl/src/main/java/org/cloudifysource/dsl/rest/response/DeploymentEvent.java
@@ -43,4 +43,38 @@ public class DeploymentEvent {
     public void setDescription(final String description) {
         this.description = description;
     }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        DeploymentEvent event = (DeploymentEvent) o;
+
+        if (index != event.index) {
+            return false;
+        }
+        if (!description.equals(event.description)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = description.hashCode();
+        result = 31 * result + index;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "DeploymentEvent{" + "description='" + description + '\''
+                + ", index=" + index + '}';
+    }
 }
