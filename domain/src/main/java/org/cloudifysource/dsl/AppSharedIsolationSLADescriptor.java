6e1ff0e3c9ef66c70ecdce1c50976fbed8b65d84
barakm <barakme@gmail.com>
2013-06-27 11:47:39 +0300

    CLOUDIFY-1865 Added domain project

---
+++ domain/src/main/java/org/cloudifysource/dsl/AppSharedIsolationSLADescriptor.java
@@ -0,0 +1,26 @@
+package org.cloudifysource.dsl;
+
+import org.cloudifysource.dsl.internal.CloudifyDSLEntity;
+import org.cloudifysource.dsl.internal.DSLValidationContext;
+import org.cloudifysource.dsl.internal.DSLValidationException;
+
+/**
+ * This class defines a service deployment which is shared across all machines 
+ * that are provisioned for the same application in the cluster.
+ * Each service instance will be allocated according to the requirements specified.
+ * @author elip
+ *
+ */
+@CloudifyDSLEntity(name = "appShared", clazz = AppSharedIsolationSLADescriptor.class, allowInternalNode = true,
+	allowRootNode = false, parent = "isolationSLA")
+public class AppSharedIsolationSLADescriptor extends SharedIsolationSLADescriptor {
+
+
+	
+	@Override
+	public String toString() {
+		return "AppSharedIsolationSLADescriptor [instanceMemoryMB="
+				+ getInstanceMemoryMB() + ", instanceCpuCores=" + getInstanceCpuCores()
+				+ ", useManagement=" + isUseManagement() + "]";
+	}
+}
