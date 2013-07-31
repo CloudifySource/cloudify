14f7145b3d19bf1584fe44034b300567601732d1
adamlavie <adaml@gigaspaces.com>
2013-07-31 17:12:42 +0300

    CLOUDIFY-1786 added linux template to the ec2-win cloud config file.

--- esc/src/main/resources/clouds/ec2-win/ec2-win-cloud.groovy
+++ esc/src/main/resources/clouds/ec2-win/ec2-win-cloud.groovy
@@ -122,8 +122,43 @@ cloud {
 	
 	
 	
+					},
+					SMALL_LINUX : computeTemplate{
+						// Mandatory. Image ID.
+						imageId linuxImageId
+						// Mandatory. Files from the local directory will be copied to this directory on the remote machine.
+						remoteDirectory "/home/ec2-user/gs-files"
+						// Mandatory. Amount of RAM available to machine.
+						machineMemoryMB 1600
+						// Mandatory. Hardware ID.
+						hardwareId linuxHardwareId
+						// Optional. Location ID.
+						locationId locationId
+						// Mandatory. All files from this LOCAL directory will be copied to the remote machine directory.
+						localDirectory "upload"
+						// Optional. Name of key file to use for authenticating to the remot machine. Remove this line if key files
+						// are not used.
+						keyFile keyFile
+
+						username "ec2-user"
+						// Additional template options.
+						// When used with the default driver, the option names are considered
+						// method names invoked on the TemplateOptions object with the value as the parameter.
+						options ([
+									"securityGroups" : ["default"]as String[],
+									"keyPair" : keyPair
+								])
+
+						// Optional. Overrides to default cloud driver behavior.
+						// When used with the default driver, maps to the overrides properties passed to the ComputeServiceContext a
+						overrides (["jclouds.ec2.ami-query":"",
+									"jclouds.ec2.cc-ami-query":""])
+
+						// enable sudo.
+						privileged true
+
 					}
-				])
+			])
 	
 		
 	}	
