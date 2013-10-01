7a8a42da47313cfd6401b9f7b967f2a64c20620a
Tgrid <tgrid@build64A.gspaces.com>
2013-10-01 12:07:39 +0200

    Update cloudify url

--- esc/src/main/resources/clouds/rackspace/rackspace-cloud.groovy
+++ esc/src/main/resources/clouds/rackspace/rackspace-cloud.groovy
@@ -42,7 +42,7 @@ cloud {
 		// different HTTP server instead.
 		// IMPORTANT: the default linux bootstrap script appends '.tar.gz' to the url whereas the default windows script appends '.zip'.
 		// Therefore, if setting a custom URL, make sure to leave out the suffix.
-		// cloudifyUrl "http://repository.cloudifysource.org/org/cloudifysource/2.7.0-5985-M3/gigaspaces-cloudify-2.7.0-m3-b5985.zip"
+		// cloudifyUrl "http://repository.cloudifysource.org/org/cloudifysource/2.7.0-5987-M4/gigaspaces-cloudify-2.7.0-m4-b5987.zip"
 
 
 		// Mandatory. The prefix for new machines started for servies.
