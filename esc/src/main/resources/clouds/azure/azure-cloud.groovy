b9434d8eb091c6d43ae4ef6cae441bbf57372df7
Tgrid <tgrid@build64A.gspaces.com>
2013-10-13 17:18:38 +0200

    Update cloudify url

--- esc/src/main/resources/clouds/azure/azure-cloud.groovy
+++ esc/src/main/resources/clouds/azure/azure-cloud.groovy
@@ -32,7 +32,7 @@ cloud {
 		
 		// IMPORTANT: the default linux bootstrap script appends '.tar.gz' to the url whereas the default windows script appends '.zip'.
 		// Therefore, if setting a custom URL, make sure to leave out the suffix.
-		// cloudifyUrl "http://repository.cloudifysource.org/org/cloudifysource/2.6.1-5200-RELEASE/gigaspaces-cloudify-2.6.1-ga-b5200.zip"
+		// cloudifyUrl "http://repository.cloudifysource.org/org/cloudifysource/2.6.2-5400-RELEASE/gigaspaces-cloudify-2.6.2-ga-b5400.zip"
 		
 		machineNamePrefix "cloudify_agent_"
 		
