182b8e5646c62c818510816806ca0c46f8e73031
adamlavie <adaml@gigaspaces.com>
2013-11-14 12:19:35 +0200

    CLOUDIFY-2217 added remote api wrapper for the NetworkProvisioningDriver

---
+++ esc/src/main/java/org/cloudifysource/esc/driver/provisioning/network/RemoteNetworkProvisioningDriverAdapter.java
@@ -0,0 +1,98 @@
+/*******************************************************************************
+ * Copyright (c) 2013 GigaSpaces Technologies Ltd. All rights reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package org.cloudifysource.esc.driver.provisioning.network;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.logging.Logger;
+
+import org.cloudifysource.domain.context.network.RemoteNetworkOperationException;
+import org.cloudifysource.domain.context.network.RemoteNetworkProvisioningDriver;
+
+/**
+ * Adapter class for remote network oporations. 
+ * @author adaml
+ * @since 2.7.0
+ *
+ */
+public class RemoteNetworkProvisioningDriverAdapter implements RemoteNetworkProvisioningDriver {
+
+	private Logger logger = java.util.logging.Logger
+			.getLogger(RemoteNetworkProvisioningDriverAdapter.class.getName());
+	
+	// one minute
+	private static final long DEFAULT_NETWORK_OPERATION_TIMEOUT_MILLIS = 60 * 1000;
+	
+	private NetworkProvisioningDriver driver;
+
+	
+	public RemoteNetworkProvisioningDriverAdapter(final NetworkProvisioningDriver networkDriver) {
+		this.driver = networkDriver;
+	}
+	
+	@Override
+	public void assign(final String ip, final String instanceID)
+			throws RemoteNetworkOperationException, TimeoutException {
+		try {
+			this.driver.assign(ip, instanceID, DEFAULT_NETWORK_OPERATION_TIMEOUT_MILLIS, TimeUnit.MILLISECONDS);
+		} catch (final NetworkProvisioningException e) {
+			logger.warning("failed to assign IP " + ip + " to instance with ID " + instanceID 
+					+ ". Error was " + e.getMessage());
+			throw new RemoteNetworkOperationException("failed to assign IP " + ip + " to instance with ID " 
+					+ instanceID + " : " + e.getMessage(), e);
+		}
+	}
+
+	@Override
+	public void unassign(final String ip, final String instanceID)
+			throws RemoteNetworkOperationException, TimeoutException {
+		try {
+			this.driver.unassign(ip, instanceID, DEFAULT_NETWORK_OPERATION_TIMEOUT_MILLIS, TimeUnit.MILLISECONDS);
+		} catch (final NetworkProvisioningException e) {
+			logger.warning("failed to unassign IP " + ip + " to instance with ID " + instanceID 
+					+ ". Error was " + e.getMessage());
+			throw new RemoteNetworkOperationException("failed to unassign IP " + ip + " to instance with ID " 
+					+ instanceID + " : " + e.getMessage(), e);
+		}
+	}
+
+	@Override
+	public String allocate(final String poolName)
+			throws RemoteNetworkOperationException, TimeoutException {
+		try {
+			return this.driver.allocate(poolName, DEFAULT_NETWORK_OPERATION_TIMEOUT_MILLIS, TimeUnit.MILLISECONDS);
+		} catch (final NetworkProvisioningException e) {
+			logger.warning("failed to allocate a new IP address from pool " + poolName
+					+ " : " + e.getMessage());
+			throw new RemoteNetworkOperationException("failed to allocate a new IP address from pool " + poolName
+					+ " : " + e.getMessage(), e);
+		}
+	}
+
+	@Override
+	public void release(final String ip) throws RemoteNetworkOperationException,
+			TimeoutException {
+		try {
+			this.driver.release(ip, DEFAULT_NETWORK_OPERATION_TIMEOUT_MILLIS, TimeUnit.MILLISECONDS);
+		} catch (final NetworkProvisioningException e) {
+			logger.warning("failed releasing IP address " + ip
+					+ " : " + e.getMessage());
+			throw new RemoteNetworkOperationException("failed releasing IP address " + ip
+					+ " : " + e.getMessage(), e);
+		}
+	}
+
+}
