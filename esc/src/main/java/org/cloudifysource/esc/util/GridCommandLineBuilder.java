33bce940dc7cdb53c145c631513ba57a7d23538e
iliapolo <elip@gigaspaces.com>
2013-12-03 12:02:49 +0200

    CLOUDIFY-2258 added start/stop machine timeouts to cloud driver configuration

--- esc/src/main/java/org/cloudifysource/esc/util/GridCommandLineBuilder.java
+++ esc/src/main/java/org/cloudifysource/esc/util/GridCommandLineBuilder.java
@@ -167,8 +167,10 @@ public final class GridCommandLineBuilder {
 		String esmCommandLineArgs = "";
 		esmCommandLineArgs += getComponentMemoryArgs(esm.getMaxMemory(), esm.getMinMemory());
 		esmCommandLineArgs += getComponentRmiArgs(esm.getPort().toString());
-        esmCommandLineArgs += "-Dorg.openspaces.grid.start-agent-timeout-seconds=" + esm.getStartMachineTimeoutInSeconds();
-        esmCommandLineArgs += "-Dorg.openspaces.grid.stop-agent-timeout-seconds=" + esm.getStopMachineTimeoutInSeconds();
+        esmCommandLineArgs += " -Dorg.openspaces.grid.start-agent-timeout-seconds="
+                + esm.getStartMachineTimeoutInSeconds();
+        esmCommandLineArgs += " -Dorg.openspaces.grid.stop-agent-timeout-seconds="
+                + esm.getStopMachineTimeoutInSeconds();
 
 		if (reboostrapping) {
 			esmCommandLineArgs +=
