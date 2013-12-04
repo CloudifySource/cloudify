315c60f43b0207c46225f41164bb90e4a4ce7857
noak <noak@gigaspaces.com>
2013-12-04 12:11:23 +0200

    CLOUDIFY-2241 - added copyrights text and javadoc.

--- security/src/main/java/org/cloudifysource/security/CloudifyUser.java
+++ security/src/main/java/org/cloudifysource/security/CloudifyUser.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2013 GigaSpaces Technologies Ltd. All rights reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 package org.cloudifysource.security;
 
 import java.util.ArrayList;
@@ -11,8 +26,18 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
 
+/**
+ * This class implements a Cloudify user, which contains the default Spring user information 
+ * implemented in {@link User} and adds support for  authorization groups data as well. 
+ * It is used by {@link CloudifyUserDetailsService}.
+ *
+ * @author noak
+ * @since 2.7
+ */
 public class CloudifyUser implements CloudifyUserDetails {
 
+	private static final long serialVersionUID = 4339626422841363440L;
+	
 	private static final String VALUES_SEPARATOR = ",";
 	private User delegateUser;
 	private Collection<String> authGroups;
--- security/src/main/java/org/cloudifysource/security/CloudifyUserDetails.java
+++ security/src/main/java/org/cloudifysource/security/CloudifyUserDetails.java
@@ -1,21 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2013 GigaSpaces Technologies Ltd. All rights reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 package org.cloudifysource.security;
 
 import java.util.Collection;
 
 import org.springframework.security.core.userdetails.UserDetails;
 
+/**
+ * Extending Spring's {@link UserDetails} interface to support authorization groups as well.
+ * 
+ * @author noak
+ * @since 2.7
+ */
 public interface CloudifyUserDetails extends UserDetails {
 
     /**
-     * Gets the permitted security roles for the active user.
-     *
+     * Gets the permitted security roles for the active user as a collection of Strings.
      * @return A collection of roles as string values.
      */
     Collection<String> getRoles();
 
+
     /**
      * Gets the registered authorization groups for the active user.
-     *
      * @return A collection of authorization groups as string values.
      */
     Collection<String> getAuthGroups();
