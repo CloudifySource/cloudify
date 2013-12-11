7f919924cd6e9c7588f655817e65ba153300156f
barakm <barakme@gigaspaces.com>
2013-12-12 00:19:53 +0200

    CLOUDIFY-2256 Added validation for monitors event

--- dsl/src/main/java/org/cloudifysource/dsl/internal/validators/ServiceLifecycleValidator.java
+++ dsl/src/main/java/org/cloudifysource/dsl/internal/validators/ServiceLifecycleValidator.java
@@ -1,19 +1,18 @@
 /*******************************************************************************
  * Copyright (c) 2013 GigaSpaces Technologies Ltd. All rights reserved
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  *******************************************************************************/
 package org.cloudifysource.dsl.internal.validators;
+
+import java.util.Map;
 
 import org.cloudifysource.domain.DSLValidation;
 import org.cloudifysource.domain.ServiceLifecycle;
@@ -24,7 +23,7 @@ import org.cloudifysource.dsl.internal.DSLValidationException;
 /**
  * 
  * @author adaml
- *
+ * 
  */
 public class ServiceLifecycleValidator implements DSLValidator {
 
@@ -34,7 +33,7 @@ public class ServiceLifecycleValidator implements DSLValidator {
 	public void setDSLEntity(Object dslEntity) {
 		this.entity = (ServiceLifecycle) dslEntity;
 	}
-	
+
 	@DSLValidation
 	void validateStopDetectorIsClosure(final DSLValidationContext validationContext)
 			throws DSLValidationException {
@@ -44,4 +43,24 @@ public class ServiceLifecycleValidator implements DSLValidator {
 					"The stop detection field only supports execution of closures");
 		}
 	}
+
+	@DSLValidation
+	void validateMonitorsIsClosureOrMap(final DSLValidationContext validationContext)
+			throws DSLValidationException {
+		Object monitors = this.entity.getMonitors();
+		if(monitors == null) {
+			return;
+		}
+		if (monitors instanceof ClosureExecutableEntry) {
+				return;
+		}
+		
+		if(monitors instanceof Map<?, ?>) {
+			return;
+		}
+		
+		throw new DSLValidationException(
+				"The monitors event only supports execution of closures or returning a map of executable entries");
+	}
+
 }
---
+++ dsl/src/test/java/org/cloudifysource/dsl/internal/validators/ServiceLifecycleValidatorTest.java
@@ -0,0 +1,229 @@
+package org.cloudifysource.dsl.internal.validators;
+
+import groovy.lang.Closure;
+
+import java.io.File;
+import java.util.HashMap;
+
+import org.cloudifysource.domain.ServiceLifecycle;
+import org.cloudifysource.dsl.entry.ExecutableDSLEntryFactory;
+import org.cloudifysource.dsl.internal.DSLValidationContext;
+import org.cloudifysource.dsl.internal.DSLValidationException;
+import org.junit.*;
+
+/**
+ * The class <code>ServiceLifecycleValidatorTest</code> contains tests for the class <code>{@link ServiceLifecycleValidator}</code>.
+ *
+ * @generatedBy CodePro at 12/11/13 5:43 PM
+ * @author barakme
+ * @version $Revision: 1.0 $
+ */
+public class ServiceLifecycleValidatorTest {
+	/**
+	 * Run the void setDSLEntity(Object) method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 12/11/13 5:43 PM
+	 */
+	@Test
+	public void testSetDSLEntity_1()
+		throws Exception {
+		ServiceLifecycleValidator fixture = new ServiceLifecycleValidator();
+		fixture.setDSLEntity(new ServiceLifecycle());
+		Object dslEntity = new ServiceLifecycle();
+
+		fixture.setDSLEntity(dslEntity);
+
+		// add additional test code here
+	}
+
+	/**
+	 * Run the void validateMonitosIsClosureOrMap(DSLValidationContext) method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 12/11/13 5:43 PM
+	 */
+	@Test
+	public void testValidateMonitosIsClosureOrMap_1()
+		throws Exception {
+		ServiceLifecycleValidator fixture = new ServiceLifecycleValidator();
+		fixture.setDSLEntity(new ServiceLifecycle());
+		DSLValidationContext validationContext = new DSLValidationContext();
+
+		fixture.validateMonitorsIsClosureOrMap(validationContext);
+
+		// add additional test code here
+	}
+
+	/**
+	 * Run the void validateMonitosIsClosureOrMap(DSLValidationContext) method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 12/11/13 5:43 PM
+	 */
+	@Test(expected = DSLValidationException.class)
+	public void testValidateMonitosIsClosureOrMap_2()
+		throws Exception {
+		ServiceLifecycleValidator fixture = new ServiceLifecycleValidator();
+		ServiceLifecycle dslEntity = new ServiceLifecycle();
+		dslEntity.setMonitors(ExecutableDSLEntryFactory.createEntry("x.groovy", "monitors", new File(".")));
+		fixture.setDSLEntity(dslEntity);
+		DSLValidationContext validationContext = new DSLValidationContext();
+
+		fixture.validateMonitorsIsClosureOrMap(validationContext);
+
+		// add additional test code here
+	}
+
+	/**
+	 * Run the void validateMonitosIsClosureOrMap(DSLValidationContext) method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 12/11/13 5:43 PM
+	 */
+	@Test
+	public void testValidateMonitosIsClosureOrMap_3()
+		throws Exception {
+		ServiceLifecycleValidator fixture = new ServiceLifecycleValidator();
+		ServiceLifecycle dslEntity = new ServiceLifecycle();
+		dslEntity.setMonitors(new HashMap<Object, Object>());
+		fixture.setDSLEntity(dslEntity);
+		DSLValidationContext validationContext = new DSLValidationContext();
+
+		fixture.validateMonitorsIsClosureOrMap(validationContext);
+
+		// add additional test code here
+	}
+
+	/**
+	 * Run the void validateMonitosIsClosureOrMap(DSLValidationContext) method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 12/11/13 5:43 PM
+	 */
+	@Test
+	public void testValidateMonitosIsClosureOrMap_4()
+		throws Exception {
+		ServiceLifecycleValidator fixture = new ServiceLifecycleValidator();
+		ServiceLifecycle dslEntity = new ServiceLifecycle();
+		Closure<Object> closure = new Closure<Object>(null) {
+			private static final long serialVersionUID = 1L;
+		};
+		
+		dslEntity.setMonitors(ExecutableDSLEntryFactory.createEntry(closure, "monitors", new File(".")));
+		fixture.setDSLEntity(dslEntity);
+		DSLValidationContext validationContext = new DSLValidationContext();
+
+		fixture.validateMonitorsIsClosureOrMap(validationContext);
+
+		// add additional test code here
+	}
+
+	/**
+	 * Run the void validateStopDetectorIsClosure(DSLValidationContext) method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 12/11/13 5:43 PM
+	 */
+	@Test
+	public void testValidateStopDetectorIsClosure_1()
+		throws Exception {
+		ServiceLifecycleValidator fixture = new ServiceLifecycleValidator();
+		fixture.setDSLEntity(new ServiceLifecycle());
+		DSLValidationContext validationContext = new DSLValidationContext();
+
+		fixture.validateStopDetectorIsClosure(validationContext);
+
+		// add additional test code here
+	}
+
+	/**
+	 * Run the void validateStopDetectorIsClosure(DSLValidationContext) method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 12/11/13 5:43 PM
+	 */
+	@Test(expected = DSLValidationException.class)
+	public void testValidateStopDetectorIsClosure_2()
+		throws Exception {
+		ServiceLifecycleValidator fixture = new ServiceLifecycleValidator();
+		ServiceLifecycle dslEntity = new ServiceLifecycle();
+		dslEntity.setStopDetection(ExecutableDSLEntryFactory.createEntry("x.groovy", "stopDetection", new File(".")));
+		fixture.setDSLEntity(dslEntity);
+		DSLValidationContext validationContext = new DSLValidationContext();
+
+		fixture.validateStopDetectorIsClosure(validationContext);
+
+		// add additional test code here
+	}
+
+	/**
+	 * Run the void validateStopDetectorIsClosure(DSLValidationContext) method test.
+	 *
+	 * @throws Exception
+	 *
+	 * @generatedBy CodePro at 12/11/13 5:43 PM
+	 */
+	@Test
+	public void testValidateStopDetectorIsClosure_3()
+		throws Exception {
+		ServiceLifecycleValidator fixture = new ServiceLifecycleValidator();
+		ServiceLifecycle dslEntity = new ServiceLifecycle();
+		dslEntity.setStopDetection(ExecutableDSLEntryFactory.createEntry(new Closure<Object>(null) {
+			private static final long serialVersionUID = 1L;
+		}, "monitors", new File(".")));
+		fixture.setDSLEntity(dslEntity);
+		
+		DSLValidationContext validationContext = new DSLValidationContext();
+
+		fixture.validateStopDetectorIsClosure(validationContext);
+
+		// add additional test code here
+	}
+
+	/**
+	 * Perform pre-test initialization.
+	 *
+	 * @throws Exception
+	 *         if the initialization fails for some reason
+	 *
+	 * @generatedBy CodePro at 12/11/13 5:43 PM
+	 */
+	@Before
+	public void setUp()
+		throws Exception {
+		// add additional set up code here
+	}
+
+	/**
+	 * Perform post-test clean-up.
+	 *
+	 * @throws Exception
+	 *         if the clean-up fails for some reason
+	 *
+	 * @generatedBy CodePro at 12/11/13 5:43 PM
+	 */
+	@After
+	public void tearDown()
+		throws Exception {
+		// Add additional tear down code here
+	}
+
+	/**
+	 * Launch the test.
+	 *
+	 * @param args the command line arguments
+	 *
+	 * @generatedBy CodePro at 12/11/13 5:43 PM
+	 */
+	public static void main(String[] args) {
+		new org.junit.runner.JUnitCore().run(ServiceLifecycleValidatorTest.class);
+	}
+}
