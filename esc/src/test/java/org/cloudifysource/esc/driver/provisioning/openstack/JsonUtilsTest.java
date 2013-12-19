c0326031e1ccfa268ae4aed9ed1938a54bb21666
Victor Keophila <victor@fastconnect.fr>
2013-12-19 15:51:17 +0100

    Fix assign floating ip on network driver. Sometimes the driver took a wrong port id leading not to be able to assign floating ip.

--- esc/src/test/java/org/cloudifysource/esc/driver/provisioning/openstack/JsonUtilsTest.java
+++ esc/src/test/java/org/cloudifysource/esc/driver/provisioning/openstack/JsonUtilsTest.java
@@ -1,8 +1,11 @@
 package org.cloudifysource.esc.driver.provisioning.openstack;
 
+import java.io.File;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.cloudifysource.esc.driver.provisioning.openstack.rest.Network;
+import org.cloudifysource.esc.driver.provisioning.openstack.rest.Port;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -24,5 +27,21 @@ public class JsonUtilsTest {
 		Network network = JsonUtils.unwrapRootToObject(Network.class, "{\"network\":{\"name\":\"test\"}}");
 		Assert.assertNotNull(network);
 		Assert.assertEquals("test", network.getName());
+	}
+
+	@Test
+	public void testPortsResponse() throws Exception {
+		File file = new File("./src/test/resources/openstack/mappings/ports-response.json");
+		String jsonString = FileUtils.readFileToString(file);
+		List<Port> ports = JsonUtils.unwrapRootToList(Port.class, jsonString);
+
+		Assert.assertNotNull(ports);
+		Assert.assertFalse(ports.isEmpty());
+		for (Port port : ports) {
+			Assert.assertNotNull(port.getFixedIps());
+			Assert.assertFalse(port.getFixedIps().isEmpty());
+			Assert.assertNotNull(port.getFixedIps().get(0).getIpAddress());
+		}
+
 	}
 }
