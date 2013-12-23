f77814e81b50e50141d8e47e2d9294264197664a
limor-gs <limor@gigaspaces.com>
2013-12-23 21:20:40 +0200

    Update cloudify url

--- esc/src/main/resources/clouds/softlayer/softlayer-cloud.groovy
+++ esc/src/main/resources/clouds/softlayer/softlayer-cloud.groovy
@@ -71,7 +71,7 @@ cloud {
 		// different HTTP server instead.
 		// IMPORTANT: the default linux bootstrap script appends '.tar.gz' to the url whereas the default windows script appends '.zip'.
 		// Therefore, if setting a custom URL, make sure to leave out the suffix.
-        // cloudifyUrl "http://repository.cloudifysource.org/org/cloudifysource/2.7.0-5988-M5/gigaspaces-cloudify-2.7.0-m5-b5988.zip"
+        // cloudifyUrl "http://repository.cloudifysource.org/org/cloudifysource/2.7.0-5994-RC/gigaspaces-cloudify-2.7.0-rc-b5994.zip"
 
 		// Mandatory. The prefix for new machines started for servies.
         machineNamePrefix "cloudify-agent-"
