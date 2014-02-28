1179814839eeedc0cb7d9de9be7dcb8d459f92ad
limor-gs <limor@gigaspaces.com>
2014-02-28 21:16:59 +0200

    Build version advanced to 6201-64

--- version/src/main/java/com/j_spaces/kernel/CloudifyVersion.java
+++ version/src/main/java/com/j_spaces/kernel/CloudifyVersion.java
@@ -34,10 +34,10 @@ public class CloudifyVersion implements ProductVersion {
 	// !!!IMPORTANT, read below
 	// Must be of either "int-int-string", "int-int" or "int" format otherwise
 	// PlatformLogicalVersion will fail parsing!!!
-	private final String BUILD_NUM = "6201-62";
+	private final String BUILD_NUM = "6201-64";
 	private final String V_NAME = "GigaSpaces";
 	private final String PRODUCT_HELP_URL = "http://www.cloudifysource.org/guide";
-    private final String BUILD_TIMESTAMP = "6201-62";
+    private final String BUILD_TIMESTAMP = "6201-64";
 
 	/** default constructor for Class.forName() - see com.j_spaces.kernel.PlatformVersion */
 	public CloudifyVersion() {
