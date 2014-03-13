0853e427dd44286662a77521bcaf43f2d6017435
adamlavie <adaml@gigaspaces.com>
2014-03-13 17:15:17 +0200

    CLOUDIFY-2517 fixed checkstyle comments

--- esc/src/test/java/org/cloudifysource/esc/driver/provisioning/openstack/OpenStackCloudifyDriverValidationTest.java
+++ esc/src/test/java/org/cloudifysource/esc/driver/provisioning/openstack/OpenStackCloudifyDriverValidationTest.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2014 GigaSpaces Technologies Ltd. All rights reserved
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ ******************************************************************************/
 package org.cloudifysource.esc.driver.provisioning.openstack;
 
 import java.io.File;
@@ -33,21 +45,20 @@ public class OpenStackCloudifyDriverValidationTest {
 	private class ValidationContextStub implements ValidationContext {
 
 		@Override
-		public void validationEvent(ValidationMessageType messageType, String message) {
+		public void validationEvent(final ValidationMessageType messageType, final String message) {
 		}
 
 		@Override
-		public void validationOngoingEvent(ValidationMessageType messageType, String message) {
+		public void validationOngoingEvent(final ValidationMessageType messageType, final String message) {
 		}
 
 		@Override
-		public void validationEventEnd(ValidationResultType validtionResultType) {
+		public void validationEventEnd(final ValidationResultType validtionResultType) {
 		}
 	}
 
 	private OpenStackComputeClient computeApi;
 	private OpenStackNetworkClient networkApi;
-	private OpenStackNetworkConfigurationHelper networkHelper;
 
 	@Before
 	public void before() throws OpenstackException {
