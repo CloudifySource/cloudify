949a89603a94bdff73828b1757a8c2837fea22bc
limor-gs <limor@gigaspaces.com>
2014-03-25 13:00:15 +0200

    Update cloudify url

--- esc/src/main/resources/clouds/ec2/ec2-cloud.groovy
+++ esc/src/main/resources/clouds/ec2/ec2-cloud.groovy
@@ -42,7 +42,7 @@ cloud {
 		// different HTTP server instead.
 		// IMPORTANT: the default linux bootstrap script appends '.tar.gz' to the url whereas the default windows script appends '.zip'.
 		// Therefore, if setting a custom URL, make sure to leave out the suffix.
-		// cloudifyUrl "http://repository.cloudifysource.org/org/cloudifysource/2.7.0-5995-RC2/gigaspaces-cloudify-2.7.0-rc2-b5995.zip"
+		// cloudifyUrl "http://repository.cloudifysource.org/org/cloudifysource/2.7.0-5997-RC2/gigaspaces-cloudify-2.7.0-rc2-b5997.zip"
 
 
 		// Mandatory. The prefix for new machines started for servies.
