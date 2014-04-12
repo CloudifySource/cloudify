d91e4c58a0c88065b1b781178caace252da1a6b7
limor-gs <limor@gigaspaces.com>
2014-04-13 01:16:56 +0300

    Update cloudify url

--- esc/src/main/resources/clouds/hp-grizzly/hp-grizzly-cloud.groovy
+++ esc/src/main/resources/clouds/hp-grizzly/hp-grizzly-cloud.groovy
@@ -37,7 +37,7 @@ cloud {
 		// different HTTP server instead.
 		// IMPORTANT: the default linux bootstrap script appends '.tar.gz' to the url whereas the default windows script appends '.zip'.
 		// Therefore, if setting a custom URL, make sure to leave out the suffix.
-		// cloudifyUrl "http://repository.cloudifysource.org/org/cloudifysource/2.7.0-5996-RELEASE/gigaspaces-cloudify-2.7.0-ga-b5996.zip"
+		// cloudifyUrl "http://repository.cloudifysource.org/org/cloudifysource/2.7.1-6205-M5/gigaspaces-cloudify-2.7.1-m5-b6205.zip"
 
 		// Mandatory. The prefix for new machines started for servies.
 		machineNamePrefix "cloudify-agent-"
