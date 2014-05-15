eabd84ebf96aff66c72c43239269dab641cba235
adamlavie <adaml@gigaspaces.com>
2014-05-15 18:43:12 +0300

    CLOUDIFY-2713 changed pre-bootstrap script so it will also work on ubuntu

--- esc/src/main/resources/clouds/exoscale/upload/pre-bootstrap.sh
+++ esc/src/main/resources/clouds/exoscale/upload/pre-bootstrap.sh
@@ -4,4 +4,9 @@ HOST_NAME=`hostname`
 echo "$MACHINE_IP_ADDRESS $HOST_NAME" | sudo -A tee -a /etc/hosts
 
 #disable firewall
-service iptables stop
+os=`uname -a`
+if [[ $os =~ .*Ubuntu.* ]]; then
+	service ufw stop
+else
+	service iptables stop
+fi
