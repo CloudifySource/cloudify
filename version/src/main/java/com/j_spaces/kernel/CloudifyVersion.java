1503077027776b85960ee8f09d8693a576e9de25
opencm <limor@gigaspaces.com>
2014-07-20 16:54:17 +0300

    Build version advanced to 6207-194

--- version/src/main/java/com/j_spaces/kernel/CloudifyVersion.java
+++ version/src/main/java/com/j_spaces/kernel/CloudifyVersion.java
@@ -34,10 +34,10 @@ public class CloudifyVersion implements ProductVersion {
 	// !!!IMPORTANT, read below
 	// Must be of either "int-int-string", "int-int" or "int" format otherwise
 	// PlatformLogicalVersion will fail parsing!!!
-	private final String BUILD_NUM = "6207-184";
+	private final String BUILD_NUM = "6207-194";
 	private final String V_NAME = "GigaSpaces";
 	private final String PRODUCT_HELP_URL = "http://www.cloudifysource.org/guide";
-    private final String BUILD_TIMESTAMP = "6207-184";
+    private final String BUILD_TIMESTAMP = "6207-194";
 
 	/** default constructor for Class.forName() - see com.j_spaces.kernel.PlatformVersion */
 	public CloudifyVersion() {
